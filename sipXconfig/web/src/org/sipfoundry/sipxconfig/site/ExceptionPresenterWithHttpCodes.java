197014fd8260f4aeb25772047994de77693adcb1
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2007-04-14 12:07:00 +0000

    Checkstyle fixes.
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@10001 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/ExceptionPresenterWithHttpCodes.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/ExceptionPresenterWithHttpCodes.java
@@ -1,7 +1,6 @@
 /*
  * 
  * 
- * Copyright (C) 2006 SIPfoundry Inc.
  * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.  
  * Contributors retain copyright to elements licensed under a Contributor Agreement.
  * Licensed to the User under the LGPL license.
