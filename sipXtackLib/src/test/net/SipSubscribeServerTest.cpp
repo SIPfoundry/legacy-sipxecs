818f8cb4a28ce3624a7ee37c01f76f284aca440b
dworley <dworley@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2007-07-10 17:51:46 +0000

    Add unit test for XECS-243:  Check that subscription is terminated if NOTIFY receives error response.
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@10444 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXtackLib/src/test/net/SipSubscribeServerTest.cpp
+++ sipXtackLib/src/test/net/SipSubscribeServerTest.cpp
@@ -22,8 +22,6 @@
 #include <net/SipSubscribeServer.h>
 #include <net/SipPublishContentMgr.h>
 
-#define UNIT_TEST_SIP_PORT 44444
-
 /**
  * Unittest for SipSubscriptionMgr
  */
@@ -32,6 +30,8 @@ class SipSubscribeServerTest : public CppUnit::TestCase
       CPPUNIT_TEST_SUITE(SipSubscribeServerTest);
       CPPUNIT_TEST(basicSubscriptionTest);
       CPPUNIT_TEST(resubscribeContentTest);
+      CPPUNIT_TEST(terminateSubscriptionOnError);
+      CPPUNIT_TEST(terminateSubscriptionOnErrorRetryAfter);
       CPPUNIT_TEST_SUITE_END();
 
       public:
@@ -42,7 +42,8 @@ class SipSubscribeServerTest : public CppUnit::TestCase
                     OsTime timeout, //< Length of time before timing out.
                     const SipMessage*& request, //< Pointer to any request that was received
                     const SipMessage*& response, //< Pointer to any response that was received
-                    int responseCode //< Response code to give to the request
+                    int responseCode, //< Response code to give to the request
+                    UtlBoolean retry = FALSE //< TRUE to add "Retry-After: 0" to response
       )
    {
       // Initialize the request and response pointers.
@@ -80,6 +81,11 @@ class SipSubscribeServerTest : public CppUnit::TestCase
                                             responseCode,
                                             // Provide dummy response text
                                             "dummy");
+            // Set "Retry-After: 0" if requested.
+            if (retry)
+            {
+               requestResponse.setHeaderValue(SIP_RETRY_AFTER_FIELD, "0", 0);
+            }
             CPPUNIT_ASSERT(userAgent.send(requestResponse));
          }
       }
@@ -387,7 +393,7 @@ class SipSubscribeServerTest : public CppUnit::TestCase
 
    // Test for XECS-243, Subscribe Server does not send NOTIFY when it
    // processes a re-SUBSCRIBE.
-//*** Error scenario not yet found.
+//*** Error scenario not yet reproduced.
    void resubscribeContentTest()
    {
       UtlString hostIp("127.0.0.1");
@@ -539,6 +545,342 @@ class SipSubscribeServerTest : public CppUnit::TestCase
          {
             const HttpBody* bodyPtr = notifyRequest->getBody();
             CPPUNIT_ASSERT(bodyPtr == NULL);
+         }
+      }
+
+      // Clean up to prevent use of the queue after it goes out of scope.
+      userAgent.removeMessageObserver(incomingClientMsgQueue);
+      userAgent.removeMessageObserver(incomingClientMsgQueue);
+
+      userAgent.shutdown(TRUE);
+       
+      delete subServer;
+      subServer = NULL;
+   }
+
+   // Test for XECS-247, When subscribe server receives 481 for a
+   // NOTIFY, it does not terminate the subscription.
+   // Verify that subscription is terminated when NOTIFY returns a variety
+   // of error responses.  (Retry-After suppresses termination.  It is tested
+   // in another test.)
+   void terminateSubscriptionOnError()
+   {
+      UtlString hostIp("127.0.0.1");
+
+      // Test MWI messages
+      const char* mwiSubscribe =
+         "SUBSCRIBE sip:111@localhost SIP/2.0\r\n"
+         "From: <sip:111@example.com>;tag=1612c1612\r\n"
+         "To: <sip:111@example.com>\r\n"
+         "Call-Id: e2aab34a72a0eb18300fbec445d5d665\r\n"
+         "Cseq: 1 SUBSCRIBE\r\n"
+         "Event: message-summary\r\n"
+         "Accept: application/simple-message-summary\r\n"
+         "Expires: 3600\r\n"
+         "Date: Tue, 26 Apr 2005 14:59:30 GMT\r\n"
+         "Max-Forwards: 20\r\n"
+         "User-Agent: Pingtel/2.2.0 (VxWorks)\r\n"
+         "Accept-Language: en\r\n"
+         "Supported: sip-cc, sip-cc-01, timer, replaces\r\n"
+         "Content-Length: 0\r\n"
+         "\r\n";
+
+      UtlString eventName(SIP_EVENT_MESSAGE_SUMMARY);
+      UtlString mwiMimeType(CONTENT_TYPE_SIMPLE_MESSAGE_SUMMARY);
+
+      // Construct a user agent that will function both as the subscriber
+      // and the notfier.
+      // Listen on only a TCP port, as we do not want to have to specify a
+      // fixed port number (to allow multiple executions of the test),
+      // and the code to select a port automatically cannot be told to
+      // open matching UDP and TCP ports.
+      SipUserAgent userAgent(PORT_DEFAULT, PORT_NONE, PORT_NONE,
+                             NULL, NULL, hostIp);
+      userAgent.start();
+
+      // Construct the URI of the notifier, which is also the URI of
+      // the subscriber.
+      UtlString aor;
+      // Also construct the name-addr version of the URI, which may be
+      // different if it has a "transport" parameter.
+      UtlString aor_name_addr;
+      // And the resource-id to use, which is the AOR with any
+      // parameters stripped off.
+      UtlString resource_id;
+      {
+         char buffer[100];
+         sprintf(buffer, "sip:111@%s:%d", hostIp.data(),
+                 userAgent.getTcpPort());
+         resource_id = buffer;
+
+         // Specify TCP transport, because that's all the UA listens to.
+         // Using an address with a transport parameter also exercises
+         // SipDialog to ensure it handles contact addresses with parameters.
+         strcat(buffer, ";transport=tcp");
+         aor = buffer;
+
+         Url aor_uri(buffer, TRUE);
+         aor_uri.toString(aor_name_addr);
+      }
+
+      SipSubscribeServer* subServer = 
+         SipSubscribeServer::buildBasicServer(userAgent, 
+                                              eventName);
+      subServer->start();
+
+      // Get pointers to the Subscription Manager and Dialog Manager.
+      SipSubscriptionMgr* subMgr = subServer->getSubscriptionMgr(eventName);
+      CPPUNIT_ASSERT(subMgr);
+      SipDialogMgr* dialogMgr = subMgr->getDialogMgr();
+      CPPUNIT_ASSERT(dialogMgr);
+
+      // Create a crude Subscription client
+      OsMsgQ incomingClientMsgQueue;
+      // Register an interest in SUBSCRIBE responses and NOTIFY requests
+      // for this event type
+      userAgent.addMessageObserver(incomingClientMsgQueue,
+                                   SIP_SUBSCRIBE_METHOD,
+                                   FALSE, // no requests
+                                   TRUE, // reponses
+                                   TRUE, // incoming
+                                   FALSE, // no outgoing
+                                   eventName,
+                                   NULL,
+                                   NULL);
+      userAgent.addMessageObserver(incomingClientMsgQueue,
+                                   SIP_NOTIFY_METHOD,
+                                   TRUE, // requests
+                                   FALSE, // not reponses
+                                   TRUE, // incoming
+                                   FALSE, // no outgoing
+                                   eventName,
+                                   NULL,
+                                   NULL);
+
+      // Send a SUBSCRIBE to ourselves
+      SipMessage mwiSubscribeRequest(mwiSubscribe);
+      mwiSubscribeRequest.setSipRequestFirstHeaderLine(SIP_SUBSCRIBE_METHOD, 
+                                                       aor, 
+                                                       SIP_PROTOCOL_VERSION);
+      mwiSubscribeRequest.setContactField(aor_name_addr);
+
+      // Loop through this scenario for a series of response codes.
+      // Even 408 is expected to terminate the subscription.
+      // (RFC 3265 section 3.2.2.)
+      int test_codes[] = { SIP_BAD_REQUEST_CODE,
+                           SIP_NOT_FOUND_CODE,
+                           SIP_REQUEST_TIMEOUT_CODE,
+                           SIP_BAD_TRANSACTION_CODE,
+                           499,
+                           SIP_SERVER_INTERNAL_ERROR_CODE,
+                           SIP_SERVICE_UNAVAILABLE_CODE,
+                           599,
+                           SIP_GLOBAL_BUSY_CODE,
+                           699 };
+      for (int i = 0; i < sizeof (test_codes) / sizeof (test_codes[0]); i++)
+      {
+         mwiSubscribeRequest.incrementCSeqNumber();
+         CPPUNIT_ASSERT(userAgent.send(mwiSubscribeRequest));
+
+         // We should get a 202 response and a NOTIFY request in the queue
+         // Send the specified response to the NOTIFY.
+         OsTime messageTimeout(1, 0);  // 1 second
+         {
+            const SipMessage* subscribeResponse;
+            const SipMessage* notifyRequest;
+            runListener(incomingClientMsgQueue, userAgent, messageTimeout,
+                        notifyRequest, subscribeResponse,
+                        test_codes[i]);
+
+            // We should have received a SUBSCRIBE response and a NOTIFY request.
+            CPPUNIT_ASSERT(subscribeResponse);
+            CPPUNIT_ASSERT(notifyRequest);
+
+            CPPUNIT_ASSERT(subscribeResponse->getResponseStatusCode() ==
+                           SIP_ACCEPTED_CODE);
+         }
+
+         // Send a re-SUBSCRIBE
+         mwiSubscribeRequest.incrementCSeqNumber();
+         // Leave the Expires header with the default value.
+
+         CPPUNIT_ASSERT(userAgent.send(mwiSubscribeRequest));
+
+         // We should get a 481 response and no NOTIFY.
+         {
+            const SipMessage* subscribeResponse;
+            const SipMessage* notifyRequest;
+            runListener(incomingClientMsgQueue, userAgent, messageTimeout,
+                        notifyRequest, subscribeResponse, SIP_OK_CODE);
+
+            // We should have received a SUBSCRIBE response and no NOTIFY request.
+            CPPUNIT_ASSERT(subscribeResponse);
+            CPPUNIT_ASSERT(!notifyRequest);
+
+            CPPUNIT_ASSERT(subscribeResponse->getResponseStatusCode() ==
+                           SIP_BAD_TRANSACTION_CODE);
+         }
+      }
+
+      // Clean up to prevent use of the queue after it goes out of scope.
+      userAgent.removeMessageObserver(incomingClientMsgQueue);
+      userAgent.removeMessageObserver(incomingClientMsgQueue);
+
+      userAgent.shutdown(TRUE);
+       
+      delete subServer;
+      subServer = NULL;
+   }
+
+   // Verify that subscription is *not* terminated when NOTIFY returns an
+   // error responses containing "Retry-After: 0" header.
+   // See RFC 3265 section 3.2.2
+   void terminateSubscriptionOnErrorRetryAfter()
+   {
+      UtlString hostIp("127.0.0.1");
+
+      // Test MWI messages
+      const char* mwiSubscribe =
+         "SUBSCRIBE sip:111@localhost SIP/2.0\r\n"
+         "From: <sip:111@example.com>;tag=1612c1612\r\n"
+         "To: <sip:111@example.com>\r\n"
+         "Call-Id: e2aab34a72a0eb18300fbec445d5d665\r\n"
+         "Cseq: 1 SUBSCRIBE\r\n"
+         "Event: message-summary\r\n"
+         "Accept: application/simple-message-summary\r\n"
+         "Expires: 3600\r\n"
+         "Date: Tue, 26 Apr 2005 14:59:30 GMT\r\n"
+         "Max-Forwards: 20\r\n"
+         "User-Agent: Pingtel/2.2.0 (VxWorks)\r\n"
+         "Accept-Language: en\r\n"
+         "Supported: sip-cc, sip-cc-01, timer, replaces\r\n"
+         "Content-Length: 0\r\n"
+         "\r\n";
+
+      UtlString eventName(SIP_EVENT_MESSAGE_SUMMARY);
+      UtlString mwiMimeType(CONTENT_TYPE_SIMPLE_MESSAGE_SUMMARY);
+
+      // Construct a user agent that will function both as the subscriber
+      // and the notfier.
+      // Listen on only a TCP port, as we do not want to have to specify a
+      // fixed port number (to allow multiple executions of the test),
+      // and the code to select a port automatically cannot be told to
+      // open matching UDP and TCP ports.
+      SipUserAgent userAgent(PORT_DEFAULT, PORT_NONE, PORT_NONE,
+                             NULL, NULL, hostIp);
+      userAgent.start();
+
+      // Construct the URI of the notifier, which is also the URI of
+      // the subscriber.
+      UtlString aor;
+      // Also construct the name-addr version of the URI, which may be
+      // different if it has a "transport" parameter.
+      UtlString aor_name_addr;
+      // And the resource-id to use, which is the AOR with any
+      // parameters stripped off.
+      UtlString resource_id;
+      {
+         char buffer[100];
+         sprintf(buffer, "sip:111@%s:%d", hostIp.data(),
+                 userAgent.getTcpPort());
+         resource_id = buffer;
+
+         // Specify TCP transport, because that's all the UA listens to.
+         // Using an address with a transport parameter also exercises
+         // SipDialog to ensure it handles contact addresses with parameters.
+         strcat(buffer, ";transport=tcp");
+         aor = buffer;
+
+         Url aor_uri(buffer, TRUE);
+         aor_uri.toString(aor_name_addr);
+      }
+
+      SipSubscribeServer* subServer = 
+         SipSubscribeServer::buildBasicServer(userAgent, 
+                                              eventName);
+      subServer->start();
+
+      // Get pointers to the Subscription Manager and Dialog Manager.
+      SipSubscriptionMgr* subMgr = subServer->getSubscriptionMgr(eventName);
+      CPPUNIT_ASSERT(subMgr);
+      SipDialogMgr* dialogMgr = subMgr->getDialogMgr();
+      CPPUNIT_ASSERT(dialogMgr);
+
+      // Create a crude Subscription client
+      OsMsgQ incomingClientMsgQueue;
+      // Register an interest in SUBSCRIBE responses and NOTIFY requests
+      // for this event type
+      userAgent.addMessageObserver(incomingClientMsgQueue,
+                                   SIP_SUBSCRIBE_METHOD,
+                                   FALSE, // no requests
+                                   TRUE, // reponses
+                                   TRUE, // incoming
+                                   FALSE, // no outgoing
+                                   eventName,
+                                   NULL,
+                                   NULL);
+      userAgent.addMessageObserver(incomingClientMsgQueue,
+                                   SIP_NOTIFY_METHOD,
+                                   TRUE, // requests
+                                   FALSE, // not reponses
+                                   TRUE, // incoming
+                                   FALSE, // no outgoing
+                                   eventName,
+                                   NULL,
+                                   NULL);
+
+      // Send a SUBSCRIBE to ourselves
+      SipMessage mwiSubscribeRequest(mwiSubscribe);
+      mwiSubscribeRequest.setSipRequestFirstHeaderLine(SIP_SUBSCRIBE_METHOD, 
+                                                       aor, 
+                                                       SIP_PROTOCOL_VERSION);
+      mwiSubscribeRequest.setContactField(aor_name_addr);
+
+      // Loop through this scenario for a series of response codes.
+      int test_codes[] = { SIP_SERVER_INTERNAL_ERROR_CODE };
+      for (int i = 0; i < sizeof (test_codes) / sizeof (test_codes[0]); i++)
+      {
+         mwiSubscribeRequest.incrementCSeqNumber();
+         CPPUNIT_ASSERT(userAgent.send(mwiSubscribeRequest));
+
+         // We should get a 202 response and a NOTIFY request in the queue
+         // Send the specified response to the NOTIFY.
+         OsTime messageTimeout(1, 0);  // 1 second
+         {
+            const SipMessage* subscribeResponse;
+            const SipMessage* notifyRequest;
+            runListener(incomingClientMsgQueue, userAgent, messageTimeout,
+                        notifyRequest, subscribeResponse,
+                        test_codes[i], TRUE);
+
+            // We should have received a SUBSCRIBE response and a NOTIFY request.
+            CPPUNIT_ASSERT(subscribeResponse);
+            CPPUNIT_ASSERT(notifyRequest);
+
+            CPPUNIT_ASSERT(subscribeResponse->getResponseStatusCode() ==
+                           SIP_ACCEPTED_CODE);
+         }
+
+         // Send a re-SUBSCRIBE
+         mwiSubscribeRequest.incrementCSeqNumber();
+         // Leave the Expires header with the default value.
+
+         CPPUNIT_ASSERT(userAgent.send(mwiSubscribeRequest));
+
+         // We should get a 202 response and a NOTIFY, because the Retry-After
+         // header suppresses the termination of the subscription.
+         {
+            const SipMessage* subscribeResponse;
+            const SipMessage* notifyRequest;
+            runListener(incomingClientMsgQueue, userAgent, messageTimeout,
+                        notifyRequest, subscribeResponse, SIP_OK_CODE);
+
+            // We should have received a SUBSCRIBE response and no NOTIFY request.
+            CPPUNIT_ASSERT(subscribeResponse);
+            CPPUNIT_ASSERT(notifyRequest);
+
+            CPPUNIT_ASSERT(subscribeResponse->getResponseStatusCode() ==
+                           SIP_ACCEPTED_CODE);
          }
       }
 
