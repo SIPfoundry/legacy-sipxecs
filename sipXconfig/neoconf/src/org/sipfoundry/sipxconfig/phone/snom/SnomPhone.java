8039ab7c3c0d49b4ea14f2789c0cb562cad10ca3
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2007-08-09 21:08:07 +0000

    Merged revisions 10657-10663 via svnmerge from https://sipxecs.sipfoundry.org/rep/sipXecs/branches/3.8
    ........
      r10657 | dkrzemin | 2007-08-09 08:58:57 -0400 (Thu, 09 Aug 2007) | 3 lines
      
      XCF-1753 add missing warning message informing about empty search result
      Need German and Italian version.
    ........
      r10659 | dkrzemin | 2007-08-09 10:57:16 -0400 (Thu, 09 Aug 2007) | 2 lines
      
      XCF-1740 SNOM: added support for configuring LDAP parameters
    ........
      r10660 | dkrzemin | 2007-08-09 13:07:06 -0400 (Thu, 09 Aug 2007) | 7 lines
      
      XCF-1402 SNOM: configuring speed dial from sipXconfig UI
      
      superadmin can configure speed dial for all users
      users can configure speeddial for their phone
      speeddial numbers can be overwritten from phone UI
      33 speeddial numbers are supported by phone 
    ........
      r10661 | dkrzemin | 2007-08-09 16:06:23 -0400 (Thu, 09 Aug 2007) | 5 lines
      
      XCF-1403 SNOM: add support for configuring phonebook in sipXconfig UI
      
      all phonebook entries will be added to default (none) category and assigned 'active' identity
      this can be changed by user from phone UI
    ........
      r10662 | dkrzemin | 2007-08-09 16:53:31 -0400 (Thu, 09 Aug 2007) | 2 lines
      
      XCF-1813 If syslog server is defined in sipXconfig add its address to SNOM configuration file.
    ........
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@10664 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/phone/snom/SnomPhone.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/phone/snom/SnomPhone.java
@@ -11,21 +11,34 @@ package org.sipfoundry.sipxconfig.phone.snom;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.sipfoundry.sipxconfig.common.User;
 import org.sipfoundry.sipxconfig.device.DeviceDefaults;
 import org.sipfoundry.sipxconfig.device.DeviceTimeZone;
+import org.sipfoundry.sipxconfig.device.ProfileContext;
 import org.sipfoundry.sipxconfig.phone.Line;
 import org.sipfoundry.sipxconfig.phone.LineInfo;
 import org.sipfoundry.sipxconfig.phone.Phone;
+import org.sipfoundry.sipxconfig.phonebook.PhonebookEntry;
+import org.sipfoundry.sipxconfig.service.UnmanagedService;
 import org.sipfoundry.sipxconfig.setting.Setting;
 import org.sipfoundry.sipxconfig.setting.SettingEntry;
+import org.sipfoundry.sipxconfig.speeddial.Button;
+import org.sipfoundry.sipxconfig.speeddial.SpeedDial;
 
 public class SnomPhone extends Phone {
     public static final String BEAN_ID = "snom";
+
+    private static final int SPEEDDIAL_MAX = 33;
+    private static final int PHONEBOOK_MAX = 100;
 
     private static final String USER_HOST = "line/user_host";
     // suspiciously, no registration server port?
@@ -56,6 +69,13 @@ public class SnomPhone extends Phone {
         SnomLineDefaults defaults = new SnomLineDefaults(getPhoneContext().getPhoneDefaults(),
                 line);
         line.addDefaultBeanSettingHandler(defaults);
+    }
+
+    @Override
+    protected ProfileContext createContext() {
+        SpeedDial speedDial = getPhoneContext().getSpeedDial(this);
+        Collection<PhonebookEntry> phoneBook = getPhoneContext().getPhonebookEntries(this);
+        return new SnomContext(this, speedDial, phoneBook, getModel().getProfileTemplate());
     }
 
     @Override
@@ -145,6 +165,11 @@ public class SnomPhone extends Phone {
             return m_defaults.getNtpServer();
         }
 
+        @SettingEntry(path = "Advanced_Network_Settings/syslog_server")
+        public String getSyslogServer() {
+            return m_defaults.getServer(0, UnmanagedService.SYSLOG);
+        }
+        
         @SettingEntry(path = DST_SETTING)
         public String getDstSetting() {
             DeviceTimeZone zone = m_defaults.getTimeZone();
@@ -247,4 +272,61 @@ public class SnomPhone extends Phone {
     public void restart() {
         sendCheckSyncToFirstLine();
     }
+
+    static class SnomContext extends ProfileContext {
+        private SpeedDial m_speedDial;
+        private Collection<PhonebookEntry> m_phoneBook;
+
+        public SnomContext(SnomPhone device, SpeedDial speedDial,
+                Collection<PhonebookEntry> phoneBook, String profileTemplate) {
+            super(device, profileTemplate);
+            m_speedDial = speedDial;
+            m_phoneBook = trim(phoneBook);
+        }
+
+        public Map<String, Object> getContext() {
+            Map<String, Object> context = super.getContext();
+            context.put("speedDial", getNumbers());
+            context.put("phoneBook", m_phoneBook);
+            return context;
+        }
+
+        Collection<PhonebookEntry> trim(Collection<PhonebookEntry> phoneBook) {
+            if (phoneBook == null) {
+                return Collections.emptyList();
+            }
+            if (phoneBook.size() <= PHONEBOOK_MAX) {
+                return phoneBook;
+            }
+            Predicate keepOnlyN = new Predicate() {
+                private int m_i;
+
+                public boolean evaluate(Object arg0) {
+                    return m_i++ < PHONEBOOK_MAX;
+                }
+            };
+            CollectionUtils.filter(phoneBook, keepOnlyN);
+            return phoneBook;
+        }
+
+        /**
+         * Create SNOM speed dial information
+         * 
+         * @return and array of at most 33 (0-32) phone numbers
+         */
+        String[] getNumbers() {
+            if (m_speedDial == null) {
+                return ArrayUtils.EMPTY_STRING_ARRAY;
+            }
+            List<Button> buttons = m_speedDial.getButtons();
+            int size = Math.min(SPEEDDIAL_MAX, buttons.size());
+            String[] numbers = new String[size];
+            for (int i = 0; i < numbers.length; i++) {
+                numbers[i] = buttons.get(i).getNumber();
+            }
+            return numbers;
+        }
+
+    }
+
 }
--- sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/phone/snom/SnomTest.java
+++ sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/phone/snom/SnomTest.java
@@ -1,15 +1,30 @@
 package org.sipfoundry.sipxconfig.phone.snom;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import junit.framework.TestCase;
 
 import org.apache.commons.io.IOUtils;
+import org.easymock.EasyMock;
+import org.easymock.IMocksControl;
 import org.sipfoundry.sipxconfig.TestHelper;
 import org.sipfoundry.sipxconfig.device.MemoryProfileLocation;
 import org.sipfoundry.sipxconfig.phone.Phone;
+import org.sipfoundry.sipxconfig.phone.PhoneContext;
 import org.sipfoundry.sipxconfig.phone.PhoneModel;
 import org.sipfoundry.sipxconfig.phone.PhoneTestDriver;
+import org.sipfoundry.sipxconfig.phone.snom.SnomPhone.SnomContext;
+import org.sipfoundry.sipxconfig.phonebook.PhonebookEntry;
+import org.sipfoundry.sipxconfig.speeddial.Button;
+import org.sipfoundry.sipxconfig.speeddial.SpeedDial;
 
 public class SnomTest extends TestCase {
+    private final Collection<PhonebookEntry> m_emptyPhonebook = Collections
+            .<PhonebookEntry> emptyList();
 
     public void testGenerateProfiles() throws Exception {
         SnomPhone phone = new SnomPhone();
@@ -17,17 +32,93 @@ public class SnomTest extends TestCase {
         model.setLabel("Snom 360");
         model.setModelDir("snom");
         model.setProfileTemplate("snom/snom.vm");
-        
+
         phone.setModel(model);
         PhoneTestDriver.supplyTestData(phone);
-        
+
         MemoryProfileLocation location = TestHelper.setVelocityProfileGenerator(phone);
-        
+
         phone.generateProfiles(location);
         String expected = IOUtils.toString(this.getClass()
                 .getResourceAsStream("expected-360.cfg"));
 
         assertEquals(expected, location.toString());
+    }
+
+    public void testGenerateProfilesWithSpeedDial() throws Exception {
+        SnomPhone phone = new SnomPhone();
+        PhoneModel model = new PhoneModel("snom");
+        model.setLabel("Snom 360");
+        model.setModelDir("snom");
+        model.setProfileTemplate("snom/snom.vm");
+
+        SpeedDial sp = new SpeedDial();
+        sp.setButtons(Arrays.asList(new Button("Yogi", "yogi@example.com"), new Button(
+                "Daffy Duck", "213")));
+
+        phone.setModel(model);
+
+        IMocksControl phoneContextControl = EasyMock.createNiceControl();
+        PhoneContext phoneContext = phoneContextControl.createMock(PhoneContext.class);
+        PhoneTestDriver.supplyVitalTestData(phoneContextControl, phoneContext, phone);
+        phoneContext.getSpeedDial(phone);
+        phoneContextControl.andReturn(sp).anyTimes();
+        phoneContext.getPhonebookEntries(phone);
+        phoneContextControl.andReturn(m_emptyPhonebook).anyTimes();
+        phoneContextControl.replay();
+
+        MemoryProfileLocation location = TestHelper.setVelocityProfileGenerator(phone);
+
+        phone.setPhoneContext(phoneContext);
+        phone.generateProfiles(location);
+
+        String profile = location.toString();
+
+        assertTrue(profile.contains("speed0!: yogi@example.com"));
+        assertTrue(profile.contains("speed1!: 213"));
+        assertFalse(profile.contains("speed2"));
+
+        phoneContextControl.verify();
+    }
+
+    public void testGenerateProfilesWithPhoneBook() throws Exception {
+        SnomPhone phone = new SnomPhone();
+        PhoneModel model = new PhoneModel("snom");
+        model.setLabel("Snom 360");
+        model.setModelDir("snom");
+        model.setProfileTemplate("snom/snom.vm");
+
+        List< ? extends PhonebookEntry> phonebook = Arrays.asList(new DummyEntry("1"),
+                new DummyEntry("3"), new DummyEntry("5"));
+
+        phone.setModel(model);
+
+        IMocksControl phoneContextControl = EasyMock.createNiceControl();
+        PhoneContext phoneContext = phoneContextControl.createMock(PhoneContext.class);
+        PhoneTestDriver.supplyVitalTestData(phoneContextControl, phoneContext, phone);
+        phoneContext.getPhonebookEntries(phone);
+        phoneContextControl.andReturn(phonebook).anyTimes();
+        phoneContext.getSpeedDial(phone);
+        phoneContextControl.andReturn(null).anyTimes();
+        phoneContextControl.replay();
+
+        MemoryProfileLocation location = TestHelper.setVelocityProfileGenerator(phone);
+
+        phone.setPhoneContext(phoneContext);
+        phone.generateProfiles(location);
+
+        String profile = location.toString();
+
+        assertTrue(profile.contains("tn_0!: first1 last1"));
+        assertTrue(profile.contains("tn_1!: first3 last3"));
+        assertTrue(profile.contains("tn_2!: first5 last5"));
+        assertTrue(profile.contains("tu_0!: number1"));
+        assertTrue(profile.contains("tu_1!: number3"));
+        assertTrue(profile.contains("tu_2!: number5"));
+        assertFalse(profile.contains("tn_3"));
+        assertFalse(profile.contains("tu_3"));
+
+        phoneContextControl.verify();
     }
 
     public void testGetProfileName() {
@@ -36,5 +127,78 @@ public class SnomTest extends TestCase {
         assertEquals("snom.htm", phone.getProfileFilename());
         phone.setSerialNumber("abc123");
         assertEquals("ABC123.htm", phone.getProfileFilename());
+    }
+
+    public void testSnomContextEmpty() {
+        SnomPhone phone = new SnomPhone();
+
+        SnomContext sc = new SnomPhone.SnomContext(phone, null, m_emptyPhonebook, null);
+        String[] numbers = (String[]) sc.getContext().get("speedDial");
+
+        assertEquals(0, numbers.length);
+    }
+
+    public void testSnomContext() {
+        SpeedDial smallSd = createSpeedDial(5);
+        SpeedDial largeSd = createSpeedDial(40);
+
+        SnomPhone phone = new SnomPhone();
+
+        SnomContext sc = new SnomPhone.SnomContext(phone, smallSd, m_emptyPhonebook, null);
+        String[] numbers = (String[]) sc.getContext().get("speedDial");
+        assertEquals(5, numbers.length);
+        for (int i = 0; i < numbers.length; i++) {
+            assertEquals(Integer.toString(i), numbers[i]);
+        }
+
+        sc = new SnomPhone.SnomContext(phone, largeSd, m_emptyPhonebook, null);
+        numbers = (String[]) sc.getContext().get("speedDial");
+        assertEquals(33, numbers.length);
+        for (int i = 0; i < numbers.length; i++) {
+            assertEquals(Integer.toString(i), numbers[i]);
+        }
+    }
+
+    public void testSnomContextLargePhonebook() {
+        Collection<PhonebookEntry> phonebook = new ArrayList<PhonebookEntry>();
+        for (int i = 0; i < 120; i++) {
+            phonebook.add(new DummyEntry(Integer.toString(i)));
+        }
+        SnomPhone phone = new SnomPhone();
+
+        SnomContext sc = new SnomPhone.SnomContext(phone, null, phonebook, null);
+        Collection<?> trimmed = (Collection<?>) sc.getContext().get("phoneBook");
+        assertEquals(100, trimmed.size());
+    }
+
+    private SpeedDial createSpeedDial(int size) {
+        List<Button> buttons = new ArrayList<Button>();
+        for (int i = 0; i < size; i++) {
+            buttons.add(new Button("test", Integer.toString(i)));
+        }
+        SpeedDial sd = new SpeedDial();
+        sd.setButtons(buttons);
+        return sd;
+    }
+
+    static class DummyEntry implements PhonebookEntry {
+        private String m_id;
+
+        public DummyEntry(String id) {
+            m_id = id;
+        }
+
+        public String getFirstName() {
+            return "first" + m_id;
+        }
+
+        public String getLastName() {
+            return "last" + m_id;
+        }
+
+        public String getNumber() {
+            return "number" + m_id;
+        }
+
     }
 }
