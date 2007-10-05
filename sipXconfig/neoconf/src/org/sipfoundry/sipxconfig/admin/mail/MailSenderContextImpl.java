db272f72db725953620513c2649a8ccc9e4c2f54
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2007-10-05 21:26:03 +0000

    XCF-650 set default domain for backup e-mail sender to SIP domain of the system
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@10884 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/mail/MailSenderContextImpl.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/mail/MailSenderContextImpl.java
@@ -17,6 +17,8 @@ import javax.mail.internet.MimeMessage;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.sipfoundry.sipxconfig.domain.DomainManager;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -27,12 +29,13 @@ public class MailSenderContextImpl implements MailSenderContext {
 
     private JavaMailSender m_mailSender;
 
-    public void sendMail(String to, String from, String subject, String body) {        
-        SimpleMailMessage msg1 = new SimpleMailMessage();
-        msg1.setFrom(from);
-        msg1.setTo(to);
-        msg1.setSubject(subject);
-        SimpleMailMessage msg = msg1;
+    private DomainManager m_domainManager;
+
+    public void sendMail(String to, String from, String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(getFullAddress(from));
+        msg.setFrom(getFullAddress(from));
+        msg.setSubject(subject);
         msg.setText(body);
         msg.setSentDate(new Date());
         try {
@@ -40,7 +43,7 @@ public class MailSenderContextImpl implements MailSenderContext {
         } catch (MailException e) {
             LOG.error(e);
         }
-        
+
     }
 
     public void sendMail(String to, String from, String subject, String body, File... files) {
@@ -51,8 +54,8 @@ public class MailSenderContextImpl implements MailSenderContext {
         try {
             MimeMessage msg = m_mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(msg, true);
-            helper.setTo(to);
-            helper.setFrom(from);
+            helper.setTo(getFullAddress(to));
+            helper.setFrom(getFullAddress(from));
             helper.setSubject(subject);
             helper.setText(body);
 
@@ -67,7 +70,26 @@ public class MailSenderContextImpl implements MailSenderContext {
         }
     }
 
+    /**
+     * Appends SIP domain name to addresses that do not have domain names
+     * 
+     * @param address
+     * @return
+     */
+    public String getFullAddress(String address) {
+        if (address.contains("@")) {
+            return address;
+        }
+        return String.format("%s@%s", address, m_domainManager.getDomain().getName());
+    }
+
+    @Required
     public void setMailSender(JavaMailSender mailSender) {
         m_mailSender = mailSender;
+    }
+
+    @Required
+    public void setDomainManager(DomainManager domainManager) {
+        m_domainManager = domainManager;
     }
 }
