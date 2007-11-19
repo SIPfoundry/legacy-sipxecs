df4f9976dcd54f428d005300a7ad7cdb415796ea
kevin <kevin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2007-11-19 18:35:02 +0000

    XCF-1918 Modified database patch to use syntax that is incompatible with Postgres 7.4.17 which runs on RHEL4.  Split the add column command into two commands; one to add the column, the other to set the default value for the column
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@11307 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/etc/database/callgroup-userforward.sql
+++ sipXconfig/neoconf/etc/database/callgroup-userforward.sql
@@ -1,4 +1,5 @@
-alter table call_group add column user_forward boolean default true;
+alter table call_group add column user_forward boolean;
+alter table call_group alter column user_forward set default true;
 
 -- support older postgres (7.4)
 update call_group set user_forward = true;
