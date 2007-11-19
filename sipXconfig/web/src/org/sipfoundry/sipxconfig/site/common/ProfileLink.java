c4d808a5e7eef7cd3fcff5f3f2902cc24492efa7
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2007-11-19 04:38:59 +0000

    XCF-1987  Add UI for SBC devices.
    New screen that shows list of SBCs and allows for adding/removing/editing
    UI support for generating SBC profiles (similar to phone and gateway profiles)
    Fix profile link to support displaying SBC profiles (in addition to gateways and phone profiles)
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@11301 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/common/ProfileLink.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/common/ProfileLink.java
@@ -20,6 +20,8 @@ import org.apache.tapestry.annotations.InjectObject;
 import org.apache.tapestry.annotations.Message;
 import org.apache.tapestry.annotations.Parameter;
 import org.apache.tapestry.web.WebResponse;
+import org.sipfoundry.sipxconfig.admin.dialplan.sbc.SbcDevice;
+import org.sipfoundry.sipxconfig.admin.dialplan.sbc.SbcDeviceManager;
 import org.sipfoundry.sipxconfig.common.UserException;
 import org.sipfoundry.sipxconfig.components.TapestryUtils;
 import org.sipfoundry.sipxconfig.device.Device;
@@ -42,6 +44,9 @@ public abstract class ProfileLink extends BaseComponent {
     @InjectObject(value = "spring:phoneContext")
     public abstract PhoneContext getPhoneContext();
 
+    @InjectObject(value = "spring:sbcDeviceManager")
+    public abstract SbcDeviceManager getSbcDeviceManager();
+
     @Parameter(required = true)
     public abstract Device getDevice();
 
@@ -51,8 +56,8 @@ public abstract class ProfileLink extends BaseComponent {
     @Message(value = "error.generation")
     public abstract String getGenerationErrorMsg();
 
-    public void export(Integer deviceId, boolean isGateway, int profileIndex) {
-        Device device = loadDevice(deviceId, isGateway);
+    public void export(Integer deviceId, boolean isGateway, boolean isSbc, int profileIndex) {
+        Device device = loadDevice(deviceId, isGateway, isSbc);
         Profile[] profileTypes = device.getProfileTypes();
         if (profileIndex < 0 || profileIndex >= profileTypes.length) {
             return;
@@ -77,15 +82,18 @@ public abstract class ProfileLink extends BaseComponent {
     public Object[] getParams() {
         Device device = getDevice();
         boolean isGateway = Gateway.class.isAssignableFrom(device.getClass());
+        boolean isSbc = SbcDevice.class.isAssignableFrom(device.getClass());
         return new Object[] {
-            device.getId(), isGateway, getProfileIndex()
+            device.getId(), isGateway, isSbc, getProfileIndex()
         };
     }
 
-    private Device loadDevice(Integer deviceId, boolean isGateway) {
+    private Device loadDevice(Integer deviceId, boolean isGateway, boolean isSbc) {
         if (isGateway) {
             return getGatewayContext().getGateway(deviceId);
-
+        }
+        if (isSbc) {
+            return getSbcDeviceManager().getSbcDevice(deviceId);
         }
         return getPhoneContext().loadPhone(deviceId);
     }
---
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/sbc/ListSbcDevices.java
@@ -0,0 +1,109 @@
+/*
+ *
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ *
+ */
+package org.sipfoundry.sipxconfig.site.sbc;
+
+import java.util.Collection;
+
+import org.apache.tapestry.IPage;
+import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.annotations.Bean;
+import org.apache.tapestry.annotations.InjectObject;
+import org.apache.tapestry.annotations.Persist;
+import org.apache.tapestry.form.IPropertySelectionModel;
+import org.apache.tapestry.html.BasePage;
+import org.sipfoundry.sipxconfig.admin.dialplan.sbc.SbcDescriptor;
+import org.sipfoundry.sipxconfig.admin.dialplan.sbc.SbcDevice;
+import org.sipfoundry.sipxconfig.admin.dialplan.sbc.SbcDeviceManager;
+import org.sipfoundry.sipxconfig.components.ExtraOptionModelDecorator;
+import org.sipfoundry.sipxconfig.components.ObjectSelectionModel;
+import org.sipfoundry.sipxconfig.components.SelectMap;
+import org.sipfoundry.sipxconfig.components.SipxValidationDelegate;
+import org.sipfoundry.sipxconfig.device.ModelSource;
+import org.sipfoundry.sipxconfig.device.ProfileManager;
+
+/**
+ * List all the gateways, allow adding and deleting gateways
+ */
+public abstract class ListSbcDevices extends BasePage {
+    public static final String PAGE = "sbc/ListSbcDevices";
+
+    @InjectObject(value = "spring:sbcDeviceManager")
+    public abstract SbcDeviceManager getSbcDeviceManager();
+
+    @InjectObject(value = "spring:sbcProfileManager")
+    public abstract ProfileManager getSbcProfileManager();
+
+    @InjectObject(value = "spring:sbcModelSource")
+    public abstract ModelSource<SbcDescriptor> getSbcDeviceModelSource();
+
+    @Bean
+    public abstract SelectMap getSelections();
+
+    @Bean
+    public abstract SipxValidationDelegate getValidator();
+
+    @Persist
+    public abstract void setGenerateProfileIds(Collection<Integer> ids);
+
+    public abstract Collection<Integer> getSbcsToDelete();
+
+    public abstract Collection<Integer> getSbcsToPropagate();
+
+    public abstract SbcDescriptor getSbcDescriptor();
+
+    public abstract SbcDevice getSbc();
+
+    public Collection getAllSelected() {
+        return getSelections().getAllSelected();
+    }
+
+    /**
+     * When user clicks on link to edit a gateway
+     */
+    public IPage formSubmit(IRequestCycle cycle) {
+        Collection selectedRows = getSbcsToDelete();
+        if (selectedRows != null) {
+            getSbcDeviceManager().deleteSbcDevices(selectedRows);
+        }
+        selectedRows = getSbcsToPropagate();
+        if (selectedRows != null) {
+            setGenerateProfileIds(selectedRows);
+        }
+        SbcDescriptor model = getSbcDescriptor();
+        if (model != null) {
+            return EditSbcDevice.getAddPage(cycle, model, this);
+        }
+        return null;
+    }
+
+    public void propagateAll() {
+        Collection gatewayIds = getSbcDeviceManager().getAllSbcDeviceIds();
+        setGenerateProfileIds(gatewayIds);
+    }
+
+    public IPage editSbc(IRequestCycle cycle, Integer sbcId) {
+        return EditSbcDevice.getEditPage(cycle, sbcId, this);
+    }
+
+    public IPropertySelectionModel getSbcDescriptorSelectionModel() {
+        DeviceDescriptorSelectionModel model = new DeviceDescriptorSelectionModel();
+        model.setModelSource(getSbcDeviceModelSource());
+        model.setExtraLabel(getMessages().getMessage("prompt.addNew"));
+        return model;
+    }
+
+    public static class DeviceDescriptorSelectionModel extends ExtraOptionModelDecorator {
+        public void setModelSource(ModelSource modelSource) {
+            ObjectSelectionModel model = new ObjectSelectionModel();
+            model.setCollection(modelSource.getModels());
+            model.setLabelExpression("label");
+            setModel(model);
+        }
+    }
+}
