b12b69abadbbc3563c908895c53450d254a19fe9
kevin <kevin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-02-01 06:50:05 +0000

    XCF-2079 Fix for failing UI unit test.  Updated AudioCodes Mediant gateway name had not been updated in unit test
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@11828 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/test/org/sipfoundry/sipxconfig/site/gateway/GatewaysTestUi.java
+++ sipXconfig/web/test/org/sipfoundry/sipxconfig/site/gateway/GatewaysTestUi.java
@@ -76,7 +76,7 @@ public class GatewaysTestUi extends WebTestCase {
         clickLink("ListGateways");
 
         String[] nonRouteGateways = {"Acme 1000", "AudioCodes MP114 FXO",
-                "AudioCodes MP118 FXO", "AudioCodes Mediant 1000",
+                "AudioCodes MP118 FXO", "AudioCodes Mediant",
                 "AudioCodes TP260", "Unmanaged gateway"};
 
         for (String gatewayType : nonRouteGateways) {
