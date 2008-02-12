2b0facca0b8f77ad8a665aa91d86bb77b9d74e1a
kevin <kevin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-02-12 19:16:19 +0000

    XCF-2235 Fixed rule validator to permit emergency rules with caller sensitive forwarding and no gateways
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@11915 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/dialplan/RuleValidator.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/dialplan/RuleValidator.java
@@ -14,6 +14,7 @@ import org.apache.tapestry.form.ValidationMessages;
 import org.apache.tapestry.form.validator.BaseValidator;
 import org.apache.tapestry.valid.ValidationConstraint;
 import org.apache.tapestry.valid.ValidatorException;
+import org.sipfoundry.sipxconfig.admin.dialplan.EmergencyRule;
 import org.sipfoundry.sipxconfig.admin.dialplan.IDialingRule;
 
 public class RuleValidator extends BaseValidator {
@@ -30,6 +31,15 @@ public class RuleValidator extends BaseValidator {
             return;
         }
         if (!rule.isInternal() && rule.getGateways().size() <= 0) {
+            if (rule instanceof EmergencyRule) {
+                EmergencyRule emergencyRule = (EmergencyRule) rule;
+                
+                // emergency rules without gateways are still valid if
+                // caller sensitive forwarding is enabled
+                if (emergencyRule.getUseMediaServer()) {
+                    return;
+                }
+            }
             // rule is invalid - external rules have to have gateways
             rule.setEnabled(false);
             throw new ValidatorException(getMessage(), ValidationConstraint.CONSISTENCY);
