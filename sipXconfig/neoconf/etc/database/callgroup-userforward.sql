13620c56acff6c0dab87957a0894e1e1a5e59832
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-02-27 16:16:59 +0000

    XCF-2296 make sure that DB patches are compatible with Postgres 7.4
    RHEL/4 is using Postgres 7.4 which does not support adding a table column and setting a default for a column in a single SQL statement.
    Because of that when adding new column we need to write code that looks like this.
    
    alter table t add column c boolean;
    alter table t alter column c set default false;
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@11997 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/etc/database/callgroup-userforward.sql
+++ sipXconfig/neoconf/etc/database/callgroup-userforward.sql
@@ -1,7 +1,7 @@
 alter table call_group add column user_forward boolean;
-alter table call_group alter column user_forward set default true;
 
 -- support older postgres (7.4)
+alter table call_group alter column user_forward set default true;
 update call_group set user_forward = true;
 alter table call_group alter column user_forward set not null;
 
--- sipXconfig/neoconf/etc/database/callgroup-vmail-destination.sql
+++ sipXconfig/neoconf/etc/database/callgroup-vmail-destination.sql
@@ -1,6 +1,7 @@
-alter table call_group add column voicemail_fallback boolean default false;
+alter table call_group add column voicemail_fallback boolean;
 
 -- support older postgres (7.4)
+alter table call_group alter column voicemail_fallback set default false;
 update call_group set voicemail_fallback = false;
 alter table call_group alter column voicemail_fallback set not null;
 
