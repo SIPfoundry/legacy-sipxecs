2868ad48db7e8b8225f83f7225d89d625f2b3b79
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-03-18 18:47:51 +0000

    XCF-2301 Added page to allow admin to add existing phones to a user, rather than have to create new phones.  This is a feature on the User->Phones page.
    Patch from Bogdan Brezoi.  Thanks Bogdan!
    
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12071 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/user/UserPhones.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/user/UserPhones.java
@@ -11,6 +11,8 @@ package org.sipfoundry.sipxconfig.site.user;
 
 import java.util.Collection;
 
+import org.apache.tapestry.IPage;
+import org.apache.tapestry.IRequestCycle;
 import org.apache.tapestry.annotations.Bean;
 import org.apache.tapestry.annotations.InjectObject;
 import org.apache.tapestry.annotations.Persist;
@@ -41,5 +43,12 @@ public abstract class UserPhones extends UserBasePage {
 
     public Collection<Phone> getPhones() {
         return getPhoneContext().getPhonesByUserId(getUserId());
+    }
+    
+    public IPage addExistingPhones(IRequestCycle cycle) {
+        AddExistingPhone page = (AddExistingPhone) cycle.getPage(AddExistingPhone.PAGE);
+        page.setUserId(getUser().getId());
+        page.setReturnPage(PAGE);
+        return page;
     }
 }
---
+++ sipXconfig/web/test/org/sipfoundry/sipxconfig/site/user/AddExistingPhoneTestUi.java
@@ -0,0 +1,82 @@
+/*
+ * 
+ * 
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.  
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ * 
+ * $
+ */
+package org.sipfoundry.sipxconfig.site.user;
+
+import junit.framework.Test;
+import net.sourceforge.jwebunit.WebTestCase;
+
+import org.sipfoundry.sipxconfig.site.SiteTestHelper;
+import org.sipfoundry.sipxconfig.site.phone.PhoneTestHelper;
+
+public class AddExistingPhoneTestUi extends WebTestCase {
+
+    private PhoneTestHelper m_helper;
+    
+    public static Test suite() throws Exception {
+        return SiteTestHelper.webTestSuite(UserPhonesTestUi.class);
+    }
+
+    protected void setUp() throws Exception {
+        getTestContext().setBaseUrl(SiteTestHelper.getBaseUrl());
+        SiteTestHelper.home(getTester(), true);
+        m_helper = new PhoneTestHelper(tester);
+        m_helper.reset();
+    }
+
+    public void testAddExistingPhone() throws Exception {
+        //create new phone
+        m_helper.seedPhone(1);
+        
+        /*clickLink("ManagePhones");
+        SiteTestHelper.assertNoUserError(tester);*/
+        
+        //create new user
+        clickLink("ManageUsers");
+        SiteTestHelper.assertNoUserError(tester);
+        clickLink("addUser");
+        SiteTestHelper.assertNoUserError(tester);
+        setFormElement("userId", "test_user");
+        setFormElement("cp:password", "123");
+        setFormElement("cp:confirmPassword", "123");
+        submit("form:ok");
+        
+        //assign created phone to user
+        SiteTestHelper.assertNoUserError(tester);
+        clickLinkWithText("test_user");
+        SiteTestHelper.assertNoUserError(tester);
+        clickLinkWithText("Phones");
+        SiteTestHelper.assertNoUserError(tester);
+        clickLink("addPhones");
+        SiteTestHelper.assertNoUserError(tester);
+        assertEquals(2,SiteTestHelper.getRowCount(tester, "phone:list"));
+        SiteTestHelper.selectRow(tester, 1, true);
+        submit("select");
+        SiteTestHelper.assertNoUserError(tester);
+        assertEquals(2, SiteTestHelper.getRowCount(tester, "phone:list"));
+        
+        //cleanup phone
+        SiteTestHelper.home(getTester(), true);
+        clickLink("ManagePhones");
+        SiteTestHelper.assertNoUserError(tester);
+        assertEquals(2, SiteTestHelper.getRowCount(tester, "phone:list"));
+        SiteTestHelper.selectRow(tester, 1, true);
+        submit("phone:delete");
+        SiteTestHelper.assertNoUserError(tester);
+        
+        //cleanup user
+        SiteTestHelper.home(getTester(), true);
+        clickLink("ManageUsers");
+        SiteTestHelper.assertNoUserError(tester);
+        assertEquals(2, SiteTestHelper.getRowCount(tester, "user:list"));
+        SiteTestHelper.selectRow(tester, 1, true);
+        submit("user:delete");
+        SiteTestHelper.assertNoUserError(tester);
+    }
+}
