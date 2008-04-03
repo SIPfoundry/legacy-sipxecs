77a5562dd9e9b8f177ef0680785d1d5e30fc3665
mranga <mranga@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-04-03 20:50:21 +0000

    Committing
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12205 ab1d8caa-1f67-47f1-9e81-24633a41865c

---
+++ sipXbridge/src/main/java/org/sipfoundry/sipxbridge/PortRange.java
@@ -0,0 +1,53 @@
+package org.sipfoundry.sipxbridge;
+
+import java.util.Map;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+public class PortRange {
+
+	private int lowerBound;
+
+	private int higherBound;
+
+	/**
+	 * @param lowerBound
+	 *            the lowerBound to set
+	 */
+	public void setLowerBound(int lowerBound) {
+		this.lowerBound = lowerBound;
+	}
+
+	/**
+	 * @return the lowerBound
+	 */
+	public int getLowerBound() {
+		return lowerBound;
+	}
+
+	/**
+	 * @param higherBound
+	 *            the higherBound to set
+	 */
+	public void setHigherBound(int higherBound) {
+		this.higherBound = higherBound;
+	}
+
+	/**
+	 * @return the higherBound
+	 */
+	public int getHigherBound() {
+		return higherBound;
+	}
+
+	public Map toMap() {
+		try {
+			Map retval = PropertyUtils.describe(this);
+			retval.remove("class");
+			return retval;
+		} catch (Exception ex) {
+			throw new RuntimeException("Error generating map", ex);
+		}
+	}
+
+}
