6a6a0ef0b8f9debd9db9b8cc6567724c2238af0b
dworley <dworley@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-04-10 03:01:19 +0000

    Remove duplicated lines of comment.
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12267 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXtackLib/include/net/Url.h
+++ sipXtackLib/include/net/Url.h
@@ -195,17 +195,6 @@ public:
      * If the values have display name components that contain commas (legal), it
      * is safest to do a complete parse of the Urls.  The nextUri parameter provides
      * a straightforward way to accomplish this by returning any part of the input string
-     * that follows a trailing comma after the first url in the string has been parsed:
-     *
-     * @code
-     * UtlString nextUrl;
-     * for 
-     * @endcode
-     *
-     * Many header field are defined to allow multiple comma-separated Url values.
-     * If the values have display name components that contain commas (legal), it
-     * is safest to do a complete parse of the Urls.  The nextUri parameter provides
-     * a straightforward way to accomplish this by returning any part of the input string
      * that follows a trailing comma after the first url in the string has been parsed.
      * In the following example, assume that 'someMethod' fills its argument UtlString
      * with some header field value that may have multiple URL values (such as
