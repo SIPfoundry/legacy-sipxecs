46ff25f6eff3248a38ba54e4bd7d08d81076ddc2
mranga <mranga@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-04-16 03:34:38 +0000

    Forgot to add.
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12322 ab1d8caa-1f67-47f1-9e81-24633a41865c

---
+++ sipXbridge/src/main/java/org/sipfoundry/sipxbridge/CrLfTimerTask.java
@@ -0,0 +1,45 @@
+package org.sipfoundry.sipxbridge;
+
+import gov.nist.javax.sip.ListeningPointExt;
+
+import java.net.InetAddress;
+import java.text.ParseException;
+import java.util.TimerTask;
+
+import javax.sip.SipProvider;
+import javax.sip.message.Request;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Sends out a CRLF to keep the NAT pinhole open.
+ * 
+ * @author mranga
+ *
+ */
+public class CrLfTimerTask extends TimerTask {
+    private static Logger logger = Logger.getLogger(CrLfTimerTask.class);
+    private ItspAccountInfo accountInfo;
+    
+    SipProvider provider;
+    
+    public CrLfTimerTask(SipProvider provider, ItspAccountInfo accountInfo) {
+        this.provider = provider;
+        this.accountInfo  = accountInfo;
+    }
+
+    @Override
+    public void run() {
+        try {
+            ListeningPointExt listeningPoint = (ListeningPointExt) provider.getListeningPoint("udp");
+            listeningPoint.sendHeartbeat
+                (InetAddress.getByName(accountInfo.getOutboundProxy()).getHostAddress(),
+                    Gateway.getSipKeepaliveSeconds());
+        } catch (Exception ex) {
+            logger.error("Unexpected parse exception",ex);
+        }
+        
+
+    }
+
+}
