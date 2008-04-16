98ad6febfe89f4ca94c084c06b0525caf157e59c
mranga <mranga@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-04-16 12:53:15 +0000

    A run script ( temporary )
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12323 ab1d8caa-1f67-47f1-9e81-24633a41865c

---
+++ sipXbridge/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+classpath="lib/log4j-1.2.14.jar:lib/jain-sip-api-1.2.jar:lib/jain-sip-ri-1.2.jar:lib/nist-sdp-1.0.jar:lib/dnsjava-2.0.6.jar:lib/Stun4J.jar:lib/commons-collections-3.2.jar:lib/commons-digester-1.8.jar:lib/commons-beanutils.jar:lib/commons-logging-1.1.1.jar:lib/commons-logging-api-1.1.1.jar:lib/xmlrpc-common-3.1.jar:lib/xmlrpc-server-3.1.jar:lib/xmlrpc-client-3.1.jar:lib/ws-commons-util-1.0.2.jar"
+
+javac -classpath $classpath -sourcepath src/main/java -d classes -g:source src/main/java/org/sipfoundry/sipxbridge/*.java
+
+java -classpath "$classpath:./classes" org.sipfoundry.sipxbridge.Gateway
