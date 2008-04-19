185c07eeeb30af2770a66f4b281a93e8c6e9e3be
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-04-19 13:34:46 +0000

    XCF-2371 initial version of ciscoplus plugin
    Supports 7906,7911,7941,7945,7961,7965,7970,7975 phones
    
    Patch from Sen Heng - thanks!
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12374 ab1d8caa-1f67-47f1-9e81-24633a41865c

---
+++ sipXconfig/plugins/ciscoplus/src/org/sipfoundry/sipxconfig/phone/ciscoplus/CiscoplusModel.java
@@ -0,0 +1,27 @@
+/*
+ *
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ *
+ * $
+ */
+package org.sipfoundry.sipxconfig.phone.ciscoplus;
+
+import org.sipfoundry.sipxconfig.phone.PhoneModel;
+
+/**
+ * Static differences in ciscoplus models
+ */
+public final class CiscoplusModel extends PhoneModel {
+
+
+    public CiscoplusModel() {
+    }
+
+    public CiscoplusModel(String beanId) {
+        super(beanId);
+    }
+
+}
---
+++ sipXconfig/plugins/ciscoplus/test/org/sipfoundry/sipxconfig/phone/ciscoplus/CiscoplusPhoneTest.java
@@ -0,0 +1,43 @@
+/*
+ *
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ *
+ * $
+ */
+package org.sipfoundry.sipxconfig.phone.ciscoplus;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.io.IOUtils;
+import org.sipfoundry.sipxconfig.TestHelper;
+import org.sipfoundry.sipxconfig.device.MemoryProfileLocation;
+import org.sipfoundry.sipxconfig.phone.PhoneTestDriver;
+
+public class CiscoplusPhoneTest extends TestCase {
+
+    private CiscoplusPhone m_phone;
+
+    protected void setUp() {
+        CiscoplusModel model = new CiscoplusModel("ciscoplusPhone");
+        model.setMaxLineCount(3);
+        model.setModelId("ciscoplus7961G");
+        model.setProfileTemplate("ciscoplusPhone/config.vm");
+        m_phone = new CiscoplusPhone();
+        m_phone.setModel(model);
+        PhoneTestDriver.supplyTestData(m_phone);
+    }
+
+    public void testGetSettings() {
+        assertNotNull(m_phone.getSettings());
+    }
+
+    public void testGenerate7960Profiles() throws Exception {
+        MemoryProfileLocation location = TestHelper.setVelocityProfileGenerator(m_phone);
+        m_phone.generateProfiles(location);
+        String expected = IOUtils.toString(this.getClass().getResourceAsStream("SEP111111111111.cnf.xml"));
+        assertEquals(expected, location.toString());
+    }
+}
