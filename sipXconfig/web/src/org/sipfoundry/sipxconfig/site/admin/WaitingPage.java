eb0cbd20cd8dee3d3c4cd663c4bb11994f20e426
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-04-21 12:34:52 +0000

    XCF-2453 fix WaitingPage after Tapestry upgrade
    - Call generated async javascript function on window.onload since Tapestry is not able to do this
    automatically. This function has the efect of calling java WaitingPage onLoad function that calls
    the restore process.
    - Align WaitingPage with the latest sipXconfig layout changes
    - Remove unecessary code
    
    Patch from Mircea Carasel - thanks!
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12382 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/WaitingPage.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/WaitingPage.java
@@ -12,12 +12,9 @@ package org.sipfoundry.sipxconfig.site.admin;
 import org.apache.tapestry.IAsset;
 import org.apache.tapestry.annotations.Asset;
 import org.apache.tapestry.annotations.EventListener;
-import org.apache.tapestry.annotations.InitialValue;
 import org.apache.tapestry.annotations.InjectObject;
 import org.apache.tapestry.annotations.Persist;
 import org.apache.tapestry.event.PageEndRenderListener;
-import org.apache.tapestry.event.PageEvent;
-import org.apache.tapestry.web.WebRequest;
 import org.sipfoundry.sipxconfig.admin.WaitingListener;
 import org.sipfoundry.sipxconfig.components.TapestryContext;
 import org.sipfoundry.sipxconfig.site.user_portal.UserBasePage;
@@ -39,27 +36,15 @@ public abstract class WaitingPage extends UserBasePage implements PageEndRenderL
     @InjectObject(value = "spring:tapestry")
     public abstract TapestryContext getTapestry();
 
-    @InjectObject(value = "infrastructure:request")
-    public abstract WebRequest getWebRequest();
-
-    @InitialValue(value = "false")
-    public abstract boolean isPageRendered();
-
-    public abstract void setPageRendered(boolean pageRendered);
-
     /**
      * This method is called immediately after the waiting page (WaitingPage.html) is loaded in
      * the browser using Tacos mechanism
      */
-    @EventListener(elements = "", events = "onLoad")
+    @EventListener(elements = "body", events = "onload")
     public void onLoad() {
         WaitingListener waitingListener = getWaitingListener();
         if (waitingListener != null) {
             waitingListener.afterResponseSent();
         }
-    }
-
-    public void pageEndRender(PageEvent e) {
-        setPageRendered(true);
     }
 }
