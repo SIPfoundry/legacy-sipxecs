9464e908d8e36af74b3b8334113059cf605e5696
dworley <dworley@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-05-08 21:26:13 +0000

    Hyphenate the phrase "music-on-hold" when it is used as an adjective or the aspects of the feature.
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12593 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/parkorbit/MohRule.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/parkorbit/MohRule.java
@@ -16,6 +16,6 @@ public class MohRule extends InternalForwardRule {
     public MohRule() {
         super("~~mh~", "<sip:moh@${ORBIT_SERVER_SIP_SRV_OR_HOSTPORT}>");
         setName("Music on Hold");
-        setDescription("Forward music on hold calls to Park Server");
+        setDescription("Forward music-on-hold calls to Park Server");
     }
 }
