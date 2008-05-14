eefc34f0dce34cde326011356c4366bd4a997882
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-05-14 21:15:57 +0000

    Freeswitch API XML/RPC proxy defined properly.
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12651 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/conference/ConferenceBridgeProvisioningImpl.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/conference/ConferenceBridgeProvisioningImpl.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.sipfoundry.sipxconfig.admin.commserver.SipxReplicationContext;
 import org.sipfoundry.sipxconfig.admin.commserver.imdb.DataSet;
 import org.sipfoundry.sipxconfig.job.JobContext;
@@ -23,100 +22,93 @@ import org.sipfoundry.sipxconfig.setting.Setting;
 import org.sipfoundry.sipxconfig.setting.SettingFilter;
 import org.sipfoundry.sipxconfig.setting.SettingValue;
 import org.sipfoundry.sipxconfig.setting.SettingValueImpl;
-import org.sipfoundry.sipxconfig.xmlrpc.XmlRpcClientInterceptor;
-
-import org.springframework.aop.framework.ProxyFactory;
+import org.sipfoundry.sipxconfig.xmlrpc.ApiProvider;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-public class ConferenceBridgeProvisioningImpl extends HibernateDaoSupport implements
-        ConferenceBridgeProvisioning {
+public class ConferenceBridgeProvisioningImpl extends HibernateDaoSupport implements ConferenceBridgeProvisioning {
 
     protected static final Log LOG = LogFactory.getLog(ConferenceBridgeProvisioningImpl.class);
     private SipxReplicationContext m_sipxReplicationContext;
     private JobContext m_jobContext;
+    private ApiProvider<FreeswitchApi> m_freeswitchApiProvider;
 
-    public interface FreeSWITCHFunctions {
-        void freeswitch_api(String funcion, String params);
-    }
-    
     public void deploy(Serializable bridgeId) {
         Bridge bridge = (Bridge) getHibernateTemplate().load(Bridge.class, bridgeId);
         List conferences = getHibernateTemplate().loadAll(Conference.class);
         generateAdmissionData(bridge, conferences);
-        generateConfigurationData(bridge, conferences);        
-        m_sipxReplicationContext.publishEvent(new ConferenceConfigReplicatedEvent(this, bridge.getServiceUri()));
+        generateConfigurationData(bridge, conferences);
+        ApplicationEvent event = new ConferenceConfigReplicatedEvent(this, bridge.getServiceUri());
+        m_sipxReplicationContext.publishEvent(event);
         m_sipxReplicationContext.generate(DataSet.ALIAS);
     }
-    
-    public void onApplicationEvent(ApplicationEvent event) {
-        if (event instanceof ConferenceConfigReplicatedEvent) {
-            boolean success = false;
-            Serializable jobId = m_jobContext.schedule("FreeSWITCH reload configuration");
-            try {
-                m_jobContext.start(jobId);
-                String serviceUrl = ((ConferenceConfigReplicatedEvent) event).getServiceUrl();
-                XmlRpcClientInterceptor interceptor = new XmlRpcClientInterceptor();
-                interceptor.setServiceInterface(FreeSWITCHFunctions.class);
-                interceptor.setServiceUrl(serviceUrl);
-                interceptor.afterPropertiesSet();
 
-                FreeSWITCHFunctions proxy = (FreeSWITCHFunctions) ProxyFactory.getProxy(FreeSWITCHFunctions.class,
-                    interceptor);
-                
-                requestConfigUpdate(proxy);                        
-                success = true;
-            } finally {
-                if (success) {
-                    m_jobContext.success(jobId);
-                } else {
-                    m_jobContext.failure(jobId, null, null);
-                }                
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (!(event instanceof ConferenceConfigReplicatedEvent)) {
+            return;
+        }
+        boolean success = false;
+        Serializable jobId = m_jobContext.schedule("FreeSWITCH reload configuration");
+        try {
+            ConferenceConfigReplicatedEvent e = (ConferenceConfigReplicatedEvent) event;
+            m_jobContext.start(jobId);
+            FreeswitchApi api = m_freeswitchApiProvider.getApi(e.getServiceUrl());
+            api.reloadxml();
+            success = true;
+        } finally {
+            if (success) {
+                m_jobContext.success(jobId);
+            } else {
+                m_jobContext.failure(jobId, null, null);
             }
         }
     }
-    
 
     void generateAdmissionData(Bridge bridge, List conferences) {
         ConferenceAdmission admission = new ConferenceAdmission();
         admission.generate(conferences);
         m_sipxReplicationContext.replicate(admission);
     }
-    
+
     void generateConfigurationData(Bridge bridge, List conferences) {
         ConferenceConfiguration configuration = new ConferenceConfiguration(bridge);
         configuration.generate(conferences);
         m_sipxReplicationContext.replicate(configuration);
     }
-    
-    void requestConfigUpdate(FreeSWITCHFunctions proxy) {                
-        proxy.freeswitch_api("reloadxml", "");
-    }
 
+    @Required
     public void setSipxReplicationContext(SipxReplicationContext sipxReplicationContext) {
         m_sipxReplicationContext = sipxReplicationContext;
     }
-    
+
+    @Required
     public void setJobContext(JobContext jobContext) {
         m_jobContext = jobContext;
-    }    
+    }
+    
+    @Required
+    public void setFreeswitchApiProvider(ApiProvider<FreeswitchApi> freeswitchApiProvider) {
+        m_freeswitchApiProvider = freeswitchApiProvider;
+    }
 
     public static class BostonBridgeFilter implements SettingFilter {
         private static final String PREFIX = "fs-conf";
+
         public boolean acceptSetting(Setting root_, Setting setting) {
             String profileName = setting.getProfileName();
             return profileName.startsWith(PREFIX);
         }
     }
-    
+
     public static class ConferenceProfileName implements ProfileNameHandler {
         private static final char SEPARATOR = '.';
         private final String m_conferenceName;
 
         ConferenceProfileName(String conferenceName) {
-            m_conferenceName = SEPARATOR + conferenceName;            
+            m_conferenceName = SEPARATOR + conferenceName;
         }
-        
+
         public SettingValue getProfileName(Setting setting) {
             String profileName = setting.getProfileName();
             StringBuffer buffer = new StringBuffer(profileName);
@@ -126,8 +118,8 @@ public class ConferenceBridgeProvisioningImpl extends HibernateDaoSupport implem
             } else {
                 buffer.append(m_conferenceName);
             }
-            
-            return new SettingValueImpl(buffer.toString());                        
+
+            return new SettingValueImpl(buffer.toString());
         }
     }
 }
---
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/conference/FreeswitchApi.java
@@ -0,0 +1,17 @@
+/*
+ *
+ *
+ * Copyright (C) 2008 Pingtel Corp., certain elements licensed under a Contributor Agreement.  
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ * 
+ *
+ */
+package org.sipfoundry.sipxconfig.conference;
+
+/**
+ * XML/RPC API provided by Freeswitch
+ */
+public interface FreeswitchApi {
+    void reloadxml();
+}
