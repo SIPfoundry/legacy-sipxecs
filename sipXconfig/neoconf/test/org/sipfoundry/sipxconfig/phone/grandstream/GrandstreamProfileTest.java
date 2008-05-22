e7c137dfb86c881bd685bcc50549bc1022794038
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-05-22 20:43:54 +0000

    XCF-2472 Grandstream: update profile tests
    Newly generated profiles are working properly according to Martin.
    I updated the unit tests and I am taking this into mainline conditionally so that it can be properly
    tested against real phones.
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12723 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/phone/grandstream/GrandstreamProfileTest.java
+++ sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/phone/grandstream/GrandstreamProfileTest.java
@@ -25,35 +25,35 @@ public class GrandstreamProfileTest extends TestCase {
     PhoneTestDriver tester;
 
     public void testHandytone() throws Exception {
-        assertProfile("gsHt496", "expected-ht.cfg");
+        assertProfile("gsHt496", "expected-ht496.cfg");
     }
 
     public void testBudgetone10x() throws Exception {
-        assertProfile("gsPhoneBt100", "expected-gsbt.cfg");
+        assertProfile("gsPhoneBt100", "expected-gsbt100.cfg");
     }
 
     public void testBudgetone200() throws Exception {
-        assertProfile("gsPhoneBt200", "expected-gsbt.cfg");
+        assertProfile("gsPhoneBt200", "expected-gsbt200.cfg");
     }
 
     public void testGxp1200() throws Exception {
-        assertProfile("gsPhoneGxp1200", "expected-gxp.cfg");
+        assertProfile("gsPhoneGxp1200", "expected-gxp1200.cfg");
     }
 
     public void testGxp2000() throws Exception {
-        assertProfile("gsPhoneGxp2000", "expected-gxp.cfg");
+        assertProfile("gsPhoneGxp2000", "expected-gxp2000.cfg");
     }
 
     public void testGxp2010() throws Exception {
-        assertProfile("gsPhoneGxp2010", "expected-gxp.cfg");
+        assertProfile("gsPhoneGxp2010", "expected-gxp2010.cfg");
     }
 
     public void testGxp2020() throws Exception {
-        assertProfile("gsPhoneGxp2020", "expected-gxp.cfg");
+        assertProfile("gsPhoneGxp2020", "expected-gxp2020.cfg");
     }
 
     public void testGxv3000() throws Exception {
-        assertProfile("gsPhoneGxv3000", "expected-gxv.cfg");
+        assertProfile("gsPhoneGxv3000", "expected-gxv3000.cfg");
     }
 
     protected void assertProfile(String modelId, String expectedProfile) throws IOException {
