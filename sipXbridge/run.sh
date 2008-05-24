61b49b7b45a83bbee4c639fc62d27d5a0c997235
mranga <mranga@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-05-24 04:37:27 +0000

    Shell script to start things up from command line ( for development only ).
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12743 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXbridge/run.sh
+++ sipXbridge/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -f /var/log/sipxpbx/*.log
-classpath="lib/log4j-1.2.14.jar:lib/jain-sip-sdp-1.2.jar:lib/dnsjava-2.0.6.jar:lib/Stun4J.jar:lib/commons-collections-3.2.jar:lib/commons-digester-1.8.jar:lib/commons-beanutils.jar:lib/commons-logging-1.1.1.jar:lib/commons-logging-api-1.1.1.jar:lib/xmlrpc-common-3.1.jar:lib/xmlrpc-server-3.1.jar:lib/xmlrpc-client-3.1.jar:lib/ws-commons-util-1.0.2.jar"
+classpath="lib/log4j-1.2.14.jar:lib/jain-sip-sdp-1.2.70.jar:lib/dnsjava-2.0.6.jar:lib/Stun4J.jar:lib/commons-collections-3.2.jar:lib/commons-digester-1.8.jar:lib/commons-beanutils.jar:lib/commons-logging-1.1.1.jar:lib/commons-logging-api-1.1.1.jar:lib/xmlrpc-common-3.1.jar:lib/xmlrpc-server-3.1.jar:lib/xmlrpc-client-3.1.jar:lib/ws-commons-util-1.0.2.jar"
 
 javac -classpath $classpath -sourcepath src/main/java -d classes -g:source,lines,vars src/main/java/org/sipfoundry/sipxbridge/*.java
 
