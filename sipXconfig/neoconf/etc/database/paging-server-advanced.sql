b62e21551073ee9cb55947808953f8db5e275cba
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-05-27 21:38:32 +0000

    XCF-2594 fix paging-server-advanced patch
    add the column, update its value, then set NOT NULL constraint
    we have to do it in 3 steps to make old Postgres happy
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12764 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/etc/database/paging-server-advanced.sql
+++ sipXconfig/neoconf/etc/database/paging-server-advanced.sql
@@ -1,2 +1,7 @@
 -- alter paging server table
-alter table paging_server add column sip_trace_level character varying(255) not null;
+ALTER TABLE paging_server ADD COLUMN sip_trace_level CHARACTER VARYING(255);
+
+UPDATE paging_server SET sip_trace_level='NONE';
+
+ALTER TABLE paging_server ALTER COLUMN sip_trace_level SET NOT NULL;
+
