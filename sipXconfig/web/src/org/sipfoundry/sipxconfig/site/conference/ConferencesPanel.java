c6db984dafdf8bff89f0d57a7b8114854a5c2a15
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-06-15 16:09:54 +0000

    XCF-2602 Conference panel updates: removing jwc file.
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12893 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/conference/ConferencesPanel.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/conference/ConferencesPanel.java
@@ -11,11 +11,40 @@ package org.sipfoundry.sipxconfig.site.conference;
 
 import java.util.Collection;
 
+import org.apache.tapestry.IActionListener;
+import org.apache.tapestry.annotations.Bean;
+import org.apache.tapestry.annotations.ComponentClass;
+import org.apache.tapestry.annotations.InjectObject;
+import org.apache.tapestry.annotations.Parameter;
+import org.sipfoundry.sipxconfig.components.SelectMap;
 import org.sipfoundry.sipxconfig.components.TablePanel;
+import org.sipfoundry.sipxconfig.conference.Conference;
 import org.sipfoundry.sipxconfig.conference.ConferenceBridgeContext;
 
+@ComponentClass(allowBody = false, allowInformalParameters = false)
 public abstract class ConferencesPanel extends TablePanel {
+
+    @InjectObject("spring:conferenceBridgeContext")
     public abstract ConferenceBridgeContext getConferenceBridgeContext();
+
+    @Bean
+    public abstract SelectMap getSelections();
+
+    @Parameter(required = true)
+    public abstract IActionListener getAddListener();
+
+    @Parameter(required = true)
+    public abstract IActionListener getSelectListener();
+
+    @Parameter(required = true)
+    public abstract Collection<Conference> getConferences();
+
+    @Parameter(required = false)
+    public abstract boolean isChanged();
+
+    public abstract Collection getRowsToDelete();
+
+    public abstract Conference getCurrentRow();
 
     protected void removeRows(Collection selectedRows) {
         getConferenceBridgeContext().removeConferences(selectedRows);
