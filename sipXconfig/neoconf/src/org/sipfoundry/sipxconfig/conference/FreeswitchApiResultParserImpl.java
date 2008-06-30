25480948d84234933de7d83a326be30cfd994cbe
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-06-30 17:56:26 +0000

    XCF-2650 minor changes in freeswitch result parser
    - handle empty strings (which in theory should not happen but stack traces looked suspicious)
    - skip user lines that do not conform to the parsed format: switch to Scanner class for easier error
      handling
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@12996 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/conference/FreeswitchApiResultParserImpl.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/conference/FreeswitchApiResultParserImpl.java
@@ -9,14 +9,15 @@
  */
 package org.sipfoundry.sipxconfig.conference;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -28,8 +29,6 @@ public class FreeswitchApiResultParserImpl implements FreeswitchApiResultParser 
 
     private static final String EMPTY_STRING = "No active conferences.";
 
-    private static final String MEMBER_DELIMITER = ";";
-
     private static final Pattern CONFERENCE_NOT_FOUND_PATTERN = Pattern.compile("Conference (.+) not found\\n");
 
     private static final Pattern CONFERENCE_NAME_PATTERN = Pattern
@@ -40,10 +39,13 @@ public class FreeswitchApiResultParserImpl implements FreeswitchApiResultParser 
 
     /**
      * Verifies that a member action was completed successfully and that the target member exists.
-     * 
+     *
      * @throws NoSuchMemberException if the member does not exist
      */
     public boolean verifyMemberAction(String resultString, ActiveConferenceMember member) {
+        if (StringUtils.isBlank(resultString)) {
+            throw new NoSuchMemberException(member);
+        }
         if (INVALID_MEMBER_PATTERN.matcher(resultString).matches()) {
             throw new NoSuchMemberException(member);
         }
@@ -53,10 +55,13 @@ public class FreeswitchApiResultParserImpl implements FreeswitchApiResultParser 
     /**
      * Verifies that a conference action was completed successfully and that the target conference
      * exists.
-     * 
+     *
      * @throws NoSuchConferenceException if the member does not exist
      */
     public boolean verifyConferenceAction(String resultString, Conference conference) {
+        if (StringUtils.isBlank(resultString)) {
+            throw new NoSuchConferenceException(conference);
+        }
         if (CONFERENCE_NOT_FOUND_PATTERN.matcher(resultString).matches()) {
             throw new NoSuchConferenceException(conference);
         }
@@ -65,20 +70,21 @@ public class FreeswitchApiResultParserImpl implements FreeswitchApiResultParser 
 
     /**
      * Parses a result string and determines the total number of active conferences.
-     * 
+     *
      * @param resultString the string returned from the FreeSWITCH API call
      * @return the number of active conferences reported by FreeSWITCH.
      */
     public int getActiveConferenceCount(String resultString) {
+        if (StringUtils.isBlank(resultString)) {
+            return 0;
+        }
         if (resultString.equals(EMPTY_STRING)) {
             return 0;
         }
-        Matcher matcher = CONFERENCE_NAME_PATTERN.matcher(resultString);
-        boolean found = matcher.find();
         int count = 0;
-        while (found) {
+        Matcher matcher = CONFERENCE_NAME_PATTERN.matcher(resultString);
+        while (matcher.find()) {
             count++;
-            found = matcher.find(matcher.end());
         }
 
         return count;
@@ -87,23 +93,25 @@ public class FreeswitchApiResultParserImpl implements FreeswitchApiResultParser 
     /**
      * Parses a result string and creates a list of ActiveConference objects representing each
      * conference.
-     * 
+     *
      * @param resultString the string returned from the FreeSWITCH API call
      * @return a List of ActiveConference objects
      */
     public List<ActiveConference> getActiveConferences(String resultString) {
+        if (StringUtils.isBlank(resultString)) {
+            return Collections.emptyList();
+        }
+        if (resultString.equals(EMPTY_STRING)) {
+            return Collections.emptyList();
+        }
         List<ActiveConference> activeConferences = new ArrayList<ActiveConference>();
 
-        if (!resultString.equals(EMPTY_STRING)) {
-            Matcher matcher = CONFERENCE_NAME_PATTERN.matcher(resultString);
-            boolean found = matcher.find();
-            while (found) {
-                String conferenceName = matcher.group(1);
-                int members = Integer.parseInt(matcher.group(2));
-                boolean locked = (matcher.group(3) != null);
-                activeConferences.add(new ActiveConference(conferenceName, members, locked));
-                found = matcher.find(matcher.end());
-            }
+        Matcher matcher = CONFERENCE_NAME_PATTERN.matcher(resultString);
+        while (matcher.find()) {
+            String conferenceName = matcher.group(1);
+            int members = Integer.parseInt(matcher.group(2));
+            boolean locked = (matcher.group(3) != null);
+            activeConferences.add(new ActiveConference(conferenceName, members, locked));
         }
 
         return activeConferences;
@@ -112,38 +120,44 @@ public class FreeswitchApiResultParserImpl implements FreeswitchApiResultParser 
     public List<ActiveConferenceMember> getConferenceMembers(String resultString) {
         List<ActiveConferenceMember> members = new ArrayList<ActiveConferenceMember>();
 
-        try {
-            BufferedReader lineReader = new BufferedReader(new StringReader(resultString));
-            String currentLine = "";
-            while ((currentLine = lineReader.readLine()) != null) {
-                String[] fields = currentLine.split(MEMBER_DELIMITER);
-                int fieldIndex = 0;
-
-                ActiveConferenceMember member = new ActiveConferenceMember();
-                member.setId(Integer.parseInt(fields[fieldIndex++]));
-
-                String sipAddress = fields[fieldIndex++].split("/")[2];
-                member.setUuid(fields[fieldIndex++]);
-                String callerIdName = fields[fieldIndex++];
-                fieldIndex++; // skip caller ID number
-
-                String permissions = fields[fieldIndex++];
-                member.setCanHear(permissions.contains("hear"));
-                member.setCanSpeak(permissions.contains("speak"));
-
-                member.setName(callerIdName + " (" + sipAddress + ")");
-
-                member.setVolumeIn(Integer.parseInt(fields[fieldIndex++]));
-                member.setVolumeOut(Integer.parseInt(fields[fieldIndex++]));
-                member.setEnergyLevel(Integer.parseInt(fields[fieldIndex++]));
-
+        Scanner scanner = new Scanner(resultString);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            try {
+                ActiveConferenceMember member = parseConferenceMember(line);
                 members.add(member);
+            } catch (NoSuchElementException e) {
+                LOG.error("Skipping conference line:" + line);
             }
-        } catch (IOException ioe) {
-            // This should never happen. A StringReader shouldn't have an IOException
-            LOG.error("Error reading line from StringReader");
         }
 
         return members;
+    }
+
+    private ActiveConferenceMember parseConferenceMember(String line) {
+        ActiveConferenceMember member = new ActiveConferenceMember();
+
+        Scanner scan = new Scanner(line);
+        scan.useDelimiter(";");
+
+        member.setId(scan.nextInt());
+
+        String sipAddress = scan.next().split("/")[2];
+
+        member.setUuid(scan.next());
+
+        String callerIdName = scan.next();
+        scan.next(); // skip caller ID number
+
+        String permissions = scan.next();
+        member.setCanHear(permissions.contains("hear"));
+        member.setCanSpeak(permissions.contains("speak"));
+
+        member.setName(callerIdName + " (" + sipAddress + ")");
+
+        member.setVolumeIn(scan.nextInt());
+        member.setVolumeOut(scan.nextInt());
+        member.setEnergyLevel(scan.nextInt());
+        return member;
     }
 }
--- sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/conference/FreeswitchApiResultParserTest.java
+++ sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/conference/FreeswitchApiResultParserTest.java
@@ -1,3 +1,12 @@
+/*
+ *
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ *
+ * $
+ */
 package org.sipfoundry.sipxconfig.conference;
 
 import java.util.List;
@@ -14,7 +23,7 @@ public class FreeswitchApiResultParserTest {
 
     public static junit.framework.Test suite() {
         return new JUnit4TestAdapter(FreeswitchApiResultParserTest.class);
-      }
+    }
 
     @Before
     public void createParser() {
@@ -23,37 +32,54 @@ public class FreeswitchApiResultParserTest {
 
     @Test
     public void testGetActiveConferenceCount() {
+        Assert.assertEquals(0, m_parser.getActiveConferenceCount(null));
+
         String resultString = "No active conferences.";
         Assert.assertEquals(0, m_parser.getActiveConferenceCount(resultString));
 
-        resultString =
-            "Conference myconf2 (1 member)\n" +
-            "4;sofia/eng.bluesocket.com/202@192.168.100.233;c6c34057-3844-43de-abbd-816fc64e1926;cardassia;202;hear|speak|floor;0;0;300\n" +
-            "Conference myconf1 (2 members)\n" +
-            "2;sofia/eng.bluesocket.com/201@192.168.100.233;f69d2b1f-4841-40a4-8e0f-847d1aeef2f0;201;201;hear|speak;0;0;300\n" +
-            "1;sofia/eng.bluesocket.com/200@192.168.100.233;a5b6cdbe-7cbf-48a7-a52d-98b871eb2491;Joe Attardi;200;hear|speak|floor;0;0;300\n";
+        Assert.assertEquals(0, m_parser.getActiveConferenceCount("Something else..."));
+
+        resultString = "Conference myconf2 (1 member)\n"
+                + "4;sofia/eng.bluesocket.com/202@192.168.100.233;c6c34057-3844-43de-abbd-816fc64e1926;cardassia;202;hear|speak|floor;0;0;300\n"
+                + "Conference myconf1 (2 members)\n"
+                + "2;sofia/eng.bluesocket.com/201@192.168.100.233;f69d2b1f-4841-40a4-8e0f-847d1aeef2f0;201;201;hear|speak;0;0;300\n"
+                + "1;sofia/eng.bluesocket.com/200@192.168.100.233;a5b6cdbe-7cbf-48a7-a52d-98b871eb2491;Joe Attardi;200;hear|speak|floor;0;0;300\n";
 
         Assert.assertEquals(2, m_parser.getActiveConferenceCount(resultString));
     }
 
     @Test
     public void testGetActiveConferences() {
-        String resultString =
-            "Conference myconf3 (1 member)\n" +
-            "4;sofia/eng.bluesocket.com/202@192.168.100.233;c6c34057-3844-43de-abbd-816fc64e1926;cardassia;202;hear|speak|floor;0;0;300\n" +
-            "Conference myconf2 (2 members)\n" +
-            "2;sofia/eng.bluesocket.com/201@192.168.100.233;f69d2b1f-4841-40a4-8e0f-847d1aeef2f0;201;201;hear|speak;0;0;300\n" +
-            "1;sofia/eng.bluesocket.com/200@192.168.100.233;a5b6cdbe-7cbf-48a7-a52d-98b871eb2491;Joe Attardi;200;hear|speak|floor;0;0;300\n" +
-            "Conference myconf1 (1 member locked)\n" +
-            "1;sofia/eng.bluesocket.com/200@192.168.100.233;a5b6cdbe-7cbf-48a7-a52d-98b871eb2491;Joe Attardi;200;hear|speak|floor;0;0;300\n";
+        List<ActiveConference> activeConferences = m_parser.getActiveConferences(null);
+        Assert.assertEquals(0, activeConferences.size());
 
-        List<ActiveConference> activeConferences = m_parser.getActiveConferences(resultString);
+        activeConferences = m_parser.getActiveConferences("No active conferences.");
+        Assert.assertEquals(0, activeConferences.size());
+
+        activeConferences = m_parser.getActiveConferences("Something else...");
+        Assert.assertEquals(0, activeConferences.size());
+
+        String resultString = "Conference myconf3 (1 member)\n"
+                + "4;sofia/eng.bluesocket.com/202@192.168.100.233;c6c34057-3844-43de-abbd-816fc64e1926;cardassia;202;hear|speak|floor;0;0;300\n"
+                + "Conference myconf2 (2 members)\n"
+                + "2;sofia/eng.bluesocket.com/201@192.168.100.233;f69d2b1f-4841-40a4-8e0f-847d1aeef2f0;201;201;hear|speak;0;0;300\n"
+                + "1;sofia/eng.bluesocket.com/200@192.168.100.233;a5b6cdbe-7cbf-48a7-a52d-98b871eb2491;Joe Attardi;200;hear|speak|floor;0;0;300\n"
+                + "Conference myconf1 (1 member locked)\n"
+                + "1;sofia/eng.bluesocket.com/200@192.168.100.233;a5b6cdbe-7cbf-48a7-a52d-98b871eb2491;Joe Attardi;200;hear|speak|floor;0;0;300\n";
+
+        activeConferences = m_parser.getActiveConferences(resultString);
 
         Assert.assertEquals(3, activeConferences.size());
 
-        String[] expectedConferenceNames = {"myconf3", "myconf2", "myconf1"};
-        int[] expectedMembers = {1, 2, 1};
-        boolean[] expectedLocked = {false, false, true};
+        String[] expectedConferenceNames = {
+            "myconf3", "myconf2", "myconf1"
+        };
+        int[] expectedMembers = {
+            1, 2, 1
+        };
+        boolean[] expectedLocked = {
+            false, false, true
+        };
         for (int n = 0; n < activeConferences.size(); n++) {
             ActiveConference conference = activeConferences.get(n);
             Assert.assertEquals(expectedConferenceNames[n], conference.getName());
@@ -61,17 +87,39 @@ public class FreeswitchApiResultParserTest {
             Assert.assertEquals(expectedLocked[n], conference.isLocked());
         }
     }
-    
+
     @Test
     public void testGetConferenceMembers() {
-        String resultString =
-            "2;sofia/eng.bluesocket.com/201@192.168.100.233;f69d2b1f-4841-40a4-8e0f-847d1aeef2f0;201;201;hear|speak;0;0;300\n" +
-            "1;sofia/eng.bluesocket.com/200@192.168.100.233;a5b6cdbe-7cbf-48a7-a52d-98b871eb2491;Joe Attardi;200;hear|speak|floor;0;0;300\n";
-        
+        String resultString = "2;sofia/eng.bluesocket.com/201@192.168.100.233;f69d2b1f-4841-40a4-8e0f-847d1aeef2f0;201;201;hear|speak;10;20;300\n"
+                + "1;sofia/eng.bluesocket.com/200@192.168.100.233;a5b6cdbe-7cbf-48a7-a52d-98b871eb2491;Joe Attardi;200;speak|floor;0;0;300\n";
+
         List<ActiveConferenceMember> members = m_parser.getConferenceMembers(resultString);
         Assert.assertEquals(2, members.size());
         Assert.assertEquals("201 (201@192.168.100.233)", members.get(0).getName());
+        Assert.assertTrue(members.get(0).getCanHear());
+        Assert.assertTrue(members.get(0).getCanSpeak());
+
+        Assert.assertEquals(10, members.get(0).getVolumeIn());
+        Assert.assertEquals(20, members.get(0).getVolumeOut());
+        Assert.assertEquals(300, members.get(0).getEnergyLevel());
+        Assert.assertEquals("f69d2b1f-4841-40a4-8e0f-847d1aeef2f0", members.get(0).getUuid());
+
         Assert.assertEquals("Joe Attardi (200@192.168.100.233)", members.get(1).getName());
+        Assert.assertFalse(members.get(1).getCanHear());
+        Assert.assertTrue(members.get(1).getCanSpeak());
+    }
+
+    @Test
+    public void testGetConferenceMembersInvalid() {
+        String resultString = "nan;sofia/eng.bluesocket.com/201@192.168.100.233\n"
+                + "1;sofia/eng.bluesocket.com/200@192.168.100.233;a5b6cdbe-7cbf-48a7-a52d-98b871eb2491;Joe Attardi;200;speak|floor;0;0;300\n";
+
+        List<ActiveConferenceMember> members = m_parser.getConferenceMembers(resultString);
+        Assert.assertEquals(1, members.size());
+
+        Assert.assertEquals("Joe Attardi (200@192.168.100.233)", members.get(0).getName());
+        Assert.assertFalse(members.get(0).getCanHear());
+        Assert.assertTrue(members.get(0).getCanSpeak());
     }
 
     @Test(expected = NoSuchMemberException.class)
@@ -79,17 +127,17 @@ public class FreeswitchApiResultParserTest {
         ActiveConferenceMember member = new ActiveConferenceMember();
         member.setName("201 (201@192.168.100.233)");
         String resultString = "Non-Existant ID 4\n";
-        m_parser.verifyMemberAction(resultString, member);        
+        m_parser.verifyMemberAction(resultString, member);
     }
-    
+
     @Test
     public void testVerifyMemberActionValid() {
         ActiveConferenceMember member = new ActiveConferenceMember();
-        member.setName("201 (201@192.168.100.233)");        
+        member.setName("201 (201@192.168.100.233)");
         String resultString = "OK mute 1\n";
         Assert.assertTrue(m_parser.verifyMemberAction(resultString, member));
     }
-    
+
     @Test(expected = NoSuchConferenceException.class)
     public void testVerifyConferenceActionInvalid() {
         Conference conference = new Conference();
@@ -97,14 +145,13 @@ public class FreeswitchApiResultParserTest {
         String resultString = "Conference conference-301 not found\n";
         m_parser.verifyConferenceAction(resultString, conference);
     }
-    
+
     @Test
     public void testVerifyConferenceActionValid() {
         Conference conference = new Conference();
         conference.setName("conference-301");
-        String resultString =
-            "2;sofia/eng.bluesocket.com/201@192.168.100.233;f69d2b1f-4841-40a4-8e0f-847d1aeef2f0;201;201;hear|speak;0;0;300\n" +
-            "1;sofia/eng.bluesocket.com/200@192.168.100.233;a5b6cdbe-7cbf-48a7-a52d-98b871eb2491;Joe Attardi;200;hear|speak|floor;0;0;300\n";
+        String resultString = "2;sofia/eng.bluesocket.com/201@192.168.100.233;f69d2b1f-4841-40a4-8e0f-847d1aeef2f0;201;201;hear|speak;0;0;300\n"
+                + "1;sofia/eng.bluesocket.com/200@192.168.100.233;a5b6cdbe-7cbf-48a7-a52d-98b871eb2491;Joe Attardi;200;hear|speak|floor;0;0;300\n";
         Assert.assertTrue(m_parser.verifyConferenceAction(resultString, conference));
     }
 }
