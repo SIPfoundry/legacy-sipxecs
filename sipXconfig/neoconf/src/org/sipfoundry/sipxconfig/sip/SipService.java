25e2d2bf53546f321742e4e31db6037ba773a9d5
mranga <mranga@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-07-20 02:34:56 +0000

    XCF-2671 pass user credentials to send refer method
    We need to authenticate the REFER as user who initiated the call.
    SipService.sendRefer now takes User parameter.
    
    This patch does not actually use the credential informaton yet, it's just passing it to the SipService.
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@13118 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/sip/SipService.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/sip/SipService.java
@@ -1,13 +1,15 @@
 /*
- * 
- * 
- * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.  
+ *
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
  * Contributors retain copyright to elements licensed under a Contributor Agreement.
  * Licensed to the User under the LGPL license.
- * 
+ *
  * $
  */
 package org.sipfoundry.sipxconfig.sip;
+
+import org.sipfoundry.sipxconfig.common.User;
 
 
 
@@ -18,6 +20,6 @@ public interface SipService {
     public void sendCheckSync(String addrSpec);
 
     public void sendNotify(String addrSpec, String eventType, String contentType, byte[] payload);
-    
-    public void sendRefer(String sourceAddrSpec, String destinationAddSpec);
+
+    public void sendRefer(User user, String sourceAddrSpec, String destinationAddSpec);
 }
