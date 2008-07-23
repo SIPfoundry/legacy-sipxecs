cbf0f27119e7723690b61c4569566d6e34e95514
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-07-23 20:14:02 +0000

    XCF-2689 fix listenr name on the ListGateways page
    Incorrect name of the listener method (was: propagateAllGateways, should be: propagateAll), 'send
    all profiles' action was causing exceptions.
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@13169 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/gateway/ListGateways.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/gateway/ListGateways.java
@@ -1,10 +1,10 @@
 /*
- * 
- * 
- * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.  
+ *
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
  * Contributors retain copyright to elements licensed under a Contributor Agreement.
  * Licensed to the User under the LGPL license.
- * 
+ *
  * $
  */
 package org.sipfoundry.sipxconfig.site.gateway;
@@ -92,7 +92,7 @@ public abstract class ListGateways extends BasePage {
         }
     }
 
-    public void propagateAllGateways() {
+    public void propagateAll() {
         Collection gatewayIds = getGatewayContext().getAllGatewayIds();
         setGenerateProfileIds(gatewayIds);
     }
