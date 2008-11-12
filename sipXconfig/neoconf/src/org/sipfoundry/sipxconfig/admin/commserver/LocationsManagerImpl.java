859519a217ae659fe57f1f597e056e16f3811a3a
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-11-12 22:34:03 +0000

    XCF-2993 rollback changes related to triggering domain-config generation when adding/removing server
    Cannot trigger domain-config generation in LocationManagerImpl: sipXconfig uses LocationManager
    during in init DB tasks. Domain might not be initialized yet. Need a smarter way of triggering
    domain-config generation.
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@14004 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/commserver/LocationsManagerImpl.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/commserver/LocationsManagerImpl.java
@@ -12,12 +12,8 @@ package org.sipfoundry.sipxconfig.admin.commserver;
 import java.util.List;
 
 import org.sipfoundry.sipxconfig.common.SipxHibernateDaoSupport;
-import org.sipfoundry.sipxconfig.domain.DomainManager;
-import org.springframework.beans.factory.annotation.Required;
 
 public class LocationsManagerImpl extends SipxHibernateDaoSupport<Location> implements LocationsManager {
-
-    private DomainManager m_domainManager;
 
     /** Return the replication URLs, retrieving them on demand */
     public Location[] getLocations() {
@@ -33,12 +29,10 @@ public class LocationsManagerImpl extends SipxHibernateDaoSupport<Location> impl
 
     public void storeLocation(Location location) {
         getHibernateTemplate().saveOrUpdate(location);
-        m_domainManager.replicateDomainConfig();
     }
 
     public void deleteLocation(Location location) {
         getHibernateTemplate().delete(location);
-        m_domainManager.replicateDomainConfig();
     }
 
     public Location getPrimaryLocation() {
@@ -47,10 +41,5 @@ public class LocationsManagerImpl extends SipxHibernateDaoSupport<Location> impl
             return getLocations()[0];
         }
         return null;
-    }
-
-    @Required
-    public void setDomainManager(DomainManager domainManager) {
-        m_domainManager = domainManager;
     }
 }
