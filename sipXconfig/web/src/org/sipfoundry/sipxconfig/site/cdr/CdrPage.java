134c07df1d4fc7b11bd425cce4b2551f46c1b6a1
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2008-12-24 12:36:44 +0000

    XCF-2529 change the way CdrPage checks if call resolver is installed
    Instead of using Process XML/RPC API CDR page now asks SipxServiceManager if call resolver is installed.
    Only primary server is checked since this is where call resolver SOAP calls are directed.
    
    Patch from Mircea Carasel - thanks!
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@14362 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/cdr/CdrPage.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/cdr/CdrPage.java
@@ -1,10 +1,10 @@
 /*
- * 
- * 
- * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.  
+ *
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
  * Contributors retain copyright to elements licensed under a Contributor Agreement.
  * Licensed to the User under the LGPL license.
- * 
+ *
  * $
  */
 package org.sipfoundry.sipxconfig.site.cdr;
@@ -18,10 +18,11 @@ import org.apache.tapestry.annotations.InjectObject;
 import org.apache.tapestry.annotations.Persist;
 import org.apache.tapestry.event.PageBeginRenderListener;
 import org.apache.tapestry.event.PageEvent;
+import org.sipfoundry.sipxconfig.admin.commserver.Location;
 import org.sipfoundry.sipxconfig.admin.commserver.LocationsManager;
-import org.sipfoundry.sipxconfig.admin.commserver.SipxProcess;
-import org.sipfoundry.sipxconfig.admin.commserver.SipxProcessContext;
 import org.sipfoundry.sipxconfig.components.SipxValidationDelegate;
+import org.sipfoundry.sipxconfig.service.SipxCallResolverService;
+import org.sipfoundry.sipxconfig.service.SipxServiceManager;
 import org.sipfoundry.sipxconfig.site.user_portal.UserBasePage;
 
 public abstract class CdrPage extends UserBasePage implements PageBeginRenderListener {
@@ -29,6 +30,7 @@ public abstract class CdrPage extends UserBasePage implements PageBeginRenderLis
     private static final String HISTORIC_TAB = "historic";
     private static final String REPORTS_TAB = "reports";
 
+    @Override
     @Bean
     public abstract SipxValidationDelegate getValidator();
 
@@ -38,32 +40,29 @@ public abstract class CdrPage extends UserBasePage implements PageBeginRenderLis
 
     public abstract void setTab(String tab);
 
-    @InjectObject(value = "spring:sipxProcessContext")
-    public abstract SipxProcessContext getSipxProcessContext();
+    @InjectObject("spring:sipxServiceManager")
+    public abstract SipxServiceManager getSipxServiceManager();
 
-    @InjectObject(value = "spring:locationsManager")
+    @InjectObject("spring:locationsManager")
     public abstract LocationsManager getLocationsManager();
 
-    public abstract SipxProcess getCallResolverProcess();
+    public abstract boolean isCallResolverInstalled();
 
-    public abstract void setCallResolverProcess(SipxProcess sipxProcess);
+    public abstract void setCallResolverInstalled(boolean callResolverInstalled);
 
-    public Collection getTabNames() {
-        SipxProcess callResolverProcess = getCallResolverProcess();
-        if (callResolverProcess.isEnabled()) {
+    public Collection<String> getTabNames() {
+        if (isCallResolverInstalled()) {
             return Arrays.asList(ACTIVE_TAB, HISTORIC_TAB, REPORTS_TAB);
         }
         return Arrays.asList(HISTORIC_TAB, REPORTS_TAB);
     }
 
+    @Override
     public void pageBeginRender(PageEvent event) {
-        SipxProcess crp = getCallResolverProcess();
-        if (crp == null) {
-            crp = new SipxProcess(getLocationsManager(), getSipxProcessContext(), "CallResolver");
-            setCallResolverProcess(crp);
-        }
-
-        if (!crp.isEnabled() && getTab().equals(ACTIVE_TAB)) {
+        Location primaryLocation = getLocationsManager().getPrimaryLocation();
+        setCallResolverInstalled(getSipxServiceManager().isServiceInstalled(primaryLocation.getId(),
+                SipxCallResolverService.BEAN_ID));
+        if (!isCallResolverInstalled() && getTab().equals(ACTIVE_TAB)) {
             setTab(HISTORIC_TAB);
         }
     }
