5c3fc98282c94f5d879be116b952deebd96f3444
huijun <huijun@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-03-06 20:34:33 +0000

    XCF-3433: Provide mechanism to deploy Polycom License Files
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@14876 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/plugins/polycom/src/org/sipfoundry/sipxconfig/phone/polycom/ApplicationConfiguration.java
+++ sipXconfig/plugins/polycom/src/org/sipfoundry/sipxconfig/phone/polycom/ApplicationConfiguration.java
@@ -10,6 +10,10 @@
 package org.sipfoundry.sipxconfig.phone.polycom;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
 
 import org.sipfoundry.sipxconfig.device.ProfileContext;
 
@@ -50,14 +54,39 @@ public class ApplicationConfiguration extends ProfileContext<PolycomPhone> {
     public String getDeviceFilename() {
         return getDevice().getDeviceFilename();
     }
+   
+   /**
+    * This will list 000000000000-license.cfg and &lt;MAC&gt-license.cfg 
+    * in the &lt;MAC&gt.cfg if they exist.          
+    *
+    */
+    public String getLicenseFileNames() {
 
+        String licenseFiles = null;
+        List<String> licenseFileNames = new ArrayList<String>();
+        String universalLicenseFilename = getUniversalLicenseFilename();
+        if (universalLicenseFilename != null) {
+            licenseFileNames.add(universalLicenseFilename);
+        }
+        String myLicenseFileName = getMyLicenseFilename();
+        if (myLicenseFileName != null) {
+            licenseFileNames.add(myLicenseFileName);
+        }
+        if (!licenseFileNames.isEmpty()) {
+            licenseFiles =  StringUtils.join(licenseFileNames, ", ");
+        }
+        return licenseFiles;
+
+    }
     /**
-     * This is to check if 000000000000-license.cfg is available.
+     * This is to check if universal license file: 000000000000-license.cfg
+     * is available under tftproot.
+     *
      */
-    public String getLicenseFilename() {
+    private String getUniversalLicenseFilename() {
 
         String licenseFilename = null;
-        File licenseFile = new File(m_profileDir + "/" + LICENSE_FILE_NAME);
+        File licenseFile = new File(m_profileDir + File.separator + LICENSE_FILE_NAME);
 
         if (licenseFile.exists()) {
             licenseFilename = LICENSE_FILE_NAME;
@@ -65,4 +94,26 @@ public class ApplicationConfiguration extends ProfileContext<PolycomPhone> {
 
         return licenseFilename;
     }
+
+    /**
+     * This is to check if &lt;MAC&gt-license.cfg is available under tftproot.
+     *
+     */
+    private String getMyLicenseFilename() {
+
+        String licenseFilename = null;
+        File licenseFile = new File(m_profileDir + File.separator + getMyLicenseFileName());
+
+        if (licenseFile.exists()) {
+            licenseFilename = getMyLicenseFileName();
+        }
+
+        return licenseFilename;
+    }
+
+    private String getMyLicenseFileName() {
+        return getDevice().getProfileFilename() + "-license.cfg";
+    }
+
+
 }
