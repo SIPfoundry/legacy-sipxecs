94558edb7c60752409288479ace76bb223e25d85
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-04-13 21:19:39 +0000

    XCF-3613 ciscoplus plug-in fixes
    - generate featureID set to 9 for line buttons
    - fixed multiline support
    - fixed and added unit tests for multiline
    
    Based on patch from Sen Heng - thanks!
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@15160 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/plugins/ciscoplus/test/org/sipfoundry/sipxconfig/phone/ciscoplus/CiscoplusPhoneTest.java
+++ sipXconfig/plugins/ciscoplus/test/org/sipfoundry/sipxconfig/phone/ciscoplus/CiscoplusPhoneTest.java
@@ -9,10 +9,13 @@
  */
 package org.sipfoundry.sipxconfig.phone.ciscoplus;
 
+import java.util.Arrays;
+
 import junit.framework.TestCase;
 
 import org.apache.commons.io.IOUtils;
 import org.sipfoundry.sipxconfig.TestHelper;
+import org.sipfoundry.sipxconfig.common.User;
 import org.sipfoundry.sipxconfig.device.MemoryProfileLocation;
 import org.sipfoundry.sipxconfig.device.Profile;
 import org.sipfoundry.sipxconfig.phone.PhoneTestDriver;
@@ -29,10 +32,10 @@ public class CiscoplusPhoneTest extends TestCase {
         model.setProfileTemplate("ciscoplus/config.vm");
         m_phone = new CiscoplusPhone();
         m_phone.setModel(model);
-        PhoneTestDriver.supplyTestData(m_phone);
     }
 
     public void testGetSettings() {
+        PhoneTestDriver.supplyTestData(m_phone);
         assertNotNull(m_phone.getSettings());
     }
 
@@ -44,9 +47,31 @@ public class CiscoplusPhoneTest extends TestCase {
     }
 
     public void testGenerate7960Profiles() throws Exception {
+        PhoneTestDriver.supplyTestData(m_phone);
         MemoryProfileLocation location = TestHelper.setVelocityProfileGenerator(m_phone);
         m_phone.generateProfiles(location);
         String expected = IOUtils.toString(this.getClass().getResourceAsStream("SEP111111111111.cnf.xml"));
+        assertEquals(expected, location.toString());
+    }
+
+    public void testGenerate7960ProfilesTwoLine() throws Exception {
+        User u1 = new User();
+        u1.setUserName("200");
+        u1.setSipPassword("200");
+        u1.setLastName("200");
+
+        User u2 = new User();
+        u2.setUserName("201");
+        u2.setSipPassword("201");
+        u2.setLastName("201");
+
+        // call this to inject dummy data
+        PhoneTestDriver.supplyTestData(m_phone, Arrays.asList(new User[] {
+            u1, u2
+        }));
+        MemoryProfileLocation location = TestHelper.setVelocityProfileGenerator(m_phone);
+        m_phone.generateProfiles(location);
+        String expected = IOUtils.toString(this.getClass().getResourceAsStream("SEP111111111111-2-lines.cnf.xml"));
         assertEquals(expected, location.toString());
     }
 }
