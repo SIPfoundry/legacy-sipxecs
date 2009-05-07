51902ad572f728e6ead41971dc85b29e0324fb03
mirceac <mirceac@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-05-07 20:43:11 +0000

    XX-5653 Fixed error in sbc_device database patch
    Removed "not null" constraint from column creation line and added it to the end of the patch, after a default value has been set for all rows in the sbc_device table.
    
    Patch from: Kevin Thorley - Thanks!
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/branches/4.0@15445 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/etc/database/sbc-device-add-port.sql
+++ sipXconfig/neoconf/etc/database/sbc-device-add-port.sql
@@ -1,3 +1,4 @@
-alter table sbc_device add column port int4 not null;
+alter table sbc_device add column port int4;
 alter table sbc_device alter column port set default 5060;
 update sbc_device set port = 5060;
+alter table sbc_device alter column port set not null;
