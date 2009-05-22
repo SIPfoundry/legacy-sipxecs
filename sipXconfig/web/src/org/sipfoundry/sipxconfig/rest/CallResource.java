c27a0319393d161f14dde6103c9fa072ee09cff6
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-05-22 15:45:46 +0000

    XX-5815 Extract class UserResource
    It can be used to implement all user specific resources.
    Allows for easy access to current user object (computed based on
    authentication credentials).
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@15613 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/rest/CallResource.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/rest/CallResource.java
@@ -10,37 +10,26 @@
 package org.sipfoundry.sipxconfig.rest;
 
 import org.restlet.Context;
-import org.restlet.data.ChallengeResponse;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.resource.Representation;
-import org.restlet.resource.Resource;
-import org.sipfoundry.sipxconfig.common.CoreContext;
 import org.sipfoundry.sipxconfig.common.SipUri;
-import org.sipfoundry.sipxconfig.common.User;
 import org.sipfoundry.sipxconfig.domain.DomainManager;
 import org.sipfoundry.sipxconfig.sip.SipService;
 import org.springframework.beans.factory.annotation.Required;
 
-public class CallResource extends Resource {
+public class CallResource extends UserResource {
     private SipService m_sipService;
     private DomainManager m_domainManager;
-    private CoreContext m_coreContext;
     private String m_to;
     private String m_from;
-    private User m_user;
 
     @Override
     public void init(Context context, Request request, Response response) {
         super.init(context, request, response);
-
-        ChallengeResponse challengeResponse = request.getChallengeResponse();
-        String username = challengeResponse.getIdentifier();
-        m_user = m_coreContext.loadUserByUserName(username);
-
         String domain = m_domainManager.getDomain().getName();
         String to = (String) getRequest().getAttributes().get("to");
-        m_from = m_user.getAddrSpec(domain);
+        m_from = getUser().getAddrSpec(domain);
         m_to = SipUri.fix(to, domain);
     }
 
@@ -56,7 +45,7 @@ public class CallResource extends Resource {
 
     @Override
     public void put(Representation entity) {
-        m_sipService.sendRefer(m_user, m_from, m_to);
+        m_sipService.sendRefer(getUser(), m_from, m_to);
     }
 
     @Required
@@ -67,10 +56,5 @@ public class CallResource extends Resource {
     @Required
     public void setSipService(SipService sipService) {
         m_sipService = sipService;
-    }
-
-    @Required
-    public void setCoreContext(CoreContext coreContext) {
-        m_coreContext = coreContext;
     }
 }
--- sipXconfig/web/src/org/sipfoundry/sipxconfig/rest/VoicemailPinResource.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/rest/VoicemailPinResource.java
@@ -10,35 +10,25 @@
 package org.sipfoundry.sipxconfig.rest;
 
 import org.restlet.Context;
-import org.restlet.data.ChallengeResponse;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.resource.Representation;
-import org.restlet.resource.Resource;
 import org.restlet.resource.ResourceException;
-import org.sipfoundry.sipxconfig.common.CoreContext;
 import org.sipfoundry.sipxconfig.common.User;
 import org.sipfoundry.sipxconfig.domain.DomainManager;
 import org.springframework.beans.factory.annotation.Required;
 
 import static org.sipfoundry.sipxconfig.permission.PermissionName.TUI_CHANGE_PIN;
 
-public class VoicemailPinResource extends Resource {
+public class VoicemailPinResource extends UserResource {
     private DomainManager m_domainManager;
-    private CoreContext m_coreContext;
-    private User m_user;
     private String m_newPin;
 
     @Override
     public void init(Context context, Request request, Response response) {
         super.init(context, request, response);
 
-        ChallengeResponse challengeResponse = request.getChallengeResponse();
-        String username = challengeResponse.getIdentifier();
-        m_user = m_coreContext.loadUserByUserName(username);
-
-        setModifiable(m_user.hasPermission(TUI_CHANGE_PIN));
-        setAvailable(m_user.hasPermission(TUI_CHANGE_PIN));
+        setModifiable(getUser().hasPermission(TUI_CHANGE_PIN));
         setReadable(false);
 
         m_newPin = (String) getRequest().getAttributes().get("pin");
@@ -52,17 +42,13 @@ public class VoicemailPinResource extends Resource {
     @Override
     public void storeRepresentation(Representation entity) throws ResourceException {
         String realm = m_domainManager.getDomain().getSipRealm();
-        m_user.setPin(m_newPin, realm);
-        m_coreContext.saveUser(m_user);
+        User user = getUser();
+        user.setPin(m_newPin, realm);
+        getCoreContext().saveUser(user);
     }
 
     @Required
     public void setDomainManager(DomainManager domainManager) {
         m_domainManager = domainManager;
-    }
-
-    @Required
-    public void setCoreContext(CoreContext coreContext) {
-        m_coreContext = coreContext;
     }
 }
