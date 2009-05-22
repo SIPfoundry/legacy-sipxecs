7c21a57af00e746fbfca81f0d03cfee05b120a05
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-05-22 15:44:51 +0000

    XX-5815 Simplify Spring/Restlet integration.
    Switch to using SpringBeanRouter, remove unused classes.
    Resources can now be mapped to URLs using aliases.
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@15610 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/rest/RestSpringAdapterServlet.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/rest/RestSpringAdapterServlet.java
@@ -10,9 +10,8 @@
 package org.sipfoundry.sipxconfig.rest;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,7 +19,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.noelios.restlet.ext.servlet.ServletConverter;
 
-import org.restlet.Restlet;
 import org.restlet.Router;
 import org.springframework.context.ApplicationContext;
 
@@ -28,26 +26,23 @@ import static org.springframework.web.context.support.WebApplicationContextUtils
 
 /**
  * Style II of RESTlet integration where you keep Spring as ApplicationContext and pull in
- * restlets
+ * restlets.
+ *
+ * The magic relies on restletSpringBeanRouter bean that finds references to all beans of type
+ * Resource: URIs have to be configured as aliases for beans. Aliases starting with '/' are
+ * treated as URIs. Internal spring finder instantiates resources lazily - they need to have
+ * prototype scope.
  */
 public class RestSpringAdapterServlet extends HttpServlet {
     private ServletConverter m_converter;
-    private final Map<String, Restlet> m_resourceMappings = new HashMap<String, Restlet>();
-
-    public void init(Router router) {
-        for (String key : m_resourceMappings.keySet()) {
-            router.attach(key, m_resourceMappings.get(key));
-        }
-    }
 
     @Override
     public void init() throws ServletException {
         super.init();
-        ApplicationContext app = getRequiredWebApplicationContext(getServletContext());
-        RestManager manager = (RestManager) app.getBean(RestManager.CONTEXT_BEAN_NAME);
-        m_converter = new ServletConverter(getServletContext());
-        Router router = new Router(m_converter.getContext());
-        manager.init(router);
+        final ServletContext servletContext = getServletContext();
+        ApplicationContext app = getRequiredWebApplicationContext(servletContext);
+        m_converter = new ServletConverter(servletContext);
+        Router router = (Router) app.getBean("restletSpringBeanRouter", Router.class);
         m_converter.setTarget(router);
     }
 
