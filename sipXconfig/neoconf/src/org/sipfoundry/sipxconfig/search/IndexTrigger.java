31777ba16d4ac10f8183757944db2e319fb95fb6
mirceac <mirceac@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-06-01 15:10:21 +0000

    XX-5782: Pre-upgrade users cannot be added to the features(ACD,Hunt group) after upgrade.
    rebuid search index - rebuid search index during sipXconfig proper and not during initialization phase.
    
    Patch from: Huijun Yang - Thanks!
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/branches/4.0@15703 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/search/IndexTrigger.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/search/IndexTrigger.java
@@ -11,12 +11,15 @@ package org.sipfoundry.sipxconfig.search;
 
 import java.io.File;
 
+import org.sipfoundry.sipxconfig.admin.AdminContext;
 import org.sipfoundry.sipxconfig.common.ApplicationInitializedEvent;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 
 public class IndexTrigger implements ApplicationListener {
     private IndexManager m_indexManager;
+
+    private AdminContext m_adminContext;
 
     private boolean m_enabled = true;
 
@@ -27,14 +30,20 @@ public class IndexTrigger implements ApplicationListener {
             return;
         }
         if (event instanceof ApplicationInitializedEvent) {
-            if (!m_indexDirectory.exists()) {
-                m_indexManager.indexAll();
+            if (!m_adminContext.inInitializationPhase()) {
+                if (!m_indexDirectory.exists()) {
+                    m_indexManager.indexAll();
+                }
             }
         }
     }
 
     public void setIndexManager(IndexManager indexManager) {
         m_indexManager = indexManager;
+    }
+
+    public void setAdminContext(AdminContext adminContext) {
+        m_adminContext = adminContext;
     }
 
     public void setIndexDirectoryName(String indexDirectoryName) {
