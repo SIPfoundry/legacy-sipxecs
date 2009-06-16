c15bab9a35107452e28847437ff3d4a0085c5676
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-06-16 02:11:13 +0000

    XX-5916 fix menus on IE7 and later
    We are still using javascript snippet (ie-fix.js) to fix IE6 problem
    with li:hover implementation
    It should not be needed since IE7 properly implements :hover style.
    
    Patch from Damian Krzeminski - Thanks!
    
    Merged from: https://sipxecs.sipfoundry.org/rep/sipXecs/branches/4.0@15784
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@15798 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/components/ie-fix.js
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/components/ie-fix.js
@@ -1,15 +1,25 @@
-function hoverToOverForIE () {
-    if (document.all&&document.getElementById) {
-        navRoot = document.getElementById("nav");
-        for (i=0; i<navRoot.childNodes.length; i++) {
-            node = navRoot.childNodes[i];
-            if (node.nodeName=="LI") {
-                node.onmouseover=function() {
-                    this.className+=" over";
-                }
-                node.onmouseout=function() {
-                    this.className=this.className.replace(" over", "");
-                }
+function hoverToOverForIE() {
+    if (typeof document.body.style.maxHeight != "undefined") {
+        // see: http://ajaxian.com/archives/detecting-ie7-in-javascript
+        // modern browser: no need to simulate li:hover
+        return;
+    }
+
+    function addOver() {
+        this.className += " over";
+    }
+
+    function removeOver() {
+        this.className = this.className.replace(" over", "");
+    }
+
+    if (document.getElementById) {
+        var navRoot = document.getElementById("nav");
+        for ( var i = 0; i < navRoot.childNodes.length; i++) {
+            var node = navRoot.childNodes[i];
+            if (node.nodeName == "LI") {
+                node.onmouseover = addOver;
+                node.onmouseout = removeOver;
             }
         }
     }
