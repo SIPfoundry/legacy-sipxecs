662bbf3dec73f804c6c631c0f6f2bfad06310e9e
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-07-13 09:21:44 +0000

    XX-5519 Implement Click to Call for the User Personall Call History
    Added Tapestry DirectLink for call history table's To field
    
    Patch from: Constantin Tambrea - thanks!
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@15954 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/cdr/CdrTable.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/cdr/CdrTable.java
@@ -1,10 +1,10 @@
 /*
- * 
- * 
- * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.  
+ *
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
  * Contributors retain copyright to elements licensed under a Contributor Agreement.
  * Licensed to the User under the LGPL license.
- * 
+ *
  * $
  */
 package org.sipfoundry.sipxconfig.site.cdr;
@@ -12,11 +12,18 @@ package org.sipfoundry.sipxconfig.site.cdr;
 import org.apache.tapestry.BaseComponent;
 import org.apache.tapestry.annotations.ComponentClass;
 import org.apache.tapestry.annotations.InjectObject;
+import org.apache.tapestry.annotations.InjectState;
 import org.apache.tapestry.annotations.Parameter;
 import org.apache.tapestry.contrib.table.model.ITableColumn;
 import org.apache.tapestry.services.ExpressionEvaluator;
 import org.sipfoundry.sipxconfig.cdr.Cdr;
+import org.sipfoundry.sipxconfig.common.CoreContext;
+import org.sipfoundry.sipxconfig.common.SipUri;
+import org.sipfoundry.sipxconfig.common.User;
 import org.sipfoundry.sipxconfig.components.TapestryUtils;
+import org.sipfoundry.sipxconfig.domain.DomainManager;
+import org.sipfoundry.sipxconfig.sip.SipService;
+import org.sipfoundry.sipxconfig.site.UserSession;
 
 @ComponentClass(allowBody = false, allowInformalParameters = false)
 public abstract class CdrTable extends BaseComponent {
@@ -29,7 +36,36 @@ public abstract class CdrTable extends BaseComponent {
     public abstract Cdr getRow();
 
     public ITableColumn getStartTimeColumn() {
-        return TapestryUtils.createDateColumn("startTime", getMessages(),
-                getExpressionEvaluator(), getPage().getLocale());
+        return TapestryUtils.createDateColumn("startTime", getMessages(), getExpressionEvaluator(), getPage()
+                .getLocale());
     }
+
+    @InjectObject("spring:domainManager")
+    public abstract DomainManager getDomainManager();
+
+    @InjectObject("spring:sip")
+    public abstract SipService getSipService();
+
+    @InjectObject(value = "spring:coreContext")
+    public abstract CoreContext getCoreContext();
+
+    @InjectState(value = "userSession")
+    public abstract UserSession getUserSession();
+
+    /**
+     * Implements click to call link
+     *
+     * @param number number to call - refer is sent to current user
+     */
+    public void call(String number) {
+        String domain = getDomainManager().getDomain().getName();
+        String userAddrSpec = getUser().getAddrSpec(domain);
+        String destAddrSpec = SipUri.fix(number, domain);
+        getSipService().sendRefer(getUser(), userAddrSpec, destAddrSpec);
+    }
+
+    private User getUser() {
+        return getUserSession().getUser(getCoreContext());
+    }
+
 }
