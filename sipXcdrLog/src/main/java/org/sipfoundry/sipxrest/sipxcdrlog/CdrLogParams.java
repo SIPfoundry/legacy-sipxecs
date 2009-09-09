ac7fb7aa5400deae56297de640bbc593b3ca4ed7
rgdans <rgdans@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-09-09 13:50:37 +0000

    XX-6291 Adding REST service plugin for obtaining CDR Call Logs for a user.
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@16391 ab1d8caa-1f67-47f1-9e81-24633a41865c

---
+++ sipXcdrLog/src/main/java/org/sipfoundry/sipxrest/sipxcdrlog/CdrLogParams.java
@@ -0,0 +1,11 @@
+package org.sipfoundry.sipxrest.cdrlog;
+
+public interface CdrLogParams {
+
+    public static final String USER = "user";
+
+    public static final String LIMIT = "limit";
+
+    public static final String FROMDATE = "fromdate";
+
+}
