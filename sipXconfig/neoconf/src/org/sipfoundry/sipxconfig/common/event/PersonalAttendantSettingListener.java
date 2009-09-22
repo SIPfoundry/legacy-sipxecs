2109b33ab9dabe82d5a0ba8474607caa74c7eaf2
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-09-22 13:32:22 +0000

    Personal Attendand: fix retrieving value from group
    Fix code in attendant setting listener. It had special provisions for
    User without "settings" set which only happens in Integration test.
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@16570 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/common/event/PersonalAttendantSettingListener.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/common/event/PersonalAttendantSettingListener.java
@@ -1,34 +1,29 @@
 /*
- * 
- * 
- * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.  
+ *
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
  * Contributors retain copyright to elements licensed under a Contributor Agreement.
  * Licensed to the User under the LGPL license.
- * 
- * $
+ *
  */
 package org.sipfoundry.sipxconfig.common.event;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.sipfoundry.sipxconfig.common.CoreContext;
 import org.sipfoundry.sipxconfig.common.User;
 import org.sipfoundry.sipxconfig.setting.Group;
 import org.sipfoundry.sipxconfig.setting.Setting;
-import org.sipfoundry.sipxconfig.setting.SettingImpl;
-import org.sipfoundry.sipxconfig.setting.SettingValue;
 import org.sipfoundry.sipxconfig.vm.MailboxManager;
 import org.sipfoundry.sipxconfig.vm.attendant.PersonalAttendant;
 
 public class PersonalAttendantSettingListener implements DaoEventListener {
 
-    private static final String OPERATOR_SETTING = 
-        "personal-attendant" + Setting.PATH_DELIM + "operator";
-    
+    private static final String OPERATOR_SETTING = "personal-attendant" + Setting.PATH_DELIM + "operator";
+
     private CoreContext m_coreContext;
     private MailboxManager m_mailboxManager;
-    
+
     public void onDelete(Object entity) {
         // nothing to do on delete
     }
@@ -44,34 +39,27 @@ public class PersonalAttendantSettingListener implements DaoEventListener {
     private void onSaveGroup(Group group) {
         Collection<User> usersInGroup = m_coreContext.getGroupMembers(group);
         for (User user : usersInGroup) {
-            Setting operatorSetting = user.getSettings().getSetting(OPERATOR_SETTING);
-            String operatorValue = operatorSetting.getValue();
-            updatePersonalAttendantForUser(user, operatorValue);
+            String operator = user.getSettingValue(OPERATOR_SETTING);
+            updatePersonalAttendantForUser(user, operator);
         }
     }
-    
+
     /**
-     * Updates the personal attendant for a user given that either a) the 
-     * user has settings associated with it (i.e. user.getSettings != null) or 
-     * b) the user's group has settings associated with it.
-     * @param user
+     * Updates the personal attendant for a user.
      */
     private void onSaveUser(User user) {
-        if (user.getSettings() != null) {
-            Setting operatorSetting = user.getSettings().getSetting(OPERATOR_SETTING);
-            updatePersonalAttendantForUser(user, operatorSetting.getValue());
-        } else {
-            List<Group> groupsForUser = user.getGroupsAsList();
-            Group group = Group.selectGroupWithHighestWeight(groupsForUser);
-            if (group != null) {
-                SettingValue operatorSetting = group.getSettingValue(new SettingImpl(OPERATOR_SETTING));
-                if (operatorSetting != null) {
-                    updatePersonalAttendantForUser(user, operatorSetting.getValue());
-                }
-            }
+        if (user.isNew()) {
+            // nothing to update yet
+            return;
         }
+        // HACK: permission manager can be empty during tests - and settings are not loaded
+        if (user.getSettings() == null) {
+            return;
+        }
+        String operator = user.getSettingValue(OPERATOR_SETTING);
+        updatePersonalAttendantForUser(user, operator);
     }
-    
+
     private void updatePersonalAttendantForUser(User user, String operatorValue) {
         PersonalAttendant pa = m_mailboxManager.loadPersonalAttendantForUser(user);
         pa.setOperator(operatorValue);
@@ -81,7 +69,7 @@ public class PersonalAttendantSettingListener implements DaoEventListener {
     public void setCoreContext(CoreContext coreContext) {
         m_coreContext = coreContext;
     }
-    
+
     public void setMailboxManager(MailboxManager mailboxManager) {
         m_mailboxManager = mailboxManager;
     }
