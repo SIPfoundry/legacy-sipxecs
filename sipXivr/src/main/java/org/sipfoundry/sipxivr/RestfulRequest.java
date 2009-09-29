39cdf69f082c8aabe610326f590ffc1e95d76a22
woof <woof@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-09-29 14:59:18 +0000

    sipXivr: Expand RestfulRequest capabilities.
    Break up RestfulRequest into component parts so more elaborite uses
    of it (such as reusing the connection, and dealing with complicated
    content) are possible.
    
    Added convienience methods for simple uses of get/put/post/delete.
    
    Added unit tests to support the above.
    
    Requested by Martin Pepin, partially from a patch he sent me.  Thanks!
    
    --Woof!
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@16667 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXivr/src/main/java/org/sipfoundry/sipxivr/RestfulRequest.java
+++ sipXivr/src/main/java/org/sipfoundry/sipxivr/RestfulRequest.java
@@ -8,6 +8,8 @@
  */
 package org.sipfoundry.sipxivr;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -22,95 +24,201 @@ public class RestfulRequest {
     
     String m_urlString;
     String m_digest;
-    String m_method;
-    String m_result;
+    String m_content;
+    String m_contentType;
     String m_response;
+    int m_responseCode;
     Exception m_exception;
-    
+
+    public RestfulRequest(String urlString) {
+        m_urlString = urlString;
+    }
+
     public RestfulRequest(String urlString, String user, String password) {
         m_urlString = urlString;
-        m_digest = "Basic " + new String(Base64.encodeBase64((user+":"+password).getBytes()));
+        if (user != null && password != null) {
+            m_digest = "Basic " + new String(Base64.encodeBase64((user+":"+password).getBytes()));
+        }
     }
 
     /**
-     * Connect via http/https, put content
-     * @throws Exception 
+     * Initialize the connection parameters then connect via http/https
+     * @param value Added to m_urlString to complete the URL.  Redacted from logs.
+     * @return an HttpURLConnection ready to use with {@link request}
+     * @throws Exception
      */
-    public boolean put(String value) throws Exception{
-        m_method = "PUT";
-        return send(value);
+    public HttpURLConnection getConnection(String value) throws Exception {
+        String urlString = m_urlString;
+        if (value != null) {
+            if (!urlString.endsWith("/")) {
+                urlString += "/";
+            }
+            urlString += value;
+        }
+
+        URL url = new URL(urlString);
+
+        // URL connection channel.
+        HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+
+        // Set the Authorization header to the encoded user/passcode
+        // combination
+        if (m_digest != null) {
+            urlConn.setRequestProperty("Authorization", m_digest);
+        }
+
+        return urlConn;
     }
 
-    /**
-     * Connect via http/https, delete content
-     * @throws Exception 
-     */
-    public boolean delete() throws Exception{
-        m_method = "DELETE";
-        return send(null);
-    }
 
     /**
-     * Connect via http/https, send content
-     * @throws Exception 
+     *  Perform the method on the URLConnection
+     *
+     * @param method  GET, POST, PUT, DELETE, etc.
+     * @param urlConn a HttpURLConnection from {@link getConnection}
+     * @return true on valid response, false on failure
+     * @throws Exception
      */
-    public boolean send(String value) throws Exception {
+    public boolean request(String method, HttpURLConnection urlConn) throws Exception {
+        if (urlConn == null) {
+            throw new Exception("Invalid connection reference");
+        }
+
         if (m_justTesting) {
             // Don't do anything if we are just in a test situation
-            m_result = m_testingResult;
+            m_content = m_testingResult;
             return true;
         }
 
-        HttpURLConnection urlConn = null;
         try {
-            String urlString = m_urlString;
-            if (value != null) {
-                if (!urlString.endsWith("/")) {
-                    urlString += "/";
-                }
-                urlString += value;
-            }
-            URL url = new URL(urlString);
-            // URL connection channel.
-            urlConn = (HttpURLConnection) url.openConnection();
-
-            // Set the Authorization header to the encoded user/passcode combination
-            urlConn.setRequestProperty ("Authorization", m_digest);
-            
-            // Let the RTS know that we want to do output.
+            // Let the RTS know that we want to do both input and output.
             urlConn.setDoOutput(true);
+            urlConn.setDoInput(true);
 
             // No caching, we want the real thing.
             urlConn.setUseCaches(false);
+            urlConn.setRequestMethod(method);
 
-            urlConn.setRequestMethod(m_method);
-
-            LOG.info(String.format("RestfulRequest::put to %s/(_REDACTED_)", m_urlString));
-
+            // Connect!
+            urlConn.connect();
+            
+            // Find out what happened
+            m_responseCode = urlConn.getResponseCode();
             m_response = urlConn.getResponseMessage();
- 
-            if (urlConn.getResponseCode() < 200 || urlConn.getResponseCode() >=300) {
+
+            LOG.info(String.format("RestfulRequest::request %s to %s response: %d", method, m_urlString,
+                    m_responseCode));
+
+            if (m_responseCode < 200 || m_responseCode >= 300) {
                 // Responses 200-299 are Okay, all other are suspect
                 return false;
             }
-            
+
             return true;
         }
 
         catch (Exception ex) {
             m_exception = ex;
-            LOG.error(String.format("RestfulRequest::put to '%s' trouble '%s'", 
-                    m_urlString, m_response), ex);
+            LOG.error(String.format("RestfulRequest::request %s to '%s' trouble '%s'", method, urlConn.getURL().toString(),
+                    urlConn.getResponseMessage()), ex);
         }
         return false;
     }
 
+    /**
+     * Convenience method to send a request to a URL
+     * 
+     * @param method an HTTP method, PUT, DELETE, POST, GET, etc. 
+     *        where no body is provided.  
+     *        Any response (assumed to be text) is in m_content;
+     * @throws Exception 
+     */
+    boolean send(String method, String value) throws Exception{
+        HttpURLConnection urlConn = getConnection(value);
+        boolean result = request(method, urlConn);
+        if (urlConn.getContentLength() > 0) {
+            m_contentType = urlConn.getContentType();
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+            String line ;
+            m_content = "";
+            for(line = br.readLine(); line != null;) {
+                m_content.concat(line);
+                m_content.concat("\n");
+            }
+        }
+        urlConn.disconnect();
+        return result;
+    }
+    
+
+    /**
+     * Convenience method to PUT to a URL (no body)
+     * 
+     * @param value A string added to the end of the url.  Redacted from logs.
+     * @throws Exception 
+     */
+    public boolean put(String value) throws Exception{
+        return send("PUT", value);
+    }
+
+    /**
+     * Convenience method to POST to a URL (no body)
+     * 
+     * @param value A string added to the end of the url.  Redacted from logs.
+     * @throws Exception 
+     */
+    public boolean post(String value) throws Exception{
+        return send("POST", value);
+    }
+
+    /**
+     * Convenience method to DELETE to a URL
+     * 
+     * @throws Exception 
+     */
+    public boolean delete() throws Exception{
+        return send("DELETE", null);
+    }
+
+    /**
+     * Convenience method to GET to a URL
+     * 
+     * @throws Exception 
+     */
+    public boolean get() throws Exception{
+        return send("GET", null);
+    }
+
+    
+    /**
+     * method to GET content from a URL with a reusable connection
+     * No content is read from the connection, that's up to the caller. 
+     * 
+     * @param urlConn a HttpURLConnection from {@link getConnection}
+     * @throws Exception 
+     */
+    public boolean get(HttpURLConnection urlConn) throws Exception{
+        return request("GET", urlConn);
+    }
+    
+    public int getResponseCode() {
+        return m_responseCode ;
+    }
+    
     public String getResponse() {
         return m_response;
     }
     
     public String getResult() {
-        return m_result;
+        return m_content;
+    }
+    
+    public String getContentType() {
+        return m_contentType;
+    }
+    
+    public String getContent() {
+        return m_content;
     }
     
     public Exception getException() {
