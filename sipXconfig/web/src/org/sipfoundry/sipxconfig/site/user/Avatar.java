e21fcb25164b5c47f69af8fd387d68329ef75629
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-10-06 22:20:09 +0000

    XX-5536 add simple avatar UI
    Displays user gravatar on Contact Info page in admin portal and My
    Information page in user portal.
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@16741 ab1d8caa-1f67-47f1-9e81-24633a41865c

---
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/user/Avatar.java
@@ -0,0 +1,37 @@
+/*
+ *
+ *
+ * Copyright (C) 2009 Pingtel Corp., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ *
+ *
+ */
+package org.sipfoundry.sipxconfig.site.user;
+
+import org.apache.tapestry.BaseComponent;
+import org.apache.tapestry.IAsset;
+import org.apache.tapestry.annotations.ComponentClass;
+import org.apache.tapestry.annotations.InjectObject;
+import org.apache.tapestry.annotations.Parameter;
+import org.apache.tapestry.asset.ExternalAsset;
+import org.sipfoundry.sipxconfig.common.User;
+import org.sipfoundry.sipxconfig.phonebook.Gravatar;
+import org.sipfoundry.sipxconfig.vm.MailboxManager;
+
+import static org.apache.commons.lang.StringUtils.defaultString;
+
+@ComponentClass(allowBody = false, allowInformalParameters = false)
+public abstract class Avatar extends BaseComponent {
+    @Parameter
+    public abstract User getUser();
+
+    @InjectObject("spring:mailboxManager")
+    public abstract MailboxManager getMailboxManager();
+
+    public IAsset getAvatarAsset() {
+        Gravatar gravatar = new Gravatar(getUser());
+        String url = defaultString(gravatar.getUrl(getMailboxManager()), "http://gravatar.com/avatar");
+        return new ExternalAsset(url, null);
+    }
+}
