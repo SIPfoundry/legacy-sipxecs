133b01f90d159ddfdb3f4cef681bef8a1a7e14a2
anair1 <anair1@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-11-05 22:31:04 +0000

    XX-6798  After changing MOH Source being asked to restart Media Service all the time
    Update audioDirectoryEmpty after marking the service for restart
    Remove isInitialized field, and use "null" as an indication that field needs to be initialized in 'pageBeginRender'
    
    Based on patch from Damian Krzeminski -- Thanks!
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@17106 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/MusicOnHold.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/MusicOnHold.java
@@ -5,13 +5,12 @@
  * Contributors retain copyright to elements licensed under a Contributor Agreement.
  * Licensed to the User under the LGPL license.
  *
- * $
+ *
  */
 package org.sipfoundry.sipxconfig.site.admin;
 
 import org.apache.tapestry.annotations.Bean;
 import org.apache.tapestry.annotations.InjectObject;
-import org.apache.tapestry.annotations.Persist;
 import org.apache.tapestry.event.PageBeginRenderListener;
 import org.apache.tapestry.event.PageEvent;
 import org.apache.tapestry.html.BasePage;
@@ -22,7 +21,6 @@ import org.sipfoundry.sipxconfig.service.SipxFreeswitchService;
 import org.sipfoundry.sipxconfig.service.SipxService;
 import org.sipfoundry.sipxconfig.service.SipxServiceManager;
 import org.sipfoundry.sipxconfig.setting.Setting;
-
 
 public abstract class MusicOnHold extends BasePage implements PageBeginRenderListener {
 
@@ -35,31 +33,25 @@ public abstract class MusicOnHold extends BasePage implements PageBeginRenderLis
     @InjectObject("spring:sipxServiceManager")
     public abstract SipxServiceManager getSipxServiceManager();
 
-    @Persist
     public abstract String getAsset();
 
-    @Persist
-    public abstract boolean isAudioDirectoryEmpty();
+    public abstract Boolean getAudioDirectoryEmpty();
 
-    public abstract void setAudioDirectoryEmpty(boolean isAudioDirectoryEmpty);
-
-    @Persist
-    public abstract boolean isInitialized();
+    public abstract void setAudioDirectoryEmpty(Boolean isAudioDirectoryEmpty);
 
     public abstract SipxService getSipxService();
 
     public abstract void setSipxService(SipxService service);
 
-    public abstract void setInitialized(boolean initialized);
-
     public void pageBeginRender(PageEvent event_) {
-        if (isInitialized()
-                && getMusicOnHoldManager().isAudioDirectoryEmpty() != isAudioDirectoryEmpty()) {
+        boolean managerDirectoryEmpty = getMusicOnHoldManager().isAudioDirectoryEmpty();
+        if (getAudioDirectoryEmpty() == null) {
+            setAudioDirectoryEmpty(managerDirectoryEmpty);
+        } else if (managerDirectoryEmpty != getAudioDirectoryEmpty()) {
             getMusicOnHoldManager().replicateMohConfiguration();
-        } else {
-            setAudioDirectoryEmpty(getMusicOnHoldManager().isAudioDirectoryEmpty());
-            setInitialized(true);
+            setAudioDirectoryEmpty(managerDirectoryEmpty);
         }
+
         if (getSipxService() == null) {
             SipxService sipxService = getSipxServiceManager().getServiceByBeanId(SipxFreeswitchService.BEAN_ID);
             setSipxService(sipxService);
