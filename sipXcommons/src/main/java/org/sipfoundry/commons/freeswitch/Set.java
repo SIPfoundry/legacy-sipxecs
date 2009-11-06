5033ccc607ce3e300b42f75dfb51490664b3df25
fowlerp <fowlerp@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-11-06 18:36:09 +0000

    Changes required to support Personal Assistant functionality
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@17116 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXcommons/src/main/java/org/sipfoundry/commons/freeswitch/Set.java
+++ sipXcommons/src/main/java/org/sipfoundry/commons/freeswitch/Set.java
@@ -10,8 +10,38 @@ package org.sipfoundry.commons.freeswitch;
 
 public class Set extends CallCommand {
 
+    String m_uuid;
+
+    public boolean start() {
+        if(m_uuid == null) {
+            return super.start();
+        }
+        m_finished = false;
+        // Send the command to the socket
+        m_fses.cmd("sendmsg " + m_uuid +
+                "\ncall-command: execute\nexecute-app-name: " + m_command);
+        return false;
+    }
+
+
+    public boolean handleEvent(FreeSwitchEvent event) {
+
+        if(m_uuid == null) {
+            return super.handleEvent(event);
+        }
+
+        return true;
+    }
+   
     public Set(FreeSwitchEventSocketInterface fses, String variable, String value) {
         super(fses);
+        m_uuid = null;
         m_command = String.format("set\nexecute-app-arg: %s=%s", variable, value);
     }
+
+    public Set(FreeSwitchEventSocketInterface fses, String chan_uuid, String variable, String value) {
+        super(fses);
+        m_uuid = chan_uuid;
+        m_command = "event\nexecute-app-arg: " + variable + "=" + value;
+    }      
 }
