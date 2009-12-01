c0524f2608f8e6ae076dfb49fbc8af4ddb1b2c83
dkrzemin <dkrzemin@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-12-01 21:58:13 +0000

    XX-5971 user portal changes for external IM accounts
    Added UI for admin portal Added 'External IM accounts' table and
    posibility to add/edit an 'External IM account'.
    
    Patch from: Laurentiu Ceausescu - thanks!
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@17418 ab1d8caa-1f67-47f1-9e81-24633a41865c

---
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/user_portal/ExternalUserImAccount.java
@@ -0,0 +1,80 @@
+/*
+ *
+ *
+ * Copyright (C) 2009 Nortel, certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ *
+ *
+ */
+package org.sipfoundry.sipxconfig.site.user_portal;
+
+import org.apache.tapestry.annotations.Persist;
+import org.apache.tapestry.event.PageEvent;
+import org.apache.tapestry.form.IPropertySelectionModel;
+import org.apache.tapestry.form.StringPropertySelectionModel;
+import org.sipfoundry.sipxconfig.components.LocalizedOptionModelDecorator;
+import org.sipfoundry.sipxconfig.components.ReportBean;
+import org.sipfoundry.sipxconfig.components.TapestryUtils;
+import org.sipfoundry.sipxconfig.im.ExternalImAccount;
+
+public abstract class ExternalUserImAccount extends UserBasePage {
+
+    public static final String PAGE = "user_portal/ExternalUserImAccount";
+
+    private static final String[] PROTOCOLS = {
+        "aim", "gtalk", "icq", "irc", "msn", "yahoo"
+    };
+
+    public abstract ExternalImAccount getExternalImAccount();
+
+    public abstract void setExternalImAccount(ExternalImAccount account);
+
+    public abstract void setProtocolType(ReportBean type);
+
+    @Persist
+    public abstract Integer getExternalImAccountId();
+
+    public abstract void setExternalImAccountId(Integer id);
+
+    public void addExternalUserImAccount(String returnPage) {
+        setExternalImAccountId(null);
+        setReturnPage(returnPage);
+    }
+
+    public void editExternalUserImAccount(Integer accountId, String returnPage) {
+        setExternalImAccountId(accountId);
+        setReturnPage(returnPage);
+    }
+
+    @Override
+    public void pageBeginRender(PageEvent event) {
+        ExternalImAccount account = getExternalImAccount();
+        if (account != null) {
+            return;
+        }
+
+        Integer accountId = getExternalImAccountId();
+        if (accountId != null) {
+            account = getCoreContext().getExternalAccountById(accountId);
+        } else {
+            account = new ExternalImAccount();
+            account.setUser(getUser());
+        }
+        setExternalImAccount(account);
+    }
+
+    public void commit() {
+        if (!TapestryUtils.isValid(this)) {
+            return;
+        }
+
+        ExternalImAccount account = getExternalImAccount();
+        getCoreContext().saveExternalAccount(account);
+    }
+
+    public IPropertySelectionModel getProtocolTypeModel() {
+        IPropertySelectionModel model = new StringPropertySelectionModel(PROTOCOLS);
+        return new LocalizedOptionModelDecorator(model, getMessages(), "label.");
+    }
+}
