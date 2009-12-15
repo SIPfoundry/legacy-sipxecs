4fe1394385bd4a6adde6255e5ffafdd6548508f2
mirceac <mirceac@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2009-12-15 22:31:48 +0000

    XX-7260:Do not generate ssl/authorities directories in source folder
    create ssl/authorities directories in specific test location in BUILD directory
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@17537 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/test/TestUtil.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/test/TestUtil.java
@@ -194,7 +194,7 @@ public final class TestUtil {
         final String mailstoreDir = outputDirectory + "/mailstore";
         final String binDir = outputDirectory + "/bin";
         final String tmpDir = outputDirectory + "/tmp";
-        final String sslDir = etcDirectory + "/ssl";
+        final String sslDir = outputDirectory + "/ssl";
         final String authDir = sslDir + "/authorities";
 
         sysProps.setProperty("sysdir.bin", binDir);
@@ -244,10 +244,11 @@ public final class TestUtil {
         File vmDir = createDirectory(mailstoreDir, "Could not create voicemail store");
         createDirectory(tmpDir, "Could not create tmp directory");
         File ssl = createDirectory(sslDir, "Could not create ssl directory");
-        createDirectory(authDir, "Could not create auth directory");
+        File auth = createDirectory(authDir, "Could not create auth directory");
 
         sysProps.setProperty("mailboxManagerImpl.mailstoreDirectory", vmDir.getAbsolutePath());
         sysProps.setProperty("certificateManagerImpl.sslDirectory", ssl.getAbsolutePath());
+        sysProps.setProperty("certificateManagerImpl.sslAuthDirectory", auth.getAbsolutePath());
     }
 
     private static File createDirectory(String directoryName, String errorMessage) {
