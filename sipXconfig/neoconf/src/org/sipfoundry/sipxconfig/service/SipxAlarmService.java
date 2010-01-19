b5e5856b37639843caed7d7d0cf79af38ab06bf4
huijun <huijun@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2010-01-19 20:59:07 +0000

    XX-6970:  Adding the missing file from previous commit.
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@17741 ab1d8caa-1f67-47f1-9e81-24633a41865c

---
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/service/SipxAlarmService.java
@@ -0,0 +1,30 @@
+/*
+ *
+ *
+ * Copyright (C) 2009 Pingtel Corp., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ *
+ *
+ */
+package org.sipfoundry.sipxconfig.service;
+
+import org.sipfoundry.sipxconfig.admin.commserver.Location;
+import org.sipfoundry.sipxconfig.common.AlarmContext;
+import org.springframework.beans.factory.annotation.Required;
+
+public class SipxAlarmService extends SipxService {
+
+    public static final String BEAN_ID = "sipxAlarmService";
+
+    private AlarmContext m_alarmContext;
+
+    public void afterReplication(Location location) {
+        m_alarmContext.reloadAlarms();
+    }
+
+    @Required
+    public void setAlarmContext(AlarmContext alarmContext) {
+        m_alarmContext = alarmContext;
+    }
+}
