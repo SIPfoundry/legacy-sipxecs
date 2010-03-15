1e1549119f719c42b5b3463464a6d3cff59a98af
hengsen <hengsen@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2010-03-15 09:56:25 +0000

    <project name="Ciscospa" default="default">
      <property name="project.dir" value="plugins/Ciscospa" />
      <property name="plugin.models.dir" value="etc/Ciscospa" />
      <property name="plugin.name" value="Ciscospa-phones" />
    
      <import file="../plugins.build.xml" />
    
    </project>
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@18316 ab1d8caa-1f67-47f1-9e81-24633a41865c

---
+++ sipXconfig/plugins/ciscospa/src/org/sipfoundry/sipxconfig/phone/Ciscospa/CiscospaModel.java
@@ -0,0 +1,47 @@
+/*
+ *
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ *
+ * $
+ */
+package org.sipfoundry.sipxconfig.phone.Ciscospa;
+
+import org.sipfoundry.sipxconfig.phone.PhoneModel;
+
+import static org.apache.commons.lang.StringUtils.isBlank;
+
+/**
+ * Static differences in linksys models
+ */
+public class CiscospaModel extends PhoneModel {
+
+    private String m_psn;
+
+    public CiscospaModel() {
+    }
+
+    public CiscospaModel(String beanId) {
+        super(beanId);
+    }
+
+    /**
+     * Linksys phones by default look for a minimum initial configuration file, which in turns
+     * provide the name of the specific configuration file.
+     *
+     * The name of the file seems to depend on the device model. Most documents on the web suggest
+     * it's 'spa$PSN.cfg' where $PSN refers to numeric portion of the model name.
+     */
+    public String getDefaultConfigName() {
+        if (isBlank(m_psn)) {
+            return null;
+        }
+        return String.format("spa%s.cfg", m_psn);
+    }
+
+    public void setPsn(String psn) {
+        m_psn = psn;
+    }
+}
---
+++ sipXconfig/plugins/ciscospa/test/org/sipfoundry/sipxconfig/phone/ciscospa/ciscospaModelTest.java
@@ -0,0 +1,22 @@
+/*
+ *
+ *
+ * Copyright (C) 2008 Pingtel Corp., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ *
+ *
+ */
+package org.sipfoundry.sipxconfig.phone.ciscospa;
+
+import junit.framework.TestCase;
+
+public class ciscospaModelTest extends TestCase {
+
+    public void testGetDefaultConfigName() {
+        ciscospaModel linksysModel = new ciscospaModel();
+        assertNull(ciscospaModel.getDefaultConfigName());
+        ciscospaModel.setPsn("508g");
+        assertEquals("spa508g.cfg", ciscospaModel.getDefaultConfigName());
+    }
+}
