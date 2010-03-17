84d556c9db53813a344ce226df842d9c9a14521b
lceausescu@itcnetworks.ro <lceausescu@itcnetworks.ro@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2010-03-17 10:04:12 +0000

    XX-7790 - Branch selection does not work as expected in user identification page Made a squeeze adapter (BranchAdapter) for BranchSelection component
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@18331 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/branch/BranchSelection.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/branch/BranchSelection.java
@@ -10,15 +10,27 @@
 
 package org.sipfoundry.sipxconfig.site.branch;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import org.apache.commons.lang.ObjectUtils;
 import org.apache.tapestry.BaseComponent;
+import org.apache.tapestry.IActionListener;
+import org.apache.tapestry.IComponent;
+import org.apache.tapestry.IMarkupWriter;
+import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.annotations.Bean;
 import org.apache.tapestry.annotations.InjectObject;
 import org.apache.tapestry.annotations.Parameter;
 import org.apache.tapestry.form.IPropertySelectionModel;
 import org.sipfoundry.sipxconfig.branch.Branch;
 import org.sipfoundry.sipxconfig.branch.BranchManager;
 import org.sipfoundry.sipxconfig.components.TapestryContext;
+import org.sipfoundry.sipxconfig.components.TapestryUtils;
+import org.sipfoundry.sipxconfig.components.selection.AdaptedSelectionModel;
+import org.sipfoundry.sipxconfig.components.selection.OptGroupPropertySelectionRenderer;
+import org.sipfoundry.sipxconfig.components.selection.OptionAdapter;
 
 public abstract class BranchSelection extends BaseComponent {
 
@@ -28,12 +40,86 @@ public abstract class BranchSelection extends BaseComponent {
     @InjectObject("spring:tapestry")
     public abstract TapestryContext getTapestry();
 
+    @Bean
+    public abstract OptGroupPropertySelectionRenderer getRender();
+
     @Parameter(required = true)
     public abstract Branch getBranch();
 
+    public abstract void setBranch(Branch branch);
+
+    public abstract void setSelectedAction(IActionListener selectedAction);
+
+    public abstract IActionListener getSelectedAction();
+
+    @Override
+    protected void renderComponent(IMarkupWriter writer, IRequestCycle cycle) {
+        Branch branch = getBranch();
+        if (branch != null) {
+            setSelectedAction(new BranchAction(branch));
+        } else {
+            setSelectedAction(null);
+        }
+
+        super.renderComponent(writer, cycle);
+        if (TapestryUtils.isRewinding(cycle, this) && TapestryUtils.isValid(this)) {
+            triggerAction(cycle);
+        }
+    }
+
+    private void triggerAction(IRequestCycle cycle) {
+        IActionListener a = getSelectedAction();
+        if (!(a instanceof BranchAdapter)) {
+            return;
+        }
+
+        BranchAdapter action = (BranchAdapter) a;
+        Branch branch = action.getSelectedBranch();
+        if (branch != null) {
+            action.setId(branch.getId());
+        }
+        action.actionTriggered(this, cycle);
+    }
+
     public IPropertySelectionModel getBranchesModel() {
         List<Branch> branches = getBranchManager().getBranches();
-        IPropertySelectionModel branchSelectionModel = new BranchSelectionModel(branches);
+        Collection<OptionAdapter<Branch>> options = new ArrayList<OptionAdapter<Branch>>();
+        for (Branch branch : branches) {
+            BranchAction adapter = new BranchAction(branch);
+            options.add(adapter);
+        }
+        AdaptedSelectionModel branchSelectionModel = new AdaptedSelectionModel();
+        branchSelectionModel.setCollection(options);
         return getTapestry().instructUserToSelect(branchSelectionModel, getMessages());
+    }
+
+    private class BranchAction extends BranchAdapter {
+        public BranchAction(Branch branch) {
+            super(branch);
+        }
+
+        public void actionTriggered(IComponent component, final IRequestCycle cycle) {
+            setBranch(getSelectedBranch());
+        }
+
+        @Override
+        public Object getValue(Object option, int index) {
+            return this;
+        }
+
+        @Override
+        public String squeezeOption(Object option, int index) {
+            return getSelectedBranch().getId().toString();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return ObjectUtils.equals(this.getSelectedBranch(), ((BranchAction) obj).getSelectedBranch());
+        }
+
+        @Override
+        public int hashCode() {
+            return this.getSelectedBranch().hashCode();
+        }
     }
 }
