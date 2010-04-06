e97fb527e6a7df0f91084ce1e258118bcb986c65
cbeeton <cbeeton@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2010-04-06 18:33:56 +0000

    Add missing headers
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@18505 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/device/BeanFactoryModelSourceTest.java
+++ sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/device/BeanFactoryModelSourceTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2010 Avaya, certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ */
 package org.sipfoundry.sipxconfig.device;
 
 import java.util.Iterator;
--- sipXpage/src/test/java/org/sipfoundry/sipxpage/JitterBufferTest.java
+++ sipXpage/src/test/java/org/sipfoundry/sipxpage/JitterBufferTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2010 Avaya, certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ */
 package sipxpage;
 
 import java.net.DatagramPacket;
