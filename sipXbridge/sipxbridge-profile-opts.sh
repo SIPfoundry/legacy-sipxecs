0382527e80dba372b96683032a27c5c98a71999e
mranga <mranga@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2010-04-30 03:01:16 +0000

    sipxbridge miscellaneous stuff.
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/branches/4.2@18747 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXbridge/sipxbridge-profile-opts.sh
+++ sipXbridge/sipxbridge-profile-opts.sh
@@ -1,3 +1,3 @@
 #You must have jprofiler installed and copy this file to to the @SIPX_CONFDIR@ to enable profiling
 export Action=PROFILE
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/jprofiler5/bin/linux-x86
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/jprofiler6/bin/linux-x86
