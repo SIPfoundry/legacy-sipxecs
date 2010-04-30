fa15a73f1bf1de95922c354e15ca5b48c691f162
mirceac <mirceac@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2010-04-30 21:23:38 +0000

    XX-8289 End User Phonebook : Cannot perform URL dialing using Click To Call.
    Merged revisions 18745 via svnmerge from 
    https://sipxecs.sipfoundry.org/rep/sipXecs/branches/4.2
    
    ........
      r18745 | mirceac | 2010-04-30 02:56:28 +0300 (Fri, 30 Apr 2010) | 7 lines
      
      XX-8289:End User Phonebook : Cannot perform URL dialing using Click To Call
      
      Regular expresions are not 100% compatible with GWT: there are some special characters that cannot be converted
      properly in javascript. Therefore I moved the regular exp validation in CallResource rest service
      -test added
      Note: in GWT spaces are removed from click to call phone number automatically; so no error will be shown when spaces
      are present
    ........
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@18761 ab1d8caa-1f67-47f1-9e81-24633a41865c

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/rest/CallResource.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/rest/CallResource.java
@@ -14,6 +14,7 @@ import org.restlet.data.MediaType;
 import org.restlet.data.Method;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
+import org.restlet.data.Status;
 import org.restlet.resource.Representation;
 import org.sipfoundry.sipxconfig.common.SipUri;
 import org.sipfoundry.sipxconfig.domain.DomainManager;
@@ -21,18 +22,26 @@ import org.sipfoundry.sipxconfig.sip.SipService;
 import org.springframework.beans.factory.annotation.Required;
 
 public class CallResource extends UserResource {
+    private static final String VALID_PHONE_OR_SIP_URI = "([-_.!~*'\\(\\)&=+$,;?/a-zA-Z0-9]|"
+        + "(%[0-9a-fA-F]{2}))+|([-_.!~*'\\(\\)&=+$,;?/a-zA-Z0-9]|"
+        + "(%[0-9a-fA-F]{2}))+@\\w[-._\\w]*\\w\\.\\w{2,6}";
+
     private SipService m_sipService;
     private DomainManager m_domainManager;
     private String m_to;
     private String m_from;
-
+    private Status m_errorStatus;
     @Override
     public void init(Context context, Request request, Response response) {
         super.init(context, request, response);
         String domain = m_domainManager.getDomain().getName();
         String to = (String) getRequest().getAttributes().get("to");
-        m_from = getUser().getAddrSpec(domain);
-        m_to = SipUri.fix(to, domain);
+        if (!to.isEmpty() && to.matches(VALID_PHONE_OR_SIP_URI)) {
+            m_from = getUser().getAddrSpec(domain);
+            m_to = SipUri.fix(to, domain);
+        } else {
+            m_errorStatus = Status.CLIENT_ERROR_BAD_REQUEST;
+        }
 
         // NOTE: Due to the bug in Restlet, it requires PUT and POST request must have
         // entity. The following hack is to workaround the bug.
@@ -53,7 +62,11 @@ public class CallResource extends UserResource {
 
     @Override
     public void put(Representation entity) {
-        m_sipService.sendRefer(getUser(), m_from, m_to);
+        if (m_errorStatus == null) {
+            m_sipService.sendRefer(getUser(), m_from, m_to);
+        } else {
+            getResponse().setStatus(m_errorStatus);
+        }
     }
 
     @Required
