395abd013634f412ae3e90dc96c0a093676e273d
cristi.ciuc <cristi.ciuc@ab1d8caa-1f67-47f1-9e81-24633a41865c>
2010-05-07 12:36:59 +0000

    XX-8294 Phonebook : No provision to download additional contacts in admin portal
    Merged revisions 18807 via svnmerge from 
    https://sipxecs.sipfoundry.org/rep/sipXecs/branches/4.2
    
    ........
      r18807 | cristi.ciuc | 2010-05-07 14:26:41 +0300 (Fri, 07 May 2010) | 10 lines
      
      XX-8294 Phonebook : No provision to download additional contacts in admin portal
      
      Added a component that provides a link to download the phonebook in vCard format
      Since phonebook entries are now kept in the DB no matter their method of inserting in the phonebook,
      it is no longer possible to download the uploaded vcf or csv files.
      
      I have added a text which explains the new behavior on the edit phonebook page.
      
      *note that due to an unknown bug, if an exception is thrown at manager level the error is not displayed to the user
    ........
    
    
    git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@18808 ab1d8caa-1f67-47f1-9e81-24633a41865c

---
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/phonebook/ExportPhonebookComponent.java
@@ -0,0 +1,65 @@
+/*
+ *
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ *
+ *
+ */
+package org.sipfoundry.sipxconfig.site.phonebook;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collection;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hivemind.Messages;
+import org.apache.tapestry.BaseComponent;
+import org.apache.tapestry.annotations.InjectObject;
+import org.apache.tapestry.annotations.Parameter;
+import org.apache.tapestry.valid.ValidationConstraint;
+import org.apache.tapestry.web.WebResponse;
+import org.sipfoundry.sipxconfig.components.SipxValidationDelegate;
+import org.sipfoundry.sipxconfig.components.TapestryUtils;
+import org.sipfoundry.sipxconfig.phonebook.PhonebookEntry;
+import org.sipfoundry.sipxconfig.phonebook.PhonebookManager;
+
+public abstract class ExportPhonebookComponent extends BaseComponent {
+    private static final Log LOG = LogFactory.getLog(ExportPhonebookComponent.class);
+
+    @InjectObject(value = "service:tapestry.globals.WebResponse")
+    public abstract WebResponse getResponse();
+
+    @InjectObject("spring:phonebookManager")
+    public abstract PhonebookManager getPhonebookManager();
+
+    @Parameter(required = true)
+    public abstract SipxValidationDelegate getValidator();
+
+    /**
+     * the name of the phonebook file. It can be the username or the name of the phonebook
+     */
+    @Parameter(required = true)
+    public abstract String getName();
+
+    @Parameter(required = true)
+    public abstract Collection<PhonebookEntry> getEntries();
+
+    public void export() {
+        try {
+            String name = String.format("phonebook_%s.vcf", getName());
+            OutputStream stream = TapestryUtils.getResponseOutputStream(getResponse(), name, "text/x-vcard");
+            Collection<PhonebookEntry> entries = getEntries();
+            getPhonebookManager().exportPhonebook(entries, stream);
+            stream.close();
+        } catch (IOException e) {
+            LOG.error("Cannot export phonebook", e);
+            Messages messages = getMessages();
+            getValidator().record("msg.exportError",
+                    ValidationConstraint.CONSISTENCY);
+        }
+    }
+
+}
--- sipXconfig/web/test/org/sipfoundry/sipxconfig/site/user_portal/UserPhonebookPageTest.java
+++ sipXconfig/web/test/org/sipfoundry/sipxconfig/site/user_portal/UserPhonebookPageTest.java
@@ -5,9 +5,11 @@
  */
 package org.sipfoundry.sipxconfig.site.user_portal;
 
-import java.util.Collection;
-import java.util.Collections;
-
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.easymock.EasyMock.isA;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
 import junit.framework.TestCase;
 
 import org.apache.hivemind.Messages;
@@ -17,17 +19,8 @@ import org.sipfoundry.sipxconfig.common.CoreContext;
 import org.sipfoundry.sipxconfig.common.User;
 import org.sipfoundry.sipxconfig.domain.Domain;
 import org.sipfoundry.sipxconfig.domain.DomainManager;
-import org.sipfoundry.sipxconfig.phonebook.Phonebook;
-import org.sipfoundry.sipxconfig.phonebook.PhonebookEntry;
-import org.sipfoundry.sipxconfig.phonebook.PhonebookManager;
 import org.sipfoundry.sipxconfig.sip.SipService;
 import org.sipfoundry.sipxconfig.test.PhonebookTestHelper;
-
-import static org.easymock.EasyMock.createMock;
-import static org.easymock.EasyMock.expectLastCall;
-import static org.easymock.EasyMock.isA;
-import static org.easymock.EasyMock.replay;
-import static org.easymock.EasyMock.verify;
 
 public class UserPhonebookPageTest extends TestCase {
 
@@ -35,8 +28,6 @@ public class UserPhonebookPageTest extends TestCase {
     private Creator m_pageCreator;
     private User m_portalUser;
     private CoreContext m_coreContext;
-    private PhonebookManager m_phonebookManager;
-    private Collection<PhonebookEntry> m_allEntries;
     private UserPhonebookPage m_out;
     private PhonebookTestHelper m_testHelper;
 
@@ -45,20 +36,12 @@ public class UserPhonebookPageTest extends TestCase {
         m_portalUser = new User();
         m_portalUser.setName("portalUser");
         m_testHelper = new PhonebookTestHelper();
-        m_allEntries = m_testHelper.getPhonebookEntries();
 
         m_coreContext = createMock(CoreContext.class);
         m_testHelper.configureCoreContextMock(m_coreContext);
 
         m_coreContext.loadUser(PORTAL_USER_ID);
         expectLastCall().andReturn(m_portalUser).anyTimes();
-
-        Collection<Phonebook> phonebooks = Collections.singletonList(new Phonebook());
-        m_phonebookManager = createMock(PhonebookManager.class);
-        m_phonebookManager.getAllPhonebooksByUser(m_portalUser);
-        expectLastCall().andReturn(phonebooks).anyTimes();
-        m_phonebookManager.getEntries(phonebooks, m_portalUser);
-        expectLastCall().andReturn(m_allEntries).anyTimes();
 
         m_pageCreator = new Creator();
 
@@ -77,15 +60,10 @@ public class UserPhonebookPageTest extends TestCase {
         expectLastCall().andReturn(domain).anyTimes();
 
         PropertyUtils.write(m_out, "coreContext", m_coreContext);
-        PropertyUtils.write(m_out, "phonebookManager", m_phonebookManager);
         PropertyUtils.write(m_out, "messages", messages);
         PropertyUtils.write(m_out, "domainManager", domainManager);
 
-        replay(m_phonebookManager, m_coreContext, messages, domainManager);
-    }
-
-    public void testGetPhoneBookEntries() {
-        assertEquals(6, m_out.getPhonebookEntries().size());
+        replay(m_coreContext, messages, domainManager);
     }
 
     public void testCall() {
