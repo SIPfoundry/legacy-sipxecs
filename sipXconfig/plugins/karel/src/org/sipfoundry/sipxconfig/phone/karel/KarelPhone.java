313b4b1effe88cde4ab220364f42f7b8ce6e3f6c
Douglas Hubler <douglas@hubler.us>
2010-07-07 23:23:59 -0400

    UC-47 Karel phone support

---
+++ sipXconfig/plugins/karel/src/org/sipfoundry/sipxconfig/phone/karel/KarelPhone.java
@@ -0,0 +1,189 @@
+/*
+ *
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ *
+ * $
+ */
+package org.sipfoundry.sipxconfig.phone.karel;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.sipfoundry.sipxconfig.common.SipUri;
+import org.sipfoundry.sipxconfig.common.User;
+import org.sipfoundry.sipxconfig.device.DeviceDefaults;
+import org.sipfoundry.sipxconfig.device.ProfileContext;
+import org.sipfoundry.sipxconfig.phone.Line;
+import org.sipfoundry.sipxconfig.phone.LineInfo;
+import org.sipfoundry.sipxconfig.phone.Phone;
+import org.sipfoundry.sipxconfig.setting.SettingEntry;
+import org.sipfoundry.sipxconfig.speeddial.SpeedDial;
+
+/**
+ * Helix phone.
+ */
+public class KarelPhone extends Phone {
+    private static final String USER_ID_SETTING = "SIP_ACCOUNT/REGIST_ID";
+    private static final String DISPLAY_NAME_SETTING = "SIP_ACCOUNT/DISP_NAME";
+    private static final String PASSWORD_SETTING = "SIP_ACCOUNT/REGIST_PASS";
+    private static final String REGISTRATION_SERVER_SETTING = "SIP_ACCOUNT/REGIST_SERVER";
+    private static final String REGISTRATION_PORT_SETTING = "SIP_ACCOUNT/RegistrarPort";
+    private static final String MOH = "SIP_ACCOUNT/MOH";
+
+
+    @Override
+    public void initializeLine(Line line) {
+        KarelPhoneDefaults defaults = new KarelPhoneDefaults(getPhoneContext().getPhoneDefaults());
+        addDefaultBeanSettingHandler(defaults);
+        line.addDefaultBeanSettingHandler(new HelixLineDefaults(line));
+    }
+
+    @Override
+    public void initialize() {
+        KarelPhoneDefaults defaults = new KarelPhoneDefaults(getPhoneContext().getPhoneDefaults());
+        addDefaultBeanSettingHandler(defaults);
+    }
+    /*
+    @Override
+    public void initializeLine(Line line) {
+        GrandstreamLineDefaults defaults = new GrandstreamLineDefaults(this, line);
+        line.addDefaultBeanSettingHandler(defaults);
+    }
+*/
+
+
+    @Override
+    public String getProfileFilename() {
+        return getSerialNumber() + ".cfg";
+    }
+
+    public static class HelixLineDefaults {
+        private final Line m_line;
+
+        HelixLineDefaults(Line line) {
+            m_line = line;
+        }
+
+        @SettingEntry(path = USER_ID_SETTING)
+        public String getUserName() {
+            String userName = null;
+            User user = m_line.getUser();
+            if (user != null) {
+                userName = user.getUserName();
+            }
+            return userName;
+        }
+
+        @SettingEntry(path = DISPLAY_NAME_SETTING)
+        public String getDisplayName() {
+            String displayName = null;
+            User user = m_line.getUser();
+            if (user != null) {
+                displayName = user.getDisplayName();
+            }
+            return displayName;
+        }
+
+        @SettingEntry(path = PASSWORD_SETTING)
+        public String getPassword() {
+            String password = null;
+            User user = m_line.getUser();
+            if (user != null) {
+                password = user.getSipPassword();
+            }
+            return password;
+        }
+
+        @SettingEntry(path = REGISTRATION_SERVER_SETTING)
+        public String getRegistrationServer() {
+            DeviceDefaults defaults = m_line.getPhoneContext().getPhoneDefaults();
+            return defaults.getDomainName();
+        }
+
+        @SettingEntry(path = "SIP_ACCOUNT/PROXY_SERVER")
+        public String getProxyServer() {
+            DeviceDefaults defaults = m_line.getPhoneContext().getPhoneDefaults();
+            return defaults.getDomainName();
+//            return defaults.getNtpServer();	checked OK
+//            return ("karelrehaboz.com");  	checked OK
+
+        }
+
+   
+        @SettingEntry(path = MOH)
+        public String getMusicOnHoldUri() {
+            User u = m_line.getUser();
+            if (u != null) {
+                String mohUri = u.getMusicOnHoldUri();
+                return  SipUri.stripSipPrefix(mohUri);
+            }
+            DeviceDefaults defaults = m_line.getPhoneContext().getPhoneDefaults();
+            return defaults.getMusicOnHoldUri();
+        }
+    }
+
+    /**
+     * Each subclass must decide how as much of this generic line information translates into its
+     * own setting model.
+     */
+    @Override
+    protected void setLineInfo(Line line, LineInfo info) {
+        line.setSettingValue(USER_ID_SETTING, info.getUserId());
+        line.setSettingValue(DISPLAY_NAME_SETTING, info.getDisplayName());
+        line.setSettingValue(PASSWORD_SETTING, info.getPassword());
+        line.setSettingValue(REGISTRATION_SERVER_SETTING, info.getRegistrationServer());
+        line.setSettingValue(REGISTRATION_PORT_SETTING, info.getRegistrationServerPort());
+    }
+
+    /**
+     * Each subclass must decide how as much of this generic line information can be contructed
+     * from its own setting model.
+     */
+    @Override
+    protected LineInfo getLineInfo(Line line) {
+        LineInfo info = new LineInfo();
+        info.setDisplayName(line.getSettingValue(DISPLAY_NAME_SETTING));
+        info.setUserId(line.getSettingValue(USER_ID_SETTING));
+        info.setPassword(line.getSettingValue(PASSWORD_SETTING));
+        info.setRegistrationServer(line.getSettingValue(REGISTRATION_SERVER_SETTING));
+         info.setRegistrationServerPort(line.getSettingValue(REGISTRATION_PORT_SETTING));
+        return info;
+    }
+
+    @Override
+    protected ProfileContext createContext() {
+        SpeedDial speedDial = getPhoneContext().getSpeedDial(this);
+        return new KarelProfileContext(this, speedDial, getModel().getProfileTemplate());
+    }
+
+    static class KarelProfileContext extends ProfileContext {
+        private final SpeedDial m_speeddial;
+
+        KarelProfileContext(KarelPhone phone, SpeedDial speeddial, String profileTemplate) {
+            super(phone, profileTemplate);
+            m_speeddial = speeddial;
+        }
+
+        @Override
+        public Map<String, Object> getContext() {
+            Map<String, Object> context = super.getContext();
+            Phone phone = (Phone) getDevice();
+
+            if (m_speeddial != null) {
+                context.put("speeddial", m_speeddial);
+            }
+
+            int speeddialOffset = 0;
+            Collection lines = phone.getLines();
+            if (lines != null) {
+                speeddialOffset = lines.size();
+            }
+            context.put("speeddial_offset", speeddialOffset);
+
+            return context;
+        }
+    }
+}
---
+++ sipXconfig/plugins/karel/src/org/sipfoundry/sipxconfig/phone/karel/KarelPhoneDefaults.java
@@ -0,0 +1,143 @@
+/*
+ *
+ * Copyright (C) 2007 Pingtel Corp., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ *
+ * $
+ */
+package org.sipfoundry.sipxconfig.phone.karel;
+
+import org.sipfoundry.sipxconfig.device.DeviceDefaults;
+import org.sipfoundry.sipxconfig.device.DeviceTimeZone;
+import org.sipfoundry.sipxconfig.setting.SettingEntry;
+
+public class KarelPhoneDefaults {
+    private final DeviceDefaults m_defaults;
+
+    KarelPhoneDefaults(DeviceDefaults defaults) {
+        m_defaults = defaults;
+    }
+
+//  @SettingEntry(path = "NTP_SERVER/PRIMARY_SERVER")  
+    @SettingEntry(path = "TIME_SETTINGS/sntpServer")
+    public String getNtpServer() {
+        return m_defaults.getNtpServer();
+    }
+
+
+/*  Comment Out
+//  @SettingEntry(path = "NTP_SERVER/TIMEZONE") 
+    @SettingEntry(path = "TIME_SETTINGS/TIMEZONE") 
+    public String getTimeZone() {
+        return getTimezoneFromRawOffsetSeconds(getZone().getOffsetInSeconds());
+    }
+
+    GrandstreamPhone.java
+     @SettingEntry(paths = { "upgrade/P192", "upgrade/P237" })
+     public String getTftpServer() {
+         return m_defaults.getTftpServer();
+     }
+*/
+
+
+    private DeviceTimeZone getZone() {
+        return m_defaults.getTimeZone();
+    }
+
+    private static String getTimezoneFromRawOffsetSeconds(int offset) {
+
+        switch (offset) {
+        case -43200:
+            return "-12:00";
+
+        case -39600:
+            return "-11:00";
+
+        case -36000:
+            return "-10:00";
+
+        case -32400:
+            return "-09:00";
+
+        case -28800:
+            return "-08:00";
+
+        case -25200:
+            return "-07:00";
+
+        case -21600:
+            return "-06:00";
+
+        case -18000:
+            return "-05:00";
+
+        case -14400:
+            return "-04:00";
+
+        case -12600:
+            return "-03:30";
+
+        case -10800:
+            return "-03:00";
+
+        case -7200:
+            return "-02:00";
+
+        case -3600:
+            return "-01:00";
+
+        case 3600:
+            return "+01:00";
+
+        case 7200:
+            return "+02:00";
+
+        case 10800:
+            return "+03:00";
+
+        case 12600:
+            return "+03:30";
+
+        case 14400:
+            return "+04:00";
+
+        case 16200:
+            return "+04:30";
+
+        case 18000:
+            return "+05:00";
+
+        case 19800:
+            return "+05:30";
+
+        case 21600:
+            return "+06:00";
+
+        case 25200:
+            return "+07:00";
+
+        case 28800:
+            return "+08:00";
+
+        case 32400:
+            return "+09:00";
+
+        case 34200:
+            return "+09:30";
+
+        case 36000:
+            return "+10:00";
+
+        case 39600:
+            return "+11:00";
+
+        case 43200:
+            return "+12:00";
+
+        case 0:
+        default: // GMT by default
+            return "+0:00";
+        }
+    }
+}
