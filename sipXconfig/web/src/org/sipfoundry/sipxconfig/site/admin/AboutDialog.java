891a64adca7d03e630f78e13a81c304931b421c7
Douglas Hubler <dhubler@ezuce.com>
2010-07-23 12:47:19 -0400

    FIX-ABOUT-HEADERS

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/AboutDialog.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/AboutDialog.java
@@ -1,11 +1,17 @@
 /*
  *
  *
- * Copyright (C) 2010 Pingtel Corp., certain elements licensed under a Contributor Agreement.
- * Contributors retain copyright to elements licensed under a Contributor Agreement.
- * Licensed to the User under the LGPL license.
+ * Copyright (c) 2010 eZuce, Inc. All rights reserved.
  *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 package org.sipfoundry.sipxconfig.site.admin;
 
