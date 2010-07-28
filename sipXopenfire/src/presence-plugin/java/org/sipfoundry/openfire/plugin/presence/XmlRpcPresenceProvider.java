0e4d23414cc3dd12d0c72661e41804dd1f4defb7
Mircea Carasel <mirceac@decebal.buc.ro>
2010-07-29 02:31:38 +0300

    UC-85: Allow queriing user's presence based on email address
    -added new method in XmlRpcPresenceProvider: public Map getXmppUsername(String emailAddress)
    that returns username that matches the given address
    when there is no user with that email address an error message is returned instead
    also, it is assumed that there cannot be two or more users with the same email address
    if by accident there are two users with the same email address only one is returned

--- sipXopenfire/src/presence-plugin/java/org/sipfoundry/openfire/plugin/presence/XmlRpcPresenceProvider.java
+++ sipXopenfire/src/presence-plugin/java/org/sipfoundry/openfire/plugin/presence/XmlRpcPresenceProvider.java
@@ -6,23 +6,54 @@
 package org.sipfoundry.openfire.plugin.presence;
 
 import java.io.NotActiveException;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+
 import org.apache.log4j.Logger;
 import org.xmpp.packet.JID;
+import org.jivesoftware.openfire.user.UserManager;
+import org.jivesoftware.openfire.XMPPServer;
+import org.jivesoftware.openfire.user.User;
+
+
 
 public class XmlRpcPresenceProvider extends XmlRpcProvider {
- 
+
     private static String myHandle = "of:" + Math.abs(new Random().nextLong());
 
     public static final String SERVICE_PATH = "/plugins/sipx-openfire-presence/status";
     public static final String SERVICE_NAME = "status";
     public static final String SERVER = "presenceServer";
 
-    private Logger log = Logger.getLogger(XmlRpcPresenceProvider.class); 
+    private final Logger log = Logger.getLogger(XmlRpcPresenceProvider.class);
 
     public XmlRpcPresenceProvider() {
-  
+
+    }
+
+    /*
+     * It returns the xmpp username being given user's address
+     */
+    public Map getXmppUsername(String emailAddress) {
+        XMPPServer server = XMPPServer.getInstance();
+        UserManager userManager = server.getUserManager();
+
+        Set searchFields = new HashSet();
+        searchFields.add(EMAIL_FIELD);
+        Collection<User> users =  userManager.findUsers(searchFields, emailAddress);
+
+        if ( users == null || users.size() == 0 ) {
+           Map errorMap = createErrorMap(ErrorCode.SIP_ID_NOT_FOUND, "No user matches given email address");
+           return errorMap;
+        }
+        Map retval = createSuccessMap();
+        User user = users.iterator().next();
+        retval.put(SIP_ID, user.getUsername());
+
+        return retval;
     }
 
     public Map getPresenceState( String xmppUsername ) {
@@ -31,7 +62,7 @@ public class XmlRpcPresenceProvider extends XmlRpcProvider {
             log.info("GetPresenceState " + jid);
             assertPlugInReady();
             Map retval = createSuccessMap();
-            UnifiedPresence unifiedPresence = 
+            UnifiedPresence unifiedPresence =
                   PresenceUnifier.getInstance().getUnifiedPresence( xmppUsername );
             retval.put(XMPP_PRESENCE, unifiedPresence.getXmppPresence().toString());
             return retval;
@@ -48,15 +79,15 @@ public class XmlRpcPresenceProvider extends XmlRpcProvider {
             return createErrorMap(ErrorCode.USER_NOT_FOUND, e.getMessage());
         }
     }
-    
+
     public Map setPresenceState (String xmppUsername , String xmppPresenceAsString ) {
         try {
             String jid = appendDomain(xmppUsername);
             log.info("setPresenceState" + jid + " XMPP presence " + xmppPresenceAsString);
             assertPlugInReady();
             Map retval = createSuccessMap();
-            UnifiedPresence.XmppPresence xmppPresence = UnifiedPresence.XmppPresence.valueOf( xmppPresenceAsString ); 
-            getPlugin().setPresenceState(jid, xmppPresence);   
+            UnifiedPresence.XmppPresence xmppPresence = UnifiedPresence.XmppPresence.valueOf( xmppPresenceAsString );
+            getPlugin().setPresenceState(jid, xmppPresence);
             return retval;
         }
         catch( NotActiveException e ){
@@ -68,16 +99,16 @@ public class XmlRpcPresenceProvider extends XmlRpcProvider {
             return createErrorMap(ErrorCode.USER_NOT_FOUND, ex.getMessage());
         }
     }
-    
+
     public Map setPresenceStatus(String id, String status) {
         try {
             String jid = appendDomain(id);
             log.info("setPresenceStatus " + jid + " status = " + status);
             assertPlugInReady();
             Map retval = createSuccessMap();
-            getPlugin().setPresenceStatus(jid,  status);   
+            getPlugin().setPresenceStatus(jid,  status);
             return retval;
-        } 
+        }
         catch( NotActiveException e ){
             log.error("plug-in not active error: ",e);
             return createErrorMap(ErrorCode.NOT_READY,e.getMessage());
@@ -87,7 +118,7 @@ public class XmlRpcPresenceProvider extends XmlRpcProvider {
             return createErrorMap(ErrorCode.USER_NOT_FOUND, ex.getMessage());
         }
     }
-    
+
     public Map getPresenceStatus(String id) {
         try {
             String jid = appendDomain(id);
@@ -99,7 +130,7 @@ public class XmlRpcPresenceProvider extends XmlRpcProvider {
                 retval.put(CUSTOM_PRESENCE_MESSAGE, presenceStatus);
             }
             return retval;
-        } 
+        }
         catch( NotActiveException e ){
             log.error("plug-in not active error: ",e);
             return createErrorMap(ErrorCode.NOT_READY,e.getMessage());
@@ -143,7 +174,7 @@ public class XmlRpcPresenceProvider extends XmlRpcProvider {
            log.debug("xmppUser = " + xmppUser);
            Map retval = createSuccessMap();
            JID jid = new JID(xmppUser);
-           UnifiedPresence unifiedPresence 
+           UnifiedPresence unifiedPresence
                    = PresenceUnifier.getInstance().getUnifiedPresence( jid.getNode());
            String xmppPresence = unifiedPresence.getXmppPresence().toString();
 
@@ -154,7 +185,7 @@ public class XmlRpcPresenceProvider extends XmlRpcProvider {
            retval.put(UNIFIED_PRESENCE, unifiedPresence.getUnifiedPresence());
            retval.put(CUSTOM_PRESENCE_MESSAGE, unifiedPresence.getXmppStatusMessageWithSipState());
            return retval;
-        } 
+        }
         catch( NotActiveException e ){
             log.error("plug-in not active error: ",e);
             return createErrorMap(ErrorCode.NOT_READY,e.getMessage());
@@ -163,13 +194,13 @@ public class XmlRpcPresenceProvider extends XmlRpcProvider {
             log.error("Processing error",ex);
             return createErrorMap(ErrorCode.PROCESSING_ERROR,ex.getMessage());
         }
-        
+
     }
-    
-    public Map registerPresenceMonitor(String protocol, String serverUrl ) 
+
+    public Map registerPresenceMonitor(String protocol, String serverUrl )
     {
         try {
-            log.info("registerPresenceMonitor " + protocol + " " + serverUrl );            
+            log.info("registerPresenceMonitor " + protocol + " " + serverUrl );
             assertPlugInReady();
             PresenceUnifier.getInstance().addUnifiedPresenceChangeListener( protocol, serverUrl );
             Map retval = createSuccessMap();
@@ -181,12 +212,12 @@ public class XmlRpcPresenceProvider extends XmlRpcProvider {
             return createErrorMap(ErrorCode.NOT_READY,e.getMessage());
         }
     }
-     
-    public Map ping( String originatorName ) 
+
+    public Map ping( String originatorName )
     {
         try
         {
-            log.info("ping received from " + originatorName );            
+            log.info("ping received from " + originatorName );
             assertPlugInReady();
             Map retval = createSuccessMap();
             retval.put(INSTANCE_HANDLE, myHandle);
@@ -197,7 +228,7 @@ public class XmlRpcPresenceProvider extends XmlRpcProvider {
             return createErrorMap(ErrorCode.NOT_READY,e.getMessage());
         }
     }
-    
+
     private void assertPlugInReady() throws NotActiveException
     {
         if( getPlugin().isInitialized() == false )
--- sipXopenfire/src/presence-plugin/java/org/sipfoundry/openfire/plugin/presence/XmlRpcProvider.java
+++ sipXopenfire/src/presence-plugin/java/org/sipfoundry/openfire/plugin/presence/XmlRpcProvider.java
@@ -22,44 +22,46 @@ public abstract class XmlRpcProvider {
     public static final String ERROR_INFO = "error-info";
 
     public static final String XMPP_PRESENCE = "xmpp-presence";
-    
+
     public static final String CUSTOM_PRESENCE_MESSAGE = "custom-presence-message";
-    
+
     public static final String ACCOUNT_EXISTS = "account-exists";
-    
+
     public static final String SIP_ID = "sip-id";
-    
+
     public static final String SIP_PASSWORD = "sip-password";
-    
+
     public static final String JABBER_ID = "jabber-id";
-    
+
     public static final String SIP_PRESENCE = "sip-presence";
-    
+
     public static final String UNIFIED_PRESENCE = "unified-presence";
-    
+
     public static final String INSTANCE_HANDLE = "instance-handle";
-    
+
     public static final String ON_THE_PHONE_MESSAGE = "on-the-phone-messasge";
-    
+
     public static final String GROUP_EXISTS = "group-exists";
 
     public static final String USER_IN_GROUP = "is-user-in-group";
-    
+
     public static final String ROOM_MEMBERS = "room-members";
-    
+
     public static final String IS_LIST_ROOM_IN_DIRECTORY = "is-list-room-in-directory";
 
     public static final String CONFERENCE_BRIDGE_EXTENSION = "conference-bridge-extension";
-    
+
     public static final String SIP_USER_NAME = "sip-user-name";
-    
+
     public static final String XMPP_USER_NAME = "xmpp-user-name";
-    
+
     public static final String USER_ACCOUNTS = "user-accounts";
-    
+
+    public static final String EMAIL_FIELD = "Email";
+
     private static SipXOpenfirePlugin plugin ;
-    
-    
+
+
     protected Map<String, Object> createErrorMap(ErrorCode errorCode, String reason) {
         Map<String, Object> retval = new HashMap<String, Object>();
         retval.put(STATUS_CODE, ERROR);
@@ -73,7 +75,7 @@ public abstract class XmlRpcProvider {
         retval.put(STATUS_CODE, OK);
         return retval;
     }
-    
+
     protected static String appendDomain(String userName) {
         if (  userName.indexOf("@") == -1) {
             // No @ in the domain so assume this is our domain.
