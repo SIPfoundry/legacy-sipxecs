ab8b3f6d07141824c4b6734cacbe7bc03b884c02
George Niculae <george@ezuce.com>
2010-12-20 18:12:11 +0200

    XX-9237: fix indentation

--- sipXivr/src/main/java/org/sipfoundry/sipxivr/SipXivr.java
+++ sipXivr/src/main/java/org/sipfoundry/sipxivr/SipXivr.java
@@ -160,9 +160,9 @@ public class SipXivr implements Runnable {
                         VoiceMail app = new VoiceMail(s_config, m_fses, parameters);
                         app.run();
                     } else if (action.equals("faxrx")) {
-			// Run fax receive application
-			FaxRx app = new FaxRx(s_config, m_fses, parameters);
-			app.run() ;
+                        // Run fax receive application
+                        FaxRx app = new FaxRx(s_config, m_fses, parameters);
+                        app.run() ;
                     } else if (action.equals("moh")) {
                         // Run Music On Hold
                         Moh app = new Moh(s_config, m_fses, parameters);
