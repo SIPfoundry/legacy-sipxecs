5ee25eaefd5d8f7d0564707549c30d3ea23d51db
Mircea Carasel <mirceac@ezuce.com>
2011-01-20 16:31:41 +0200

    XX-9352: sipxIvr: MailboxServlet: xml response should follow XML specifiation
    -surrounded attributes with quotes, added voicemail author information
    (user name if available, user id otherwise)

---
+++ sipXcommons/src/main/java/org/sipfoundry/commons/util/SipUriUtil.java
@@ -0,0 +1,49 @@
+/*
+ *
+ *
+ * Copyright (C) 2011 eZuce, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package org.sipfoundry.commons.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SipUriUtil {
+    private static final Pattern EXTRACT_USER_RE = Pattern.compile("\\s*<?(?:sip:)?(.+?)[@;].+");
+    private static final Pattern EXTRACT_FULL_USER_RE = Pattern
+            .compile("\\s*(?:\"?\\s*([^\"<]+?)\\s*\"?)?\\s*<(?:sip:)?(.+?)[@;].+");
+
+    /**
+     * Extracts user name if available. Otherwise it returns the user id
+     */
+    public static String extractUserName(String uri) {
+        if (uri == null) {
+            return null;
+        }
+        Matcher matcher = EXTRACT_FULL_USER_RE.matcher(uri);
+        if (!matcher.matches()) {
+            matcher = EXTRACT_USER_RE.matcher(uri);
+            if (matcher.matches()) {
+                return matcher.group(1);
+            }
+            return null;
+        }
+        String fullName = matcher.group(1);
+        String userId = matcher.group(2);
+
+        if (fullName == null || fullName.equals(userId)) {
+            return userId;
+        }
+        return fullName;
+    }
+}
