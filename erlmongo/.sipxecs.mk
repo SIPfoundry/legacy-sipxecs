b7759a0247d590f63b310ab2b4b6714f227bbff2
Douglas Hubler <dhubler@ezuce.com>
2011-01-28 14:49:05 -0500

    XX-9320 - make erlmongo an rpm and move source around

---
+++ erlmongo/.sipxecs.mk
@@ -0,0 +1,18 @@
+erlmongo_VER = 0.0.9
+erlmongo_REV = 1
+erlmongo_SPEC = $(SRC)/erlmongo/erlmongo.spec
+erlmongo_SOURCES = $(erlmongo_TARBALL)
+erlmongo_SRPM = erlmongo-$(erlmongo_VER)-$(erlmongo_REV)$(RPM_DIST).src.rpm
+erlmongo_TARBALL = erlmongo-$(erlmongo_VER).tar.gz
+
+erlmongo.autoreconf erlmongo.configure erlmongo.dist:;
+
+help.erlmongo.update_dist = Called manually when you want to pull in new changes from erlmongo project
+erlmongo.update_dist :
+	git clone git://github.com/wpntv/erlmongo.git
+	cd erlmongo; \
+	  ./configure; \
+	  make dist
+	mv erlmongo/erlmongo-*.tar.gz $(SRC)/erlmongo
+
+
