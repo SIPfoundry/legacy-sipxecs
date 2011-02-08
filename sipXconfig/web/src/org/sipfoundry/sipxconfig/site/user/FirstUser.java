8ef75e9e6fef9f68fa3bc11891fbfced74d038bf
Mircea Carasel <mirceac@ezuce.com>
2011-02-08 21:16:56 +0200

    XX-9409: Extend sipXconfig license pluggable mechanism to support multiple license files
    many licenses files are accepted. Each file content will be displayed upon a button click
    the plugin should override names such as: license.txt, 1_license.txt, 2_license.txt etc
    updated Tapestry Border component to be able to use it on FirstUserPage
    -Tests updated

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/user/FirstUser.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/user/FirstUser.java
@@ -9,14 +9,19 @@
  */
 package org.sipfoundry.sipxconfig.site.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.tapestry.IAsset;
 import org.apache.tapestry.PageRedirectException;
+import org.apache.tapestry.annotations.Asset;
 import org.apache.tapestry.annotations.Bean;
 import org.apache.tapestry.annotations.InjectObject;
 import org.apache.tapestry.annotations.Persist;
 import org.apache.tapestry.event.PageBeginRenderListener;
 import org.apache.tapestry.event.PageEvent;
 import org.sipfoundry.sipxconfig.common.CoreContext;
+import org.sipfoundry.sipxconfig.common.VersionInfo;
 import org.sipfoundry.sipxconfig.components.SipxBasePage;
 import org.sipfoundry.sipxconfig.components.SipxValidationDelegate;
 import org.sipfoundry.sipxconfig.components.TapestryContext;
@@ -32,6 +37,9 @@ public abstract class FirstUser extends SipxBasePage implements PageBeginRenderL
     @InjectObject(value = "spring:tapestry")
     public abstract TapestryContext getTapestry();
 
+    @InjectObject(value = "spring:versionInfo")
+    public abstract VersionInfo getVersionInfo();
+
     @Bean
     public abstract SipxValidationDelegate getValidator();
 
@@ -42,11 +50,38 @@ public abstract class FirstUser extends SipxBasePage implements PageBeginRenderL
     @Persist
     public abstract boolean isLicenseAccepted();
 
-    public abstract void setLicense(String license);
+    public abstract int getPos();
 
     public abstract void setRenderLicense(boolean renderLicense);
 
     public abstract boolean getRenderLicense();
+
+    @Asset("context:/WEB-INF/user/FirstUser.script")
+    public abstract IAsset getFirstUserScript();
+
+    public  String getLicense(int i) {
+        return getTapestry().getLicense(i);
+    }
+
+    public String getLicenseTitle(int i) {
+        String prefix = "";
+        if (i > 0) {
+            prefix = i + "_";
+        }
+        return getMessages().getMessage(prefix + "license.title");
+    }
+
+    public List<Integer> getLicensesNumber() {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < getTapestry().getLicensesNumber(); i++) {
+            list.add(new Integer(i));
+        }
+        return list;
+    }
+
+    public String getDetails() {
+        return getMessages().format("product.details", getVersionInfo().getVersionDetails());
+    }
 
     public void pageBeginRender(PageEvent event) {
         // This page runs only when there are no users, and the first user
@@ -59,7 +94,6 @@ public abstract class FirstUser extends SipxBasePage implements PageBeginRenderL
         // Render licence only if it's required but not yet accepted.
         if (getTapestry().isLicenseRequired() && !isLicenseAccepted()) {
             setRenderLicense(true);
-            setLicense(getTapestry().getLicense());
         } else {
             setRenderLicense(false);
         }
