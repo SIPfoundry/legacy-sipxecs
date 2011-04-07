46a74b238dcfca98440c04dbcce2109a7dabdc41
Mircea Carasel <mirceac@ezuce.com>
2011-04-07 18:22:51 +0300

    XX-9430 Error message not displayed in presence server
    -fixed error message display on presence server page
    -localized error message by improving ConflictingFeatureCodeException
    -added AliasOwner to sipxregistrar service. in this way users/conferences etc
    will not be allowed to contain extensions used in sipxregistrar
    -ui test written

--- sipXconfig/web/test/org/sipfoundry/sipxconfig/site/service/EditSipxServiceTestUi.java
+++ sipXconfig/web/test/org/sipfoundry/sipxconfig/site/service/EditSipxServiceTestUi.java
@@ -66,4 +66,26 @@ public class EditSipxServiceTestUi extends WebTestCase {
         initService("seedRelayService");
         SiteTestHelper.assertNoUserError(tester);
     }
+
+    public void testPresenceServiceDisplay() {
+        initService("seedPresenceService");
+        SiteTestHelper.assertNoException(tester);
+        assertTextPresent("SIP_PRESENCE_SIGN_IN_CODE");
+        assertTextNotPresent("SIP_PRESENCE_LOG_LEVEL");
+    }
+
+    public void testPresenceServiceConflictCode() {
+	initService("seedRegistrarService");
+	setTextField("setting:SIP_REDIRECT.100-PICKUP.DIRECTED_CALL_PICKUP_CODE", "*78");
+	clickButton("form:apply");
+        SiteTestHelper.assertNoException(tester);
+        SiteTestHelper.assertNoUserError(tester);
+        initService("seedPresenceService");
+        setTextField("setting:SIP_PRESENCE_SIGN_IN_CODE", "*78");
+        clickButton("form:apply");
+        SiteTestHelper.assertUserError(tester);
+        setTextField("setting:SIP_PRESENCE_SIGN_IN_CODE", "*89");
+        clickButton("form:apply");
+        SiteTestHelper.assertNoUserError(tester);
+    }
 }
