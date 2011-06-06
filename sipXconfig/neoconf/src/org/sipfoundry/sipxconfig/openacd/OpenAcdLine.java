4cc138e2a954420b227af30f29dcffeb731f7d1d
laurentiu <laurentiu@ezuce.com>
2011-06-06 21:17:12 +0300

    XX-9578 - Caller able to leave a VM while waiting in queue even the VM option is disabled in the Call Center Line

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/openacd/OpenAcdLine.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/openacd/OpenAcdLine.java
@@ -45,7 +45,8 @@ public class OpenAcdLine extends OpenAcdExtension {
     }
 
     public static FreeswitchAction createVoicemailAction(boolean allow) {
-        return createAction(FreeswitchAction.PredefinedAction.set.toString(), ALLOW_VOICEMAIL + allow);
+        return allow ? createAction(FreeswitchAction.PredefinedAction.set.toString(), ALLOW_VOICEMAIL + allow)
+                : null;
     }
 
     public static FreeswitchAction createQueueAction(String queue) {
