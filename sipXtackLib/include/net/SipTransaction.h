11c2907155d827c9ee194b855a0463ca264cf0e3
Joegen Baclor <jbaclor@ezuce.com>
2011-06-13 19:58:36 +0800

    recourseDnsChildren do not take into account TLS resulting to a NULL return value when only TLS is the sole protocol configured

--- sipXtackLib/include/net/SipTransaction.h
+++ sipXtackLib/include/net/SipTransaction.h
@@ -358,7 +358,8 @@ protected:
     //: Starts search on any immediate children of the highest unpursued Q value
 
     UtlBoolean recurseDnsSrvChildren(SipUserAgent& userAgent,
-                              SipTransactionList& transactionList);
+                              SipTransactionList& transactionList,
+                              SipMessage* pRequest = 0);
     //: Starts search on any immediate DNS SRV children of the highest unpursued Q value
 
     /// Copy unique realms from any proxy challenges in the response into realmList
