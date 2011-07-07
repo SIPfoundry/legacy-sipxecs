969fde255c5c86844c540e0b0f727a3346d0b51f
Joegen Baclor <jbaclor@ezuce.com>
2011-07-07 14:24:22 +0800

    XX-9757 - Leak in Sip Unified presence redirector. When inserting new presence monitor information, the old value being overwritten is not deleted. This is noticed more frequently with systems that have large amounts of users that use the IM functionality.

--- sipXregistry/lib/redirect_plugins/SipRedirectorPresenceRouting.cpp
+++ sipXregistry/lib/redirect_plugins/SipRedirectorPresenceRouting.cpp
@@ -122,7 +122,10 @@ UnifiedPresenceContainer* UnifiedPresenceContainer::getInstance( void )
 void UnifiedPresenceContainer::insert( UtlString* pAor, UnifiedPresence* pUnifiedPresence )
 {
     OsLock lock( mMutex );
-    mUnifiedPresences.remove( pAor );
+    UnifiedPresence* pOldUnifiedPresence = (UnifiedPresence*)mUnifiedPresences.remove( pAor );
+    if (pOldUnifiedPresence)
+      delete pOldUnifiedPresence;
+
     mUnifiedPresences.insertKeyAndValue( pAor, pUnifiedPresence );
     Os::Logger::instance().log(FAC_SIP, PRI_DEBUG, "UnifiedPresenceContainer::insert "
                   "Presence information for '%s':\r\n"
