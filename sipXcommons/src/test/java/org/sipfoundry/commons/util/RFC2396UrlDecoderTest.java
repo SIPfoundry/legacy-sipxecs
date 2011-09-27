3895c8283d338cc210a5d45a64798a60b6b5d8ba
Douglas Hubler <dhubler@ezuce.com>
2011-09-27 13:55:18 -0400

    Extra tests for parsing uris

--- sipXcommons/src/test/java/org/sipfoundry/commons/util/RFC2396UrlDecoderTest.java
+++ sipXcommons/src/test/java/org/sipfoundry/commons/util/RFC2396UrlDecoderTest.java
@@ -7,9 +7,13 @@
  */
 package org.sipfoundry.commons.util;
 
-import junit.framework.Test;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.apache.commons.io.IOUtils;
 
 public class RFC2396UrlDecoderTest extends TestCase {
 
@@ -23,5 +27,14 @@ public class RFC2396UrlDecoderTest extends TestCase {
        assertEquals(url = "a@example.com%", RFC2396UrlDecoder.decode(url));
        assertEquals(url = "a@example.com%3", RFC2396UrlDecoder.decode(url));
        assertEquals("a@example.com0", RFC2396UrlDecoder.decode("a@example.com%30"));
+   }
+   
+   public void testSampling() throws IOException {
+       InputStream urisIn = RFC2396UrlDecoderTest.class.getResourceAsStream("sample-uris.txt");
+       List uris = IOUtils.readLines(urisIn);
+       for (Object uri : uris) {
+           RFC2396UrlDecoder.decode((String)uri);
+       }
+       IOUtils.closeQuietly(urisIn);
    }
 }
