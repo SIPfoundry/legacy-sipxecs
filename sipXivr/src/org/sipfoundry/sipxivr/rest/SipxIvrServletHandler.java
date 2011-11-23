700bc89c3efa112417ac9252ff3b2aa19d82a713
dizzy <george@ezuce.com>
2011-11-23 13:24:15 +0200

    XX-9976: port sipXivr to Spring
    - header for servlet handler

--- sipXivr/src/org/sipfoundry/sipxivr/rest/SipxIvrServletHandler.java
+++ sipXivr/src/org/sipfoundry/sipxivr/rest/SipxIvrServletHandler.java
@@ -1,3 +1,12 @@
+/*
+ *
+ *
+ * Copyright (C) 2011 eZuce Inc., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the AGPL license.
+ *
+ * $
+ */
 package org.sipfoundry.sipxivr.rest;
 
 import java.util.Map;
