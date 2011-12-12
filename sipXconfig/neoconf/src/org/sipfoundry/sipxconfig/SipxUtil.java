073da203d1edb3139be22c39b5f21cf3dc1aa12d
Mircea Carasel <mirceac@ezuce.com>
2011-12-12 12:39:48 +0200

    Fix line too long in SipxUtil.java

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/SipxUtil.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/SipxUtil.java
@@ -19,9 +19,9 @@ package org.sipfoundry.sipxconfig;
 import java.io.File;
 import java.io.IOException;
 /**
- * This is a utility class with static utilities methods that can be used in sipxecs source and test layers as well
- * This class should not contain any dependencies on test libraries, otherwise the source sipxecs layer won't be ale to load
- * this class
+ * This is a utility class with static utilities methods that can be used in sipxecs source and
+ * test layers as well This class should not contain any dependencies on test libraries, otherwise
+ * the source sipxecs layer won't be ale to load this class
  */
 public final class SipxUtil {
     private SipxUtil() {
