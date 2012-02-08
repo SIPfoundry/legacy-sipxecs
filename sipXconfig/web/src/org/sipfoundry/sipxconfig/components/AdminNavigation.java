23d26fc839a680735de2a2db6a54dcd744fd6cc2
Douglas Hubler <dhubler@ezuce.com>
2012-02-08 08:42:11 -0500

    dial plans not showing up on admin menu

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/components/AdminNavigation.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/components/AdminNavigation.java
@@ -9,15 +9,24 @@
  */
 package org.sipfoundry.sipxconfig.components;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.tapestry.BaseComponent;
 import org.apache.tapestry.annotations.ComponentClass;
 import org.apache.tapestry.annotations.InjectObject;
 import org.sipfoundry.sipxconfig.common.CoreContext;
+import org.sipfoundry.sipxconfig.feature.FeatureManager;
+import org.sipfoundry.sipxconfig.feature.GlobalFeature;
+import org.sipfoundry.sipxconfig.feature.LocationFeature;
 import org.sipfoundry.sipxconfig.sbc.SbcDeviceManager;
 import org.sipfoundry.sipxconfig.update.PackageUpdateManager;
 
 @ComponentClass(allowBody = false, allowInformalParameters = false)
 public abstract class AdminNavigation extends BaseComponent {
+
+    @InjectObject("spring:featureManager")
+    public abstract FeatureManager getFeatureManager();
 
     @InjectObject("spring:coreContext")
     public abstract CoreContext getContext();
@@ -27,4 +36,35 @@ public abstract class AdminNavigation extends BaseComponent {
 
     @InjectObject("spring:sbcDeviceManager")
     public abstract SbcDeviceManager getSbcDeviceManager();
+
+    public abstract Set<String> getEnabled();
+
+    public abstract void setEnabled(Set<String> enabled);
+
+    public boolean isOn(String featureId) {
+        return safeGetEnabled().contains(featureId);
+    }
+
+    Set<String> safeGetEnabled() {
+        Set<String> enabled = getEnabled();
+        if (enabled == null) {
+            enabled = getEnabledFeatures(getFeatureManager());
+        }
+
+        return enabled;
+    }
+
+    public static Set<String> getEnabledFeatures(FeatureManager manager) {
+        // merge global and local features into one pool
+        Set<String> enabled = new HashSet<String>();
+        Set<GlobalFeature> global = manager.getEnabledGlobalFeatures();
+        for (GlobalFeature f : global) {
+            enabled.add(f.getId());
+        }
+        Set<LocationFeature> local = manager.getEnabledLocationFeatures();
+        for (LocationFeature f : local) {
+            enabled.add(f.getId());
+        }
+        return enabled;
+    }
 }
--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/InternalPlugins.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/InternalPlugins.java
@@ -7,14 +7,12 @@
  */
 package org.sipfoundry.sipxconfig.site;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.tapestry.annotations.InjectObject;
 import org.apache.tapestry.html.BasePage;
+import org.sipfoundry.sipxconfig.components.AdminNavigation;
 import org.sipfoundry.sipxconfig.feature.FeatureManager;
-import org.sipfoundry.sipxconfig.feature.GlobalFeature;
-import org.sipfoundry.sipxconfig.feature.LocationFeature;
 
 public abstract class InternalPlugins extends BasePage {
 
@@ -28,15 +26,7 @@ public abstract class InternalPlugins extends BasePage {
     Set<String> safeGetEnabled() {
         Set<String> enabled = getEnabled();
         if (enabled == null) {
-            enabled = new HashSet<String>();
-            Set<GlobalFeature> global = getFeatureManager().getEnabledGlobalFeatures();
-            for (GlobalFeature f : global) {
-                enabled.add(f.getId());
-            }
-            Set<LocationFeature> local = getFeatureManager().getEnabledLocationFeatures();
-            for (LocationFeature f : local) {
-                enabled.add(f.getId());
-            }
+            enabled = AdminNavigation.getEnabledFeatures(getFeatureManager());
         }
 
         return enabled;
