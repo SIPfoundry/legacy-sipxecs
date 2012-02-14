4aba500d44e610d2f0c4f4dcd01b8ab10798e9b6
dizzy <george@ezuce.com>
2012-02-14 02:04:54 +0200

    style fix

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/im/ImManager.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/im/ImManager.java
@@ -16,7 +16,7 @@ import org.sipfoundry.sipxconfig.feature.LocationFeature;
 public abstract class ImManager  {
     public static final LocationFeature FEATURE = new LocationFeature("instantMessage");
     public static final AddressType XMPP_ADDRESS = new AddressType("instantMessageXmpp");
-    public static final AddressType XMLRPC_ADDRESS = new AddressType("instantMessageXmlrpc", 
+    public static final AddressType XMLRPC_ADDRESS = new AddressType("instantMessageXmlrpc",
         "http://%s:%d/xmlrpc");
     public static final AddressType WATCHER_ADDRESS = new AddressType("instantMessageWatcher");
 }
