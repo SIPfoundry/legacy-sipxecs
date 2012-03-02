a6ce04844574473396ce3dd6fc0702a700124c45
Douglas Hubler <dhubler@ezuce.com>
2012-03-02 16:59:14 -0500

    create plsql as a patch, f16 already has psql

--- sipXconfig/etc/sipxpbx/database/migrate-pregroup-alarm-contacts.sql
+++ sipXconfig/etc/sipxpbx/database/migrate-pregroup-alarm-contacts.sql
@@ -1,19 +1,19 @@
-create or replace function migrate_pregroup_alarm_contacts() returns void as '\n
-declare\n
-  index_contact integer;\n
-  contact record;\n
-  default_group_id integer;\n
-begin\n
-  SELECT alarm_group_id INTO default_group_id FROM alarm_group WHERE name = ''default'';\n
-\n
-  index_contact := 0;\n
-\n
-  for contact in select * from alarm_contacts loop\n
-    insert into alarm_group_emailcontacts values(default_group_id, contact.address, index_contact);\n
-	index_contact := index_contact + 1;\n
-  end loop;\n
-end;\n
-' language plpgsql;
+create or replace function migrate_pregroup_alarm_contacts() returns void as $$
+declare
+  index_contact integer;
+  contact record;
+  default_group_id integer;
+begin
+  SELECT alarm_group_id INTO default_group_id FROM alarm_group WHERE name = 'default';
+
+  index_contact := 0;
+
+  for contact in select * from alarm_contacts loop
+    insert into alarm_group_emailcontacts values(default_group_id, contact.address, index_contact);
+	index_contact := index_contact + 1;
+  end loop;
+end;
+$$ language plpgsql;
 
 select migrate_pregroup_alarm_contacts();
 
