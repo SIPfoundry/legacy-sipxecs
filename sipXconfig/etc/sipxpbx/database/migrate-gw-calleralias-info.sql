5f37d2f45d56ad00dfc3ca84433a92be1cbb56f6
Douglas Hubler <dhubler@ezuce.com>
2012-03-02 13:15:52 -0500

    fix bugs on f16, snmpd not starting on reboot, logwatcher not starting at all

--- sipXconfig/etc/sipxpbx/database/migrate-gw-calleralias-info.sql
+++ sipXconfig/etc/sipxpbx/database/migrate-gw-calleralias-info.sql
@@ -1,15 +1,13 @@
-drop language if exists plpgsql cascade;
-create language plpgsql;
-create or replace function migrate_gcai() returns void as '\n
-declare\n
-  gw_rec record;\n
-begin\n
- for gw_rec in select * from gateway gw where gw.enable_caller_id=true\n
-  loop\n
-    update gateway set default_caller_alias=gw_rec.caller_id where gateway_id=gw_rec.gateway_id;\n
-  end loop;\n
-end;\n
-' language plpgsql;
+create or replace function migrate_gcai() returns void as $$
+declare
+  gw_rec record;
+begin
+ for gw_rec in select * from gateway gw where gw.enable_caller_id=true
+  loop
+    update gateway set default_caller_alias=gw_rec.caller_id where gateway_id=gw_rec.gateway_id;
+  end loop;
+end;
+$$ language plpgsql;
 
 select migrate_gcai();
 
