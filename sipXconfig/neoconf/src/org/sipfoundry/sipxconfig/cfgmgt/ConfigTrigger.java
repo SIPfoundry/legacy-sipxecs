e9274c5fa6e987aac12e09408fbf42ee1a25e53b
Douglas Hubler <dhubler@ezuce.com>
2012-03-03 07:02:08 -0500

    send event to config providers when feature enabled

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/cfgmgt/ConfigTrigger.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/cfgmgt/ConfigTrigger.java
@@ -10,9 +10,14 @@ package org.sipfoundry.sipxconfig.cfgmgt;
 import java.util.Collection;
 
 import org.sipfoundry.sipxconfig.common.event.DaoEventListener;
+import org.sipfoundry.sipxconfig.commserver.Location;
 import org.sipfoundry.sipxconfig.feature.Feature;
+import org.sipfoundry.sipxconfig.feature.FeatureListener;
+import org.sipfoundry.sipxconfig.feature.FeatureManager;
+import org.sipfoundry.sipxconfig.feature.GlobalFeature;
+import org.sipfoundry.sipxconfig.feature.LocationFeature;
 
-public class ConfigTrigger implements DaoEventListener {
+public class ConfigTrigger implements DaoEventListener, FeatureListener {
     private ConfigManager m_configManager;
 
     @Override
@@ -40,5 +45,22 @@ public class ConfigTrigger implements DaoEventListener {
 
     public void setConfigManager(ConfigManager configManager) {
         m_configManager = configManager;
+    }
+
+    @Override
+    public void enableLocationFeature(FeatureManager manager, FeatureEvent event, LocationFeature feature,
+            Location location) {
+        if (event == FeatureEvent.POST_DISABLE || event == FeatureEvent.POST_ENABLE) {
+            // even though location is given, we configure everywhere because it's up to the
+            // ConfigProvider to decide
+            m_configManager.configureEverywhere(feature);
+        }
+    }
+
+    @Override
+    public void enableGlobalFeature(FeatureManager manager, FeatureEvent event, GlobalFeature feature) {
+        if (event == FeatureEvent.POST_DISABLE || event == FeatureEvent.POST_ENABLE) {
+            m_configManager.configureEverywhere(feature);
+        }
     }
 }
