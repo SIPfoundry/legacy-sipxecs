6f5c0dd95caf22cc290917952bc72277d477a595
laurentiu <laurentiu@ezuce.com>
2012-03-15 16:28:41 +0200

    update the copyright notice

--- sipXcommserverLib/include/sipXecsService/daemon.h
+++ sipXcommserverLib/include/sipXecsService/daemon.h
@@ -1,8 +1,18 @@
-// Copyright (C) 2011 eZuce Inc., certain elements licensed under a Contributor Agreement.
-// Contributors retain copyright to elements licensed under a Contributor Agreement.
-// Licensed to the User under the AGPL license.
-//
-//////////////////////////////////////////////////////////////////////////////
+/*
+ * Copyright (c) 2011 eZuce, Inc. All rights reserved.
+ * Contributed to SIPfoundry under a Contributor Agreement
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 #ifndef _SIPXSERVICE_DAEMON_H_
 #define _SIPXSERVICE_DAEMON_H_
 
