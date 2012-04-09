932142921e176c6fefe6733ae7fc66d7e4e1f4d1
Douglas Hubler <dhubler@ezuce.com>
2012-04-09 06:44:55 -0400

    apache setup fixes

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/apache/ApacheManagerImpl.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/apache/ApacheManagerImpl.java
@@ -12,8 +12,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
+import org.sipfoundry.sipxconfig.address.Address;
+import org.sipfoundry.sipxconfig.address.AddressManager;
+import org.sipfoundry.sipxconfig.address.AddressProvider;
+import org.sipfoundry.sipxconfig.address.AddressType;
 import org.sipfoundry.sipxconfig.cfgmgt.ConfigManager;
 import org.sipfoundry.sipxconfig.cfgmgt.ConfigProvider;
 import org.sipfoundry.sipxconfig.cfgmgt.ConfigRequest;
@@ -28,7 +33,7 @@ import org.sipfoundry.sipxconfig.setup.SetupListener;
 import org.sipfoundry.sipxconfig.setup.SetupManager;
 
 public class ApacheManagerImpl extends SipxHibernateDaoSupport<Object> implements ApacheManager, ConfigProvider,
-    SetupListener, FirewallProvider {
+    SetupListener, FirewallProvider, AddressProvider {
 
     @Override
     public void replicate(ConfigManager manager, ConfigRequest request) throws IOException {
@@ -46,7 +51,7 @@ public class ApacheManagerImpl extends SipxHibernateDaoSupport<Object> implement
 
     @Override
     public void setup(SetupManager manager) {
-        if (manager.isSetup(FEATURE.getId())) {
+        if (!manager.isSetup(FEATURE.getId())) {
             Location primary = manager.getConfigManager().getLocationManager().getPrimaryLocation();
             manager.getFeatureManager().enableLocationFeature(FEATURE, primary, true);
             manager.setSetup(FEATURE.getId());
@@ -60,5 +65,15 @@ public class ApacheManagerImpl extends SipxHibernateDaoSupport<Object> implement
     @Override
     public Collection<DefaultFirewallRule> getFirewallRules(FirewallManager manager) {
         return DefaultFirewallRule.rules(Arrays.asList(HTTP_ADDRESS, HTTPS_ADDRESS), FirewallRule.SystemId.PUBLIC);
+    }
+
+    @Override
+    public Collection<Address> getAvailableAddresses(AddressManager manager, AddressType type, Location requester) {
+        if (!type.equalsAnyOf(HTTP_ADDRESS, HTTPS_ADDRESS)) {
+            return null;
+        }
+
+        List<Location> locations = manager.getFeatureManager().getLocationsForEnabledFeature(FEATURE);
+        return Location.toAddresses(type, locations);
     }
 }
