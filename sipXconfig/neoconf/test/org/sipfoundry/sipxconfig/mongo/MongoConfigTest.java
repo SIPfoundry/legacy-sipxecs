e70a7301c10df5089952412ccb88caf07e019dde
Douglas Hubler <dhubler@ezuce.com>
2012-04-24 10:29:24 -0400

    mongo primary in server list twice

--- sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/mongo/MongoConfigTest.java
+++ sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/mongo/MongoConfigTest.java
@@ -16,9 +16,11 @@
  */
 package org.sipfoundry.sipxconfig.mongo;
 
+
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Before;
@@ -27,28 +29,27 @@ import org.sipfoundry.sipxconfig.commserver.Location;
 
 public class MongoConfigTest {
     private MongoConfig m_config;
-    private Location m_primary;
-    private List<Location> m_secondary;
+    private List<Location> m_single;
+    private List<Location> m_multi;
     
     @Before
     public void setUp() {
         m_config = new MongoConfig();
-        m_primary = new Location("primary");
-        m_primary.setAddress("1.1.1.0");
-        m_secondary = Arrays.asList(new Location("one"), new Location("two"));
-        m_secondary.get(0).setAddress("1.1.1.1");
-        m_secondary.get(1).setAddress("1.1.1.2");
+        m_multi = Arrays.asList(new Location("one"), new Location("two"));
+        m_multi.get(0).setAddress("1.1.1.1");
+        m_multi.get(1).setAddress("1.1.1.2");
+        m_single = Collections.singletonList(m_multi.get(0));
     }
     
     @Test
-    public void connString() {
-        assertEquals("sipxecs/primary:1", m_config.getConnectionString(m_primary, null, 1, false));
-        assertEquals("sipxecs/primary:1,one:1,two:1", m_config.getConnectionString(m_primary, m_secondary, 1, false));
+    public void getConnectionString() {
+        assertEquals("sipxecs/one:1", m_config.getConnectionString(m_single, 1));
+        assertEquals("sipxecs/one:1,two:1", m_config.getConnectionString(m_multi, 1));
     }
 
     @Test
-    public void connUrl() {
-        assertEquals("mongodb://primary:1/?slaveOk=true", m_config.getConnectionUrl(m_primary, null, 1, false));
-        assertEquals("mongodb://primary:1,one:1,two:1/?slaveOk=true", m_config.getConnectionUrl(m_primary, m_secondary, 1, false));
+    public void getConnectionUrl() {
+        assertEquals("mongodb://one:1/?slaveOk=true", m_config.getConnectionUrl(m_single, 1));
+        assertEquals("mongodb://one:1,two:1/?slaveOk=true", m_config.getConnectionUrl(m_multi, 1));
     }
 }
