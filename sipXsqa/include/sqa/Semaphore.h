203fa6d40eabfcb622ebe1d3c1aa8bbec9b95fc7
Joegen Baclor <jbaclor@ezuce.com>
2012-05-02 13:28:01 +0800

    Promoted the rest of the client classes from LGPL to AGPL.

--- sipXsqa/include/sqa/Semaphore.h
+++ sipXsqa/include/sqa/Semaphore.h
@@ -1,15 +1,15 @@
 /*
- * Copyright (c) 2011 eZuce, Inc. All rights reserved.
+ * Copyright (c) eZuce, Inc. All rights reserved.
  * Contributed to SIPfoundry under a Contributor Agreement
  *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
+ * This software is free software; you can redistribute it and/or modify it under
+ * the terms of the Affero General Public License (AGPL) as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your option)
  * any later version.
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT
+ * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
  */
 
--- sipXsqa/include/sqa/StateQueueDriverTest.h
+++ sipXsqa/include/sqa/StateQueueDriverTest.h
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) eZuce, Inc. All rights reserved.
+ * Contributed to SIPfoundry under a Contributor Agreement
+ *
+ * This software is free software; you can redistribute it and/or modify it under
+ * the terms of the Affero General Public License (AGPL) as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ */
+
 #ifndef STATEQUEUEDRIVERTEST_H
 #define	STATEQUEUEDRIVERTEST_H
 
@@ -7,20 +22,7 @@
 #include "StateQueueDialogDataClient.h"
 #include "StateQueueRegData.h"
 #include "sqaclient.h"
-/*
- * Copyright (c) 2011 eZuce, Inc. All rights reserved.
- * Contributed to SIPfoundry under a Contributor Agreement
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
+
 
 #include <sstream>
 #include <string>
