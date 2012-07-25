eaafb01aba3d9e9201d9e8272103f6b788f4d249
laurentiu <laurentiu@ezuce.com>
2012-07-25 12:38:59 +0300

    import of 4.4 back-up configuration to 4.6 doesn't work

--- sipXconfig/etc/sipxpbx/database/migrate_services.sql
+++ sipXconfig/etc/sipxpbx/database/migrate_services.sql
@@ -1,4 +1,3 @@
-
 -- hopefully you've already used these tables to convert the data first (example: meetme_location.sql)
 drop table location_specific_service;
 drop table sipx_service;
@@ -7,6 +6,7 @@ drop table location_bundle;
 drop sequence location_specific_service_seq;
 
 -- not bothering to migrate ATM, user can recreate. it's only enable/disable everyone setting
-delete from value_storage where value_storage_id in (select value_storage_id from general_phonebook_settings); 
+delete from setting_value where value_storage_id in (select value_storage_id from general_phonebook_settings);
+delete from value_storage where value_storage_id in (select value_storage_id from general_phonebook_settings);
 drop table general_phonebook_settings;
 drop sequence general_phonebook_settings_seq;
