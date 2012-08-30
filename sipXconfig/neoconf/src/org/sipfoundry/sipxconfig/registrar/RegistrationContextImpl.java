37498116cad2b50719ed1fa9df5262eb77533228
dizzy <george@ezuce.com>
2012-08-31 00:07:04 +0300

    Do not load all registrations in memory, just active one (further improvement would be to paginate / add option for searching registrations after extension) Load only registrations for the queried user )

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/registrar/RegistrationContextImpl.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/registrar/RegistrationContextImpl.java
@@ -10,70 +10,63 @@
 package org.sipfoundry.sipxconfig.registrar;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.sipfoundry.sipxconfig.commserver.imdb.RegistrationItem;
 import org.sipfoundry.sipxconfig.common.SipUri;
 import org.sipfoundry.sipxconfig.common.User;
+import org.sipfoundry.sipxconfig.commserver.imdb.RegistrationItem;
+import org.sipfoundry.sipxconfig.domain.DomainManager;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
+import com.mongodb.QueryBuilder;
 
 public class RegistrationContextImpl implements RegistrationContext {
     public static final Log LOG = LogFactory.getLog(RegistrationContextImpl.class);
     private static final String DB_COLLECTION_NAME = "registrar";
+    private static final String EXPIRED = "expired";
+    private static final String URI = "uri";
     private MongoTemplate m_nodedb;
+    private DomainManager m_domainManager;
 
     /**
      * @see org.sipfoundry.sipxconfig.registrar.RegistrationContext#getRegistrations()
      */
     public List<RegistrationItem> getRegistrations() {
-        try {
-            DB datasetDb = m_nodedb.getDb();
-            DBCollection registrarCollection = datasetDb.getCollection(DB_COLLECTION_NAME);
-            DBCursor cursor = registrarCollection.find();
-            List<RegistrationItem> items = new ArrayList<RegistrationItem>(cursor.size());
-            while (cursor.hasNext()) {
-                DBObject registration = cursor.next();
-                if ((Boolean) registration.get("expired")) {
-                    continue;
-                }
-                RegistrationItem item = new RegistrationItem();
-                item.setContact((String) registration.get("contact"));
-                item.setPrimary((String) registration.get("primary"));
-                item.setExpires((Integer) registration.get("expirationTime"));
-                item.setUri((String) registration.get("uri"));
-                item.setInstrument((String) registration.get("instrument"));
-                item.setRegCallId((String) registration.get("callId"));
-                items.add(item);
-            }
-            return items;
-        } catch (Exception e) {
-            // we are handling this separately - server returns FileNotFound even if everything is
-            // OK but we have no registrations present
-            LOG.warn("Cannot retrieve registrations.", e);
-            return Collections.emptyList();
-        }
+        DB datasetDb = m_nodedb.getDb();
+        DBCollection registrarCollection = datasetDb.getCollection(DB_COLLECTION_NAME);
+        DBCursor cursor = registrarCollection.find(QueryBuilder.start(EXPIRED).is(Boolean.FALSE).get());
+        return getItems(cursor);
     }
 
     public List<RegistrationItem> getRegistrationsByUser(User user) {
-        return getRegistrationsByUser(getRegistrations(), user);
+        DB datasetDb = m_nodedb.getDb();
+        DBCollection registrarCollection = datasetDb.getCollection(DB_COLLECTION_NAME);
+        DBCursor cursor = registrarCollection.find(QueryBuilder.start(URI)
+                .is(SipUri.format(user.getUserName(), m_domainManager.getDomainName(), false)).and(EXPIRED)
+                .is(Boolean.FALSE).get());
+        return getItems(cursor);
     }
 
-    List<RegistrationItem> getRegistrationsByUser(List<RegistrationItem> registrations, User user) {
-        List<RegistrationItem> result = new ArrayList<RegistrationItem>();
-        for (RegistrationItem registration : registrations) {
-            if (SipUri.extractUser(registration.getUri()).equals(user.getUserName())) {
-                result.add(registration);
-            }
+    private List<RegistrationItem> getItems(DBCursor cursor) {
+        List<RegistrationItem> items = new ArrayList<RegistrationItem>(cursor.size());
+        while (cursor.hasNext()) {
+            DBObject registration = cursor.next();
+            RegistrationItem item = new RegistrationItem();
+            item.setContact((String) registration.get("contact"));
+            item.setPrimary((String) registration.get("primary"));
+            item.setExpires((Integer) registration.get("expirationTime"));
+            item.setUri((String) registration.get(URI));
+            item.setInstrument((String) registration.get("instrument"));
+            item.setRegCallId((String) registration.get("callId"));
+            items.add(item);
         }
-        return result;
+        return items;
     }
 
     public MongoTemplate getNodedb() {
@@ -82,5 +75,9 @@ public class RegistrationContextImpl implements RegistrationContext {
 
     public void setNodedb(MongoTemplate nodedb) {
         m_nodedb = nodedb;
+    }
+
+    public void setDomainManager(DomainManager mgr) {
+        m_domainManager = mgr;
     }
 }
--- sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/registrar/RegistrationContextTestIntegration.java
+++ sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/registrar/RegistrationContextTestIntegration.java
@@ -14,6 +14,7 @@ import java.util.List;
 import org.sipfoundry.sipxconfig.common.User;
 import org.sipfoundry.sipxconfig.commserver.imdb.ImdbTestCase;
 import org.sipfoundry.sipxconfig.commserver.imdb.RegistrationItem;
+import org.sipfoundry.sipxconfig.domain.DomainManager;
 import org.sipfoundry.sipxconfig.registrar.RegistrationContextImpl;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
@@ -24,15 +25,16 @@ import com.mongodb.DBObject;
 public class RegistrationContextTestIntegration extends ImdbTestCase {
     private RegistrationContextImpl m_builder;
     private MongoTemplate m_nodeDb;
+    private DomainManager m_mgr;
 
     private Object[][] DATA = {
         {
-            "063b4c2f5e11bf66a232762a7cf9e73a", "2395", "3000@example.org",
+            "063b4c2f5e11bf66a232762a7cf9e73a", "2395", "sip:3000@example.org",
             "\"John Doe\"<sip:john.doe@example.org;LINEID=f57f2117d5997f8d03d8395732f463f3>", true,
             "3000@example.org", 1299762967, 1299762667, ""
         },
         {
-            "063b4c2f5e11bf66a232762a7cf9e73b", "2399", "3001@example.org",
+            "063b4c2f5e11bf66a232762a7cf9e73b", "2399", "sip:3001@example.org",
             "\"John Doe\"<sip:jane.doe@example.org>", false, "3001@example.org", 1299762968, 1299762668, ""
         }
     };
@@ -62,6 +64,7 @@ public class RegistrationContextTestIntegration extends ImdbTestCase {
 
         m_builder = new RegistrationContextImpl();
         m_builder.setNodedb(m_nodeDb);
+        m_builder.setDomainManager(m_mgr);
     }
 
     public void testGetRegistrations() throws Exception {
@@ -69,7 +72,7 @@ public class RegistrationContextTestIntegration extends ImdbTestCase {
         assertEquals(1, registrations.size());
         RegistrationItem ri = (RegistrationItem) registrations.get(0);
         assertEquals(1299762968, ri.getExpires());
-        assertEquals("3001@example.org", ri.getUri());
+        assertEquals("sip:3001@example.org", ri.getUri());
         assertTrue(ri.getContact().indexOf("Doe") > 0);
     }
 
@@ -77,16 +80,20 @@ public class RegistrationContextTestIntegration extends ImdbTestCase {
         List<RegistrationItem> registrations = m_builder.getRegistrations();
         User user = new User();
         user.setUserName("3001");
-        registrations = m_builder.getRegistrationsByUser(registrations, user);
+        registrations = m_builder.getRegistrationsByUser(user);
         assertEquals(1, registrations.size());
         RegistrationItem ri = registrations.get(0);
         assertEquals(1299762968, ri.getExpires());
-        assertEquals("3001@example.org", ri.getUri());
+        assertEquals("sip:3001@example.org", ri.getUri());
         assertTrue(ri.getContact().indexOf("Doe") > 0);
     }
 
     public void setNodeDb(MongoTemplate nodeDb) {
         m_nodeDb = nodeDb;
+    }
+
+    public void setDomainManager(DomainManager mgr) {
+        m_mgr = mgr;
     }
 
 }
