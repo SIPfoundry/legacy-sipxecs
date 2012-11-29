0332a262fa9a2092860091f37a6bbb0c9768fea1
dizzy <george@ezuce.com>
2012-11-29 19:40:59 +0200

    PT40374529: Do not append sipxecs domain on external MWIs that are already sip

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/commserver/imdb/UserStaticMapping.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/commserver/imdb/UserStaticMapping.java
@@ -23,15 +23,22 @@ import static org.sipfoundry.commons.mongo.MongoConstants.FROM_URI;
 import static org.sipfoundry.commons.mongo.MongoConstants.IDENTITY;
 import static org.sipfoundry.commons.mongo.MongoConstants.TO_URI;
 
+import org.apache.commons.lang.StringUtils;
 import org.sipfoundry.sipxconfig.common.SipUri;
 
 public class UserStaticMapping extends DataSetRecord {
+    private static final String AT_SIGN = "@";
 
     public UserStaticMapping(String domain, String username, String mwi) {
-        String identity = username + "@" + domain;
+        String identity = username + AT_SIGN + domain;
         put(IDENTITY, identity);
         put(EVENT, "message-summary");
-        put(CONTACT, SipUri.format(mwi, domain, false));
+        if (StringUtils.contains(mwi, AT_SIGN)) {
+            String[] mwiParts = StringUtils.split(mwi, AT_SIGN);
+            put(CONTACT, SipUri.format(mwiParts[0], mwiParts[1], false));
+        } else {
+            put(CONTACT, SipUri.format(mwi, domain, false));
+        }
         put(FROM_URI, SipUri.format("IVR", domain, false));
         put(TO_URI, SipUri.format(username, domain, false));
         put(CALLID, "static-mwi-" + identity);
