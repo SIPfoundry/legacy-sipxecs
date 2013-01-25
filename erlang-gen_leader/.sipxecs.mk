7c759150e18e7411913b01ed55022e8b233b87de
Douglas Hubler <dhubler@ezuce.com>
2013-01-25 13:17:42 -0500

    add more deps for openacd

---
+++ erlang-gen_leader/.sipxecs.mk
@@ -0,0 +1,6 @@
+lib_common += erlang-gen_leader
+
+erlang-gen_leader_SRPM=erlang-gen_leader-1.0-3.fc18.src.rpm
+
+# targets not defined, nothing to do
+erlang-gen_leader.autoreconf erlang-gen_leader.configure erlang-gen_leader.dist:;
---
+++ erlang-lager/.sipxecs.mk
@@ -0,0 +1,6 @@
+lib_common += erlang-lager
+
+erlang-lager_SRPM=erlang-lager-1.2.1-1.el6.src.rpm
+
+# targets not defined, nothing to do
+erlang-lager.autoreconf erlang-lager.configure erlang-lager.dist:;
