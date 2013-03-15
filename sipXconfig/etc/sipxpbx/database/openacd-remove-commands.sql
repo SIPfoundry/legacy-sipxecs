9cf69218d84eb93de34b0ab2737a5896111c1c3d
Douglas Hubler <dhubler@ezuce.com>
2013-03-15 10:33:01 -0400

    PT-46270019 - Fix migration issue of openacd freeswitch commands from 4.6 update #0

--- sipXconfig/etc/sipxpbx/database/openacd-remove-commands.sql
+++ sipXconfig/etc/sipxpbx/database/openacd-remove-commands.sql
@@ -1,27 +1,27 @@
-delete from freeswitch_action where freeswitch_condition_id = 
-	(select freeswitch_condition_id from freeswitch_condition where freeswitch_ext_id = 
+delete from freeswitch_action where freeswitch_condition_id in
+	(select freeswitch_condition_id from freeswitch_condition where freeswitch_ext_id in 
 		(select freeswitch_ext_id from freeswitch_extension where name = 'login')
 	);
-delete from freeswitch_condition where freeswitch_ext_id = (select freeswitch_ext_id from freeswitch_extension where name = 'login');
+delete from freeswitch_condition where freeswitch_ext_id in (select freeswitch_ext_id from freeswitch_extension where name = 'login');
 delete from freeswitch_extension where name = 'login';
 
-delete from freeswitch_action where freeswitch_condition_id = 
-	(select freeswitch_condition_id from freeswitch_condition where freeswitch_ext_id = 
+delete from freeswitch_action where freeswitch_condition_id in 
+	(select freeswitch_condition_id from freeswitch_condition where freeswitch_ext_id in
 		(select freeswitch_ext_id from freeswitch_extension where name = 'available')
 	);
-delete from freeswitch_condition where freeswitch_ext_id = (select freeswitch_ext_id from freeswitch_extension where name = 'available');
+delete from freeswitch_condition where freeswitch_ext_id in (select freeswitch_ext_id from freeswitch_extension where name = 'available');
 delete from freeswitch_extension where name = 'available';
 
-delete from freeswitch_action where freeswitch_condition_id = 
-	(select freeswitch_condition_id from freeswitch_condition where freeswitch_ext_id = 
+delete from freeswitch_action where freeswitch_condition_id in 
+	(select freeswitch_condition_id from freeswitch_condition where freeswitch_ext_id in 
 		(select freeswitch_ext_id from freeswitch_extension where name = 'logoff')
 	);
-delete from freeswitch_condition where freeswitch_ext_id = (select freeswitch_ext_id from freeswitch_extension where name = 'logoff');
+delete from freeswitch_condition where freeswitch_ext_id in (select freeswitch_ext_id from freeswitch_extension where name = 'logoff');
 delete from freeswitch_extension where name = 'logoff';
 
-delete from freeswitch_action where freeswitch_condition_id = 
-	(select freeswitch_condition_id from freeswitch_condition where freeswitch_ext_id = 
+delete from freeswitch_action where freeswitch_condition_id in 
+	(select freeswitch_condition_id from freeswitch_condition where freeswitch_ext_id in 
 		(select freeswitch_ext_id from freeswitch_extension where name = 'release')
 	);
-delete from freeswitch_condition where freeswitch_ext_id = (select freeswitch_ext_id from freeswitch_extension where name = 'release');
+delete from freeswitch_condition where freeswitch_ext_id in (select freeswitch_ext_id from freeswitch_extension where name = 'release');
 delete from freeswitch_extension where name = 'release';
