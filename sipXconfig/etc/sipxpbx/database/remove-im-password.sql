1250a10858ef6e7410a6f9529cabf67e0f2ffb3a
Douglas Hubler <dhubler@ezuce.com>
2013-03-27 06:49:34 -0400

    PT-46976001 Migration for 4.4 and attempting to restore pins, if any users also an im password, all pins got reset to that one password.

--- sipXconfig/etc/sipxpbx/database/remove-im-password.sql
+++ sipXconfig/etc/sipxpbx/database/remove-im-password.sql
@@ -1,6 +1,4 @@
-update users set pintoken = a.im_password from users u
-  inner join address_book_entry a 
-  on u.address_book_entry_id = a.address_book_entry_id
-  where a.im_password is not NULL;
+-- careful - updates with joins are silently misleading in postgres. google it
+update users u set pintoken = x.im_password from (select a.im_password, a.address_book_entry_id from address_book_entry a join users u2 on  u2.address_book_entry_id = a.address_book_entry_id and a.im_password is not NULL) x where u.address_book_entry_id = x.address_book_entry_id;
 
 alter table address_book_entry drop column im_password;
--- sipXconfig/etc/sipxpbx/database/voicemail-pin.sql
+++ sipXconfig/etc/sipxpbx/database/voicemail-pin.sql
@@ -1,11 +1,2 @@
-update users set pintoken = a.im_password 
-from users u
-inner join 
- address_book_entry a
-on
- u.address_book_entry_id = a.address_book_entry_id
-where
- a.im_password is not NULL;
-
 alter table users
   add column voicemail_pintoken varchar(255);
