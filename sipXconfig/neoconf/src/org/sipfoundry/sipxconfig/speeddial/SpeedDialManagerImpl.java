ed11e94c459b2bdae0e82f1d5d7e40bebf581fce
dizzy <george@ezuce.com>
2013-04-01 20:02:07 +0300

    PT42411863: deleting and re-adding same user problem with IM ID
    - do not publicate save user event when user with speed dials deleted. It's not needed and  will trigger save in user profile db

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/speeddial/SpeedDialManagerImpl.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/speeddial/SpeedDialManagerImpl.java
@@ -156,7 +156,6 @@ public class SpeedDialManagerImpl extends SipxHibernateDaoSupport implements Spe
         if (!speedDials.isEmpty()) {
             getDaoEventPublisher().publishDeleteCollection(speedDials);
             getHibernateTemplate().deleteAll(speedDials);
-            getDaoEventPublisher().publishSave(m_coreContext.getUser(userId));
         }
     }
 
