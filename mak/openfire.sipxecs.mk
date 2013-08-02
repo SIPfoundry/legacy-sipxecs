3db1814399a7091350927d81606c30d1abc2b089
Alex Mateescu <amateescu@ezuce.com>
2013-08-02 12:44:20 +0300

    build :  need to add back plugin sources - we still build the search plugin

--- mak/openfire.sipxecs.mk
+++ mak/openfire.sipxecs.mk
@@ -11,4 +11,4 @@ openfire_RPM_DEFS = $(openfire_DEFS)
 openfire.dist : $(openfire_GIT_SUBMODULE)
 	test -d $(dir $(openfire_TARBALL)) || mkdir -p $(dir $(openfire_TARBALL))
 	cd $(SRC)/$(PROJ); \
-	  tar -caf $(openfire_TARBALL) --transform="s#^#openfire-$(openfire_VER)/#" --exclude=src/plugins/* `ls | egrep -v "bin|target|work"`
+	  tar -caf $(openfire_TARBALL) --transform="s#^#openfire-$(openfire_VER)/#" `ls | egrep -v "bin|target|work"`
