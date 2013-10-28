1d5cfce644afe45961a911d2cca5996162bfad71
Douglas Hubler <douglas@hubler.us>
2013-10-28 09:43:49 -0400

    build : oss_core websockets submod was out of date, refresh before every build now

--- mak/oss_core.sipxecs.mk
+++ mak/oss_core.sipxecs.mk
@@ -4,4 +4,7 @@ oss_core_TAR = $(PROJ)/oss_core-$(oss_core_VER).tar.gz
 oss_core_SRPM = oss_core-$(oss_core_VER)-$(oss_core_REV).src.rpm
 
 oss_core.dist : oss_core.autoreconf oss_core.configure
+	cd $(SRC)/$(PROJ); \
+	  git submodule init; \
+	  git submodule update
 	$(MAKE) -C $(PROJ) dist
