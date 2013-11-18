1b294c616fd1bd7ba22fe9aad642ef646b73d1f3
avilcu <avilcu@ezuce.com>
2013-11-18 12:42:13 +0200

    XX-10806 Removed Resource Limits from Admin Settings

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/EditAdmin.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/EditAdmin.java
@@ -14,10 +14,6 @@
  */
 package org.sipfoundry.sipxconfig.site.admin;
 
-import java.io.IOException;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.tapestry.annotations.Bean;
 import org.apache.tapestry.annotations.InjectObject;
 import org.apache.tapestry.annotations.Message;
@@ -25,17 +21,12 @@ import org.apache.tapestry.event.PageBeginRenderListener;
 import org.apache.tapestry.event.PageEvent;
 import org.sipfoundry.sipxconfig.admin.AdminContext;
 import org.sipfoundry.sipxconfig.admin.AdminSettings;
-import org.sipfoundry.sipxconfig.admin.ResLimitsConfiguration;
 import org.sipfoundry.sipxconfig.cfgmgt.ConfigManager;
-import org.sipfoundry.sipxconfig.common.UserException;
 import org.sipfoundry.sipxconfig.components.PageWithCallback;
 import org.sipfoundry.sipxconfig.components.SipxValidationDelegate;
-import org.sipfoundry.sipxconfig.components.TapestryUtils;
 
 public abstract class EditAdmin extends PageWithCallback implements PageBeginRenderListener {
     public static final String PAGE = "admin/EditAdmin";
-
-    private static final Log LOG = LogFactory.getLog(EditAdmin.class);
 
     @Bean
     public abstract SipxValidationDelegate getValidator();
@@ -45,9 +36,6 @@ public abstract class EditAdmin extends PageWithCallback implements PageBeginRen
 
     @InjectObject("spring:configManager")
     public abstract ConfigManager getConfigManager();
-
-    @InjectObject("spring:resLimitsConfiguration")
-    public abstract ResLimitsConfiguration getResLimitsConfiguration();
 
     public abstract AdminSettings getSettings();
 
@@ -64,18 +52,6 @@ public abstract class EditAdmin extends PageWithCallback implements PageBeginRen
     }
 
     public void apply() {
-
-        if (!TapestryUtils.validateFDSoftAndHardLimits(this, getSettings(), "configserver-config")) {
-            return;
-        }
-
         getAdminContext().saveSettings(getSettings());
-        //Reset resource limits to default for all processes and restart affected ones
-        try {
-            getResLimitsConfiguration().writeDefaultsResourceLimits(getConfigManager());
-        } catch (IOException e) {
-            LOG.error("Cannot reset resource limits to defaults", e);
-            throw new UserException(getResourceLimitsError(), e.getLocalizedMessage());
-        }
     }
 }
