5a5f7178cbc843316358b8b63d576ccadbc94484
ionut <ionut@islaveanu-10.dev-ezuce.ro>
2013-11-28 15:55:54 +0200

    - changed sipxunit code to also save the results in an xml file format

--- sipXportLib/src/test/sipxunit/TestRunner.cpp
+++ sipXportLib/src/test/sipxunit/TestRunner.cpp
@@ -19,6 +19,8 @@
 
 #include <sipxunit/TestOsSysLogListener.h>
 
+#include <iostream>
+
 TestRunner::TestRunner()
     : CppUnit::TestRunner()
 {
@@ -26,21 +28,31 @@ TestRunner::TestRunner()
     m_outputter = new TestOutputter(m_monitor);
     m_result = new CppUnit::TestResult();
 
+    m_resultCollector = new CppUnit::TestResultCollector();
+
     m_logger = new TestOsSysLogListener();
 
     m_result->addListener(m_monitor);
 
     // To disable the use of OsSysLog during unit tests, comment out the following line
     m_result->addListener(m_logger);
-}
 
+    m_result->addListener(m_resultCollector);
+
+    m_xmlout.open("sipxunit-test-result.xml");
+    m_xmlOutputter = new CppUnit::XmlOutputter(m_resultCollector, m_xmlout);
+}
 
 TestRunner::~TestRunner()
 {
+    delete m_xmlOutputter;
+    delete m_logger;
+    delete m_resultCollector;
     delete m_result;
     delete m_outputter;
     delete m_monitor;
-    delete m_logger;
+
+    m_xmlout.close();
 }
 
 bool TestRunner::run()
@@ -48,6 +60,7 @@ bool TestRunner::run()
     CppUnit::TestRunner *pthis = this;
     pthis->run(*m_result);
     m_outputter->write();
+    m_xmlOutputter->write();
 
     return m_monitor->wasSuccessful();
 }
