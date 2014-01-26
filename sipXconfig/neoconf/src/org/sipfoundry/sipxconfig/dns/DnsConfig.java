902e91e46c145194720a93c6544aad9a8c8ea763
dizzy <george@ezuce.com>
2014-01-27 01:31:09 +0200

    XX-10915: Bind won't start if sip domain != network domain
    - generate FQDNs for dns server host names instead short names when sip domain != network domain

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/dns/DnsConfig.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/dns/DnsConfig.java
@@ -59,6 +59,7 @@ public class DnsConfig implements ConfigProvider {
     private static final String YML_DOMAIN = "domain";
     private static final String YML_TARGET = ":target";
     private static final String VIEW_NAME = "%s.view";
+    private static final String QUALIFIED = "%s.";
     private static final Log LOG = LogFactory.getLog(DnsConfig.class);
     private DnsManager m_dnsManager;
     private RegionManager m_regionManager;
@@ -219,7 +220,7 @@ public class DnsConfig implements ConfigProvider {
         String qualifiedTarget = null;
         boolean domainIsFqdn = domain.equals(all.get(0).getFqdn());
         if (domainIsFqdn) {
-            qualifiedTarget = domain + ".";
+            qualifiedTarget = String.format(QUALIFIED, domain);
         }
         if (rrs != null) {
             for (DnsSrvRecord rr : rrs) {
@@ -228,33 +229,33 @@ public class DnsConfig implements ConfigProvider {
             }
         }
         c.endArray();
-        writeServerYaml(c, all, "dns_servers", dns, qualifiedTarget);
+        writeServerYaml(c, all, "dns_servers", dns, qualifiedTarget, generateARecords);
         List<Address> dnsAddresses = new ArrayList<Address>();
         if (generateARecords) {
             dnsAddresses = Location.toAddresses(DnsManager.DNS_ADDRESS, all);
         }
-        writeServerYaml(c, all, "all_servers", dnsAddresses, qualifiedTarget);
+        writeServerYaml(c, all, "all_servers", dnsAddresses, qualifiedTarget, generateARecords);
     }
 
     /**
      * my-id : [ { :name: my-fqdn, :ipv4: 1.1.1.1 }, ... ]
      */
     void writeServerYaml(YamlConfiguration c, List<Location> all, String id, List<Address> addresses,
-            String qualifiedTarget)
+            String qualifiedTarget, boolean isSameSipNetworkDomain)
         throws IOException {
         c.startArray(id);
         if (addresses != null) {
             for (Address a : addresses) {
                 c.nextElement();
-                writeAddress(c, all, a.getAddress(), a.getPort(), qualifiedTarget);
+                writeAddress(c, all, a.getAddress(), a.getPort(), qualifiedTarget, isSameSipNetworkDomain);
             }
         }
         c.endArray();
     }
 
     void writeAddress(YamlConfiguration c, List<Location> all, String address, int port,
-            String qualifiedTarget) throws IOException {
-        String host = getHostname(all, address);
+            String qualifiedTarget, boolean isSameSipNetworkDomain) throws IOException {
+        String host = getHostname(all, address, isSameSipNetworkDomain);
         if (host != null) {
             if (qualifiedTarget != null) {
                 c.write(YML_NAME, qualifiedTarget);
@@ -281,10 +282,15 @@ public class DnsConfig implements ConfigProvider {
         c.write(":host", srv.getHost());
     }
 
-    String getHostname(List<Location> locations, String ip) {
+    String getHostname(List<Location> locations, String ip, boolean isSameSipNetworkDomain) {
         for (Location l : locations) {
             if (ip.equals(l.getAddress())) {
-                return l.getHostname();
+                if (isSameSipNetworkDomain) {
+                    return l.getHostname();
+                }
+                // cannot use short names if sip domain different than network domain
+                // as DNS will use short name + sip domain as host. Return FQDN qualified for DNS
+                return String.format(QUALIFIED, l.getFqdn());
             }
         }
         LOG.warn("No hostname found for " + ip + ", could be unmanged service");
