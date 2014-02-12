2f781ad7a692d08879e5cc9f6d584a3a229e104e
Cristi Starasciuc <cristi@ezuce.com>
2014-02-12 10:41:51 +0200

    UC-2259 - added order by to sql statement that retrieves user group members by page. Also changed to jdbc template from hibernate template.
    Conflicts:
    	sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/common/DaoUtils.java

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/common/DaoUtils.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/common/DaoUtils.java
@@ -19,6 +19,8 @@ import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Transformer;
 import org.apache.commons.collections.functors.ChainedTransformer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Criterion;
@@ -37,6 +39,7 @@ import org.springframework.orm.hibernate3.HibernateTemplate;
  */
 public final class DaoUtils {
     public static final int PAGE_SIZE = 1000;
+    private static final Log LOG = LogFactory.getLog(DaoUtils.class);
     private static final String ID_PROPERTY_NAME = "id";
 
     private DaoUtils() {
@@ -65,6 +68,7 @@ public final class DaoUtils {
         final Criterion expression = Restrictions.eq(propName, propValue);
         final Class klass = beanClass;
         HibernateCallback callback = new HibernateCallback() {
+            @Override
             public Object doInHibernate(Session session) {
                 Criteria criteria = session.createCriteria(klass).add(expression).setProjection(
                         Projections.property(ID_PROPERTY_NAME));
@@ -211,14 +215,14 @@ public final class DaoUtils {
 
     public static void addToGroup(HibernateTemplate hibernate, DaoEventPublisher eventPublisher, Integer groupId,
             Class klass, Collection ids) {
-        Group group = (Group) hibernate.load(Group.class, groupId);
+        Group group = hibernate.load(Group.class, groupId);
         Transformer addTag = new BeanWithGroups.AddTag(group);
         doForAllBeanIds(hibernate, eventPublisher, addTag, klass, ids);
     }
 
     public static void removeFromGroup(HibernateTemplate hibernate, DaoEventPublisher eventPublisher,
             Integer groupId, Class klass, Collection ids) {
-        Group group = (Group) hibernate.load(Group.class, groupId);
+        Group group = hibernate.load(Group.class, groupId);
         Transformer addTag = new BeanWithGroups.RemoveTag(group);
         doForAllBeanIds(hibernate, eventPublisher, addTag, klass, ids);
     }
