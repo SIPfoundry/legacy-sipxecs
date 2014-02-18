dba32bb4f545cb38550def27dcf26529f946eb63
Alex Mateescu <amateescu@ezuce.com>
2014-02-18 13:50:53 +0200

    XX-10938 Added CSV and VCard format to phone book GET, for export purposes

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/rest/UserPhonebookResource.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/rest/UserPhonebookResource.java
@@ -9,25 +9,54 @@
  */
 package org.sipfoundry.sipxconfig.rest;
 
+import static org.restlet.data.MediaType.TEXT_PLAIN;
+import static org.restlet.data.MediaType.TEXT_VCARD;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.restlet.Context;
 import org.restlet.data.MediaType;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
 import org.restlet.resource.Representation;
 import org.restlet.resource.ResourceException;
+import org.restlet.resource.StringRepresentation;
 import org.restlet.resource.Variant;
 import org.sipfoundry.sipxconfig.common.User;
 import org.sipfoundry.sipxconfig.phonebook.Phonebook;
 import org.sipfoundry.sipxconfig.phonebook.PhonebookEntry;
+import org.sipfoundry.sipxconfig.phonebook.PhonebookManager.PhonebookFormat;
 
 import com.thoughtworks.xstream.XStream;
 
 public class UserPhonebookResource extends UserPhonebookSearchResource {
+    private static final Logger LOG = Logger.getLogger(UserPhonebookResource.class);
+
+    @Override
+    public void init(Context context, Request request, Response response) {
+        super.init(context, request, response);
+        getVariants().add(new Variant(TEXT_PLAIN));
+        getVariants().add(new Variant(TEXT_VCARD));
+    }
+
     @Override
     public Representation represent(Variant variant) throws ResourceException {
         Collection<Phonebook> phonebooks = getPhonebookManager().getAllPhonebooksByUser(getUser());
         Collection<PhonebookEntry> entries = getPhonebookManager().getEntries(phonebooks, getUser());
-        return new PhonebookEntryRepresentation(variant.getMediaType(), convertPhonebookEntries(entries));
+        Representation reprObj;
+        if (TEXT_PLAIN.equals(variant.getMediaType())) {
+            reprObj = new StringRepresentation(convert(entries, PhonebookFormat.CSV));
+        } else if (TEXT_VCARD.equals(variant.getMediaType())) {
+            reprObj = new StringRepresentation(convert(entries, PhonebookFormat.VCARD));
+        } else {
+            reprObj = new PhonebookEntryRepresentation(variant.getMediaType(), convertPhonebookEntries(entries));
+        }
+
+        return reprObj;
     }
 
     @Override
@@ -54,7 +83,20 @@ public class UserPhonebookResource extends UserPhonebookSearchResource {
         getPhonebookManager().removePrivatePhonebook(user);
     }
 
-    static class PrivatePhonebookRepresentation extends XStreamRepresentation<Collection<PhonebookEntry>> {
+    private String convert(Collection<PhonebookEntry> entries, PhonebookFormat fmt) {
+        String converted = "";
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        try {
+            m_phonebookManager.exportPhonebook(entries, stream, fmt);
+            converted = stream.toString("UTF-8");
+        } catch (IOException e) {
+            LOG.error(String.format("Failed to convert phonebook to %s format: %s", fmt, e.getMessage()));
+        }
+
+        return converted;
+    }
+
+    private static class PrivatePhonebookRepresentation extends XStreamRepresentation<Collection<PhonebookEntry>> {
         public PrivatePhonebookRepresentation(MediaType mediaType, Collection<PhonebookEntry> object) {
             super(mediaType, object);
         }
--- sipXconfig/web/test/org/sipfoundry/sipxconfig/rest/UserPhonebookResourceTest.java
+++ sipXconfig/web/test/org/sipfoundry/sipxconfig/rest/UserPhonebookResourceTest.java
@@ -12,11 +12,21 @@ package org.sipfoundry.sipxconfig.rest;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expectLastCall;
 import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.restlet.data.MediaType.TEXT_PLAIN;
+import static org.restlet.data.MediaType.TEXT_VCARD;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Collection;
 
+import org.easymock.EasyMock;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.Variant;
 import org.sipfoundry.sipxconfig.phonebook.Phonebook;
+import org.sipfoundry.sipxconfig.phonebook.PhonebookEntry;
 import org.sipfoundry.sipxconfig.phonebook.PhonebookManager;
+import org.sipfoundry.sipxconfig.phonebook.PhonebookManager.PhonebookFormat;
 import org.sipfoundry.sipxconfig.test.TestHelper;
 
 public class UserPhonebookResourceTest extends UserPhonebookSearchResourceTest {
@@ -30,12 +40,54 @@ public class UserPhonebookResourceTest extends UserPhonebookSearchResourceTest {
         m_phonebookManager.getAllPhonebooksByUser(null);
         expectLastCall().andReturn(phonebooks);
         m_phonebookManager.getEntries(phonebooks, m_user);
-        expectLastCall().andReturn(getMockPhonebookEntries());
+        Collection<PhonebookEntry> entries = getMockPhonebookEntries();
+        expectLastCall().andReturn(entries);
 
         replay(m_phonebookManager);
 
         m_resource = new UserPhonebookResource();
-        UserPhonebookResource resource = (UserPhonebookResource)m_resource;
+        UserPhonebookResource resource = (UserPhonebookResource) m_resource;
         resource.setPhonebookManager(m_phonebookManager);
+    }
+
+    public void testGetCsv() throws ResourceException, IOException {
+        Collection<Phonebook> phonebooks = TestHelper.getMockAllPhonebooks();
+
+        PhonebookManager mgr = exportSetup(phonebooks, PhonebookFormat.CSV);
+
+        m_resource = new UserPhonebookResource();
+        UserPhonebookResource resource = (UserPhonebookResource) m_resource;
+        resource.setPhonebookManager(mgr);
+
+        m_resource.represent(new Variant(TEXT_PLAIN));
+        verify(mgr);
+    }
+
+    public void testGetVcard() throws ResourceException, IOException {
+        Collection<Phonebook> phonebooks = TestHelper.getMockAllPhonebooks();
+
+        PhonebookManager mgr = exportSetup(phonebooks, PhonebookFormat.VCARD);
+
+        m_resource = new UserPhonebookResource();
+        UserPhonebookResource resource = (UserPhonebookResource) m_resource;
+        resource.setPhonebookManager(mgr);
+
+        m_resource.represent(new Variant(TEXT_VCARD));
+        verify(mgr);
+    }
+
+    private PhonebookManager exportSetup(Collection<Phonebook> phonebooks, PhonebookFormat fmt) throws IOException {
+        PhonebookManager mgr = createMock(PhonebookManager.class);
+
+        mgr.getAllPhonebooksByUser(null);
+        expectLastCall().andReturn(phonebooks);
+        mgr.getEntries(phonebooks, m_user);
+        Collection<PhonebookEntry> entries = getMockPhonebookEntries();
+        expectLastCall().andReturn(entries);
+        mgr.exportPhonebook(EasyMock.eq(entries), (OutputStream) EasyMock.anyObject(), EasyMock.eq(fmt));
+        expectLastCall();
+
+        replay(mgr);
+        return mgr;
     }
 }
