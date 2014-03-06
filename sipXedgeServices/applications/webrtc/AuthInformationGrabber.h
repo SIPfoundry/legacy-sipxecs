9d36261a5bb8c4c406613d693a1a83d7bb875523
joegen <joegen@ossapp.com>
2014-03-06 12:17:40 +0800

    UC-2195:  Added copyright headers

--- sipXedgeServices/applications/webrtc/AuthInformationGrabber.h
+++ sipXedgeServices/applications/webrtc/AuthInformationGrabber.h
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) eZuce, Inc. All rights reserved.
+ * Contributed to SIPfoundry under a Contributor Agreement
+ *
+ * This software is free software; you can redistribute it and/or modify it under
+ * the terms of the Affero General Public License (AGPL) as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ */
+
 #ifndef AUTHINFORMATIONGRABBER_H_INCLUDED
 #define	AUTHINFORMATIONGRABBER_H_INCLUDED
 
