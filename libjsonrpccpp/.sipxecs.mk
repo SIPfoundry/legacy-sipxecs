7af42609db2d945a0c1129cc8abdb35f40bbfdf0
joegen <joegen@ossapp.com>
2014-03-06 10:54:49 +0800

    XX-10962: Compile Json RPC library from tarball

---
+++ libjsonrpccpp/.sipxecs.mk
@@ -0,0 +1,7 @@
+libjsonrpccpp_VER = 0.2.1
+libjsonrpccpp_REV = 1
+libjsonrpccpp_SPEC = $(SRC)/libjsonrpccpp/libjsonrpccpp.spec
+libjsonrpccpp_SRPM = libjsonrpccpp-$(libjsonrpccpp_VER)-$(libjsonrpccpp_REV).src.rpm
+libjsonrpccpp_SOURCES = libjsonrpccpp-$(libjsonrpccpp_VER).tar.gz
+
+libjsonrpccpp.dist :;
