e5f477a53ac9668b375dc893ea9b404dc13e80d9
Mircea Carasel <mirceac@ezuce.com>
2014-03-23 10:30:22 +0200

    UC-2523: After updating ,web interface wont start until service sipxconfig restart
    -change sipxconfig.sec.erb to dinamically handle alarms
    for PHONE, BACKUP, LDAP alarms

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/bulk/ldap/LdapImportManagerImpl.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/bulk/ldap/LdapImportManagerImpl.java
@@ -65,7 +65,7 @@ public class LdapImportManagerImpl extends HibernateDaoSupport implements LdapIm
             int notImportedSize = m_rowInserter.getNotImportedUserNames().size();
             int importedSize = m_rowInserter.getImportedUserNames().size();
             if (notImportedSize > 0) {
-                LOG.info("LDAP users imported: " + importedSize + " rejected: " + notImportedSize);
+                LOG.info("ALARM_LDAP_IMPORT_FAILED Users imported: " + importedSize + " rejected: " + notImportedSize);
             } else {
                 LOG.info("All LDAP users are successfully imported... " + importedSize);
             }
