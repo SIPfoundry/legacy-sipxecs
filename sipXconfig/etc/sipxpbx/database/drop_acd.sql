12f7ec17e4d8eecf0bfd78ec2e7e7169b3a88a35
laurentiu <laurentiu@ezuce.com>
2014-03-24 15:55:09 +0200

    UC-1806 - Legacy ACD Packages should be removed - delete FKs from acd_* tables to value_storage, users and location tables

--- sipXconfig/etc/sipxpbx/database/drop_acd.sql
+++ sipXconfig/etc/sipxpbx/database/drop_acd.sql
@@ -1,4 +1,13 @@
--- delete first works because no FKs from acd_* tables to value_storage
+-- delete FKs from acd_* tables to value_storage
+alter table acd_agent drop constraint "fkab9535086c899bce";
+alter table acd_line drop constraint "fk816cf1916c899bce";
+alter table acd_queue drop constraint "fkac7d0b146c899bce";
+alter table acd_server drop constraint "fke5b27da06c899bce";
+
+-- delete FKs from acd_* tables to users and location
+alter table acd_agent drop constraint "fkab953508a53eb3e4";
+alter table acd_server drop constraint "fk_location_id";
+
 delete from setting_value where value_storage_id in (select value_storage_id from acd_agent);
 delete from value_storage where value_storage_id in (select value_storage_id from acd_agent);
 delete from setting_value where value_storage_id in (select value_storage_id from acd_line);
@@ -7,11 +16,13 @@ delete from setting_value where value_storage_id in (select value_storage_id fro
 delete from value_storage where value_storage_id in (select value_storage_id from acd_queue);
 delete from setting_value where value_storage_id in (select value_storage_id from acd_server);
 delete from value_storage where value_storage_id in (select value_storage_id from acd_server);
--- drop in order to avoid FKs
+
+-- drop tables in order to avoid FKs
 drop table acd_agent_queue cascade;
 drop table acd_queue_agent cascade;
 drop table acd_line_queue cascade;
 drop table acd_agent cascade;
 drop table acd_line cascade;
+drop table acd_queue cascade;
 drop table acd_server cascade;
 drop sequence acd_seq;
