78e534601fc7316ef427d868df2be4f58c5f7ef0
dizzy <george@ezuce.com>
2014-04-16 02:35:28 +0300

    XX-11061: Login to https will redirect to http
    - use Referer to compose redirect URL

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/Home.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/Home.java
@@ -9,6 +9,8 @@
  */
 package org.sipfoundry.sipxconfig.site;
 
+import java.net.URL;
+
 import org.apache.tapestry.PageRedirectException;
 import org.apache.tapestry.RedirectException;
 import org.apache.tapestry.annotations.InitialValue;
@@ -16,6 +18,7 @@ import org.apache.tapestry.annotations.InjectObject;
 import org.apache.tapestry.annotations.InjectState;
 import org.apache.tapestry.event.PageBeginRenderListener;
 import org.apache.tapestry.event.PageEvent;
+import org.apache.tapestry.web.WebRequest;
 import org.sipfoundry.sipxconfig.admin.AdminContext;
 import org.sipfoundry.sipxconfig.common.CoreContext;
 import org.sipfoundry.sipxconfig.components.SipxBasePage;
@@ -47,19 +50,46 @@ public abstract class Home extends SipxBasePage implements PageBeginRenderListen
 
     @Override
     public void pageBeginRender(PageEvent event) {
+        // TODO this implementation is wrong, redirection should not be done from Tapestry but
+        // at Spring level in SipxSimpleUrlAuthenticationSuccessHandler!
+        // Right now we're redirecting twice on success authentication, once to Home and again to unite web
         if (!getUserSession().isAdmin()) {
             Setting adminSettings = getAdminContext().getSettings().getSettings();
             Setting oldPortal = adminSettings.getSetting("user-portal/old-portal");
             if (oldPortal != null) {
                 boolean oldPortalEnabled = (Boolean) oldPortal.getTypedValue();
                 if (!oldPortalEnabled) {
-                    throw new RedirectException("/unitelite");
+                    throw new RedirectException(getRedirectUrl("/unitelite"));
                 }
             }
             if (getFeatureManager().isFeatureEnabled(Ivr.FEATURE)) {
                 throw new PageRedirectException(ManageVoicemail.PAGE);
             }
             throw new PageRedirectException(UserRegistrations.PAGE);
+        }
+    }
+
+    protected String getRedirectUrl(String suffix) {
+        try {
+            WebRequest request = getRequest();
+            String referer = request.getHeader("Referer");
+            URL refererUrl = new URL(referer);
+            StringBuilder url = new StringBuilder();
+            String protocol = refererUrl.getProtocol();
+            url.append(protocol);
+            url.append("://");
+            url.append(refererUrl.getHost());
+            int port = refererUrl.getPort();
+            if (port > 0
+                    && ((protocol.equalsIgnoreCase("http") && port != 80)
+                    || (protocol.equalsIgnoreCase("https") && port != 443))) {
+                url.append(':');
+                url.append(port);
+            }
+            url.append(suffix);
+            return url.toString();
+        } catch (Exception ex) {
+            return suffix;
         }
     }
 
