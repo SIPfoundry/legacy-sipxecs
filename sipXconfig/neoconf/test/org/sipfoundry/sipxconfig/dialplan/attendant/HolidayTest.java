95eae1007c34ad7d1980c5c98c436034f053a0e8
Mircea Carasel <mirceac@ezuce.com>
2014-05-12 17:39:56 +0300

    UC-2823: 14.04 Holiday Attendant faild on upgrade from 4.6 to 14.04
    -if startDate is empty we default it to today
    -if endDate is empty we default it to startDate + 1 day
    -test added

--- sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/dialplan/attendant/HolidayTest.java
+++ sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/dialplan/attendant/HolidayTest.java
@@ -10,12 +10,14 @@
 package org.sipfoundry.sipxconfig.dialplan.attendant;
 
 import java.sql.Timestamp;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
-import org.sipfoundry.commons.util.HolidayPeriod;
-
 import junit.framework.TestCase;
+
+import org.sipfoundry.commons.util.HolidayPeriod;
 
 public class HolidayTest extends TestCase {
     private HolidayPeriod m_now;
@@ -47,6 +49,61 @@ public class HolidayTest extends TestCase {
         assertEquals(1, holiday.getPeriods().size());
         holiday.removePeriod(m_now);
         assertTrue(holiday.getPeriods().isEmpty());
+    }
+
+    public void testNotNullPeriod() {
+        Calendar calStart = new GregorianCalendar();
+        Calendar calEnd = new GregorianCalendar();
+        Calendar calStartToday = new GregorianCalendar();
+        calStartToday.setTime(new Date());
+        calStartToday.set(Calendar.HOUR_OF_DAY, 0);
+        calStartToday.set(Calendar.MINUTE, 0);
+        calStartToday.set(Calendar.SECOND, 0);
+        Calendar calStartADay = new GregorianCalendar();
+        calStartADay.set(Calendar.YEAR, 2014);
+        calStartADay.set(Calendar.MONTH, 4);
+        calStartADay.set(Calendar.DAY_OF_MONTH, 22);
+        calStartADay.set(Calendar.HOUR_OF_DAY, 0);
+        calStartADay.set(Calendar.MINUTE, 0);
+        calStartADay.set(Calendar.SECOND, 0);
+        //when start date is set to null, the period will default to 1 day holiday: today
+        HolidayPeriod period = new HolidayPeriod();
+        period.setStartDate(null);
+        period.setEndDate(null);
+        calStart.setTime(period.getStartDate());
+        calEnd.setTime(period.getEndDate());
+        assertEquals(calStart.get(Calendar.YEAR), calStartToday.get(Calendar.YEAR));
+        assertEquals(calStart.get(Calendar.MONTH), calStartToday.get(Calendar.MONTH));
+        assertEquals(calStart.get(Calendar.DAY_OF_MONTH), calStartToday.get(Calendar.DAY_OF_MONTH));
+        assertEquals(calStart.get(Calendar.HOUR_OF_DAY), calStartToday.get(Calendar.HOUR_OF_DAY));
+        assertEquals(calStart.get(Calendar.MINUTE), calStartToday.get(Calendar.MINUTE));
+        assertEquals(calStart.get(Calendar.SECOND), calStartToday.get(Calendar.SECOND));
+        assertEquals(calEnd.get(Calendar.YEAR), calStartToday.get(Calendar.YEAR));
+        assertEquals(calEnd.get(Calendar.MONTH), calStartToday.get(Calendar.MONTH));
+        assertEquals(calEnd.get(Calendar.DAY_OF_MONTH), calStartToday.get(Calendar.DAY_OF_MONTH) + 1);
+        assertEquals(calEnd.get(Calendar.HOUR_OF_DAY), 0);
+        assertEquals(calEnd.get(Calendar.MINUTE), 0);
+        assertEquals(calEnd.get(Calendar.SECOND), 0);
+        //when start date is set to a day not null, but end date is null,
+        //the period will default to 1 day holiday (the day of start date)
+        HolidayPeriod period2 = new HolidayPeriod();
+        period2.setStartDate(calStartADay.getTime());
+        period2.setEndDate(null);
+        calStart.setTime(period2.getStartDate());
+        calEnd.setTime(period2.getEndDate());
+        assertEquals(calStart.get(Calendar.YEAR), 2014);
+        assertEquals(calStart.get(Calendar.MONTH), 4);
+        assertEquals(calStart.get(Calendar.DAY_OF_MONTH), 22);
+        assertEquals(calStart.get(Calendar.HOUR_OF_DAY), 0);
+        assertEquals(calStart.get(Calendar.MINUTE), 0);
+        assertEquals(calStart.get(Calendar.SECOND), 0);
+        assertEquals(calEnd.get(Calendar.YEAR), 2014);
+        assertEquals(calEnd.get(Calendar.MONTH), 4);
+        assertEquals(calEnd.get(Calendar.DAY_OF_MONTH), 23);
+        assertEquals(calEnd.get(Calendar.HOUR_OF_DAY), 0);
+        assertEquals(calEnd.get(Calendar.MINUTE), 0);
+        assertEquals(calEnd.get(Calendar.SECOND), 0);
+
     }
 
     public void testGetDay() {
@@ -97,7 +154,7 @@ public class HolidayTest extends TestCase {
         holiday.chop(4);
         assertEquals(3, holiday.getPeriods().size());
     }
-    
+
     private HolidayPeriod getNewHolidayPeriod() {
         HolidayPeriod holidayPeriod = new HolidayPeriod();
         holidayPeriod.setStartDate(new Date());
