aaf2188531cf48250ed49fc3e2cb39d8126a1a54
dizzy <george@ezuce.com>
2014-05-19 17:24:35 +0300

    XX-11115: Update Freeswitch to current
    - update to 1.5.12 beta

--- mak/freeswitch.sipxecs.mk
+++ mak/freeswitch.sipxecs.mk
@@ -1,26 +1,24 @@
-freeswitch_VER = 1.0.7
-freeswitch_PACKAGE_REVISION = $(shell cd $(SRC)/$(PROJ); ../config/revision-gen $(freeswitch_VER))
-
+freeswitch_VER = 1.5.12b
+freeswitch_TAG = 1.5.12
+freeswitch_PACKAGE_REVISION = $(shell cd $(SRC)/$(PROJ); ../config/revision-gen $(freeswitch_TAG))
 freeswitch_SRPM = freeswitch-$(freeswitch_VER)-$(freeswitch_PACKAGE_REVISION).src.rpm
 freeswitch_SPEC = $(SRC)/$(PROJ)/freeswitch.spec
 freeswitch_TARBALL = $(BUILDDIR)/$(PROJ)/freeswitch-$(freeswitch_VER).tar.bz2
 freeswitch_SOURCES = $(freeswitch_TARBALL) \
-	celt-0.10.0.tar.gz \
-	flite-1.3.99-latest.tar.gz \
-	lame-3.97.tar.gz \
-	libshout-2.2.2.tar.gz \
-	mpg123-1.13.2.tar.gz \
-	openldap-2.4.11.tar.gz \
-	pocketsphinx-0.7.tar.gz \
-	soundtouch-1.5.0.tar.gz \
-	sphinxbase-0.7.tar.gz \
-	communicator_semi_6000_20080321.tar.gz \
-	libmemcached-0.32.tar.gz \
-	json-c-0.9.tar.gz \
-	opus-0.9.0.tar.gz
+	v8-3.24.14.tar.bz2 \
+	opus-1.1.tar.gz \
+	soundtouch-1.7.1.tar.gz \
+	lame-3.98.4.tar.gz \
+	flite-1.5.4-current.tar.bz2 \
+	pcre-8.34.tar.gz
 
-freeswitch_SRPM_DEFS = --define "buildno $(freeswitch_PACKAGE_REVISION)"
-freeswitch_RPM_DEFS = --define="buildno $(freeswitch_PACKAGE_REVISION)"
+freeswitch_SRPM_DEFS = \
+	--define "BUILD_NUMBER $(freeswitch_PACKAGE_REVISION)" \
+	--define "VERSION_NUMBER $(freeswitch_VER)"
+
+freeswitch_RPM_DEFS = \
+	--define="BUILD_NUMBER $(freeswitch_PACKAGE_REVISION)" \
+	--define "VERSION_NUMBER $(freeswitch_VER)"
 
 freeswitch.dist : $(SRC)/freeswitch/.git
 	test -d $(dir $(freeswitch_TARBALL)) || mkdir -p $(dir $(freeswitch_TARBALL))
