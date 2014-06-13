ffca38aa9e8ca569ea4323b9689da335823cb18f
Mircea Carasel <mirceac@ezuce.com>
2014-06-13 12:29:11 +0300

    XX-11168: Update web.xml to use servlet 3.0 capabilities including websocket support in sipXconfig
    -update web.xml to servlet 3.0 spec
    -add spring 4 web socket api dependencies
    -add STOMP (with keep-alive capabilities - version 1.1) and SockJs java script APIs
    -add websocket capabilities to the new Jetty 9.1.5 (include Jetty 9.1.5 websocket jars in sipxconfig classpath)
    -create ws path below CXF /api (api/ws) and map it with spring servlet to route ws packets - same security rules as for CXF /api path
    -the websocket router (spring servlet) inherits sipXconfig root context, and has a dedicated child context (empty now: ws-servlet.xml)
    and can benefit of all sipxconfig context beans and event listener mechanisms (DaoListener for example)
    -websocket app to receive change (save/update) phone notifications(using stomp/sockJs/spring4)
    -reuse CXF REST beans and perform automatic marshaling between JAVA pojo and JSON websocket packet(using jackson API & STOMP 1.1)

---
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/websocket/PhoneController.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2014 eZuce, Inc. All rights reserved.
+ * Contributed to SIPfoundry under a Contributor Agreement
+ *
+ * This software is free software; you can redistribute it and/or modify it under
+ * the terms of the Affero General Public License (AGPL) as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ */
+package org.sipfoundry.sipxconfig.websocket;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.sipfoundry.sipxconfig.api.model.PhoneBean;
+import org.sipfoundry.sipxconfig.common.event.DaoEventListener;
+import org.sipfoundry.sipxconfig.phone.Phone;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PhoneController implements DaoEventListener {
+    private static final Log LOG = LogFactory.getLog(PhoneController.class);
+
+    @Autowired
+    private SimpMessagingTemplate m_template;
+
+    @Override
+    public void onDelete(Object entity) {
+
+    }
+
+    @Override
+    public void onSave(Object entity) {
+        if (entity instanceof Phone) {
+            Phone phone = (Phone) entity;
+            try {
+                m_template.convertAndSend("/topic/store-phone", PhoneBean.convertPhone(phone));
+            } catch (Exception ex) {
+                LOG.error("Cannot push packet ", ex);
+            }
+        }
+
+    }
+}
