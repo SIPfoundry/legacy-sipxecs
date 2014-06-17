712894a0d8af67b422a616cf5f81a56a8a2800dc
Mircea Carasel <mirceac@ezuce.com>
2014-06-17 10:35:35 +0300

    XX-11169: Setup Hazelcast framework
    -hazelcast 3.3 jar available in sipXcommons
    -hazelcast 3.3 jar added in sipXconfig, sipXivr classpaths
    -added replication mechanism in sipXconfig to generate hazelcast config file
    -sipXivr and sipXconfig automatically join hazelcast cluster using custom generated config file
    -hazelcast topic for voicemail event handling
    -fixed security bug in ws URL - wire the sipXconfig authenticated user from root securityContext into ws
    by creating a custom WS security handler
    -websocket VmController to route vm events to websocket for dedicated authenticated user

---
+++ sipXcommons/src/main/java/org/sipfoundry/commons/hz/HzVmEvent.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2014 eZuce, Inc. All rights reserved.
+ * Contributed to SIPfoundry under a Contributor Agreement
+ *
+ * This software is free software; you can redistribute it and/or modify it under
+ * the terms of the Affero General Public License (AGPL) as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ */
+package org.sipfoundry.commons.hz;
+
+import java.io.Serializable;
+
+public class HzVmEvent implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String m_userIdFrom;
+    private String m_userIdTo;
+    private String m_description;
+
+    public HzVmEvent(String userIdFrom, String userIdTo, String description) {
+        m_userIdFrom = userIdFrom;
+        m_userIdTo = userIdTo;
+        m_description = description;
+    }
+
+    public String getUserIdFrom() {
+        return m_userIdFrom;
+    }
+
+    public String getUserIdTo() {
+        return m_userIdTo;
+    }
+
+    public String getDescription() {
+        return m_description;
+    }
+}
