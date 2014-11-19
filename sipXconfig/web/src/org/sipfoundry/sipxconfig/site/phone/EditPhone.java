4640bd934a00b062b4e846e448e9e7947fc95320
Cristi Starasciuc <cristi@ezuce.com>
2014-11-19 10:38:09 +0200

    XX-11460 - make sure group added to a phone is saved if it does not exist.

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/phone/EditPhone.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/phone/EditPhone.java
@@ -11,6 +11,7 @@ package org.sipfoundry.sipxconfig.site.phone;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Set;
 
 import org.apache.tapestry.IPage;
 import org.apache.tapestry.IRequestCycle;
@@ -24,6 +25,7 @@ import org.sipfoundry.sipxconfig.components.TapestryUtils;
 import org.sipfoundry.sipxconfig.device.ProfileManager;
 import org.sipfoundry.sipxconfig.phone.Phone;
 import org.sipfoundry.sipxconfig.phone.PhoneContext;
+import org.sipfoundry.sipxconfig.setting.Group;
 import org.sipfoundry.sipxconfig.setting.SettingDao;
 
 /**
@@ -72,6 +74,7 @@ public abstract class EditPhone extends PhoneBasePage implements PageBeginRender
         if (valid) {
             PhoneContext dao = getPhoneContext();
             Phone phone = getPhone();
+            saveGroups(getSettingDao(), phone.getGroups());
             dao.storePhone(phone);
         }
 
@@ -92,5 +95,14 @@ public abstract class EditPhone extends PhoneBasePage implements PageBeginRender
         // Load the phone with the ID that was passed in
         PhoneContext context = getPhoneContext();
         setPhone(context.loadPhone(getPhoneId()));
+    }
+
+    public static void saveGroups(SettingDao dao, Set<Group> groups) {
+        for (Group group : groups) {
+            if (group.isNew()) {
+                group.setResource(PhoneContext.GROUP_RESOURCE_ID);
+                dao.saveGroup(group);
+            }
+        }
     }
 }
