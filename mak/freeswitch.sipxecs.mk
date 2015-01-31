e36fcbc73188f052e022d77a30918ce43005e0ce
dizzy <george@ezuce.com>
2015-01-31 12:02:18 +0200

    XX-11576: first phase: Upgrade Freeswitch to 1.4.14

--- mak/freeswitch.sipxecs.mk
+++ mak/freeswitch.sipxecs.mk
@@ -6,11 +6,14 @@ freeswitch_SPEC = $(SRC)/$(PROJ)/freeswitch.spec
 freeswitch_TARBALL = $(BUILDDIR)/$(PROJ)/freeswitch-$(freeswitch_VER).tar.bz2
 freeswitch_SOURCES = $(freeswitch_TARBALL) \
 	v8-3.24.14.tar.bz2 \
-	opus-1.1.tar.gz \
+	opus-1.1-p2.tar.gz \
 	soundtouch-1.7.1.tar.gz \
 	lame-3.98.4.tar.gz \
 	flite-1.5.4-current.tar.bz2 \
-	pcre-8.34.tar.gz
+	pcre-8.34.tar.gz \
+	sphinxbase-0.8.tar.gz \
+	pocketsphinx-0.8.tar.gz \
+	mongo-c-driver-0.92.2.tar.gz
 
 freeswitch_SRPM_DEFS = \
 	--define "BUILD_NUMBER $(freeswitch_PACKAGE_REVISION)" \
