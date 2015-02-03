5827d9d750d8c1e23ca57f10ec6812e1aa8d6416
Ionut Oancea <ioancea@ezuce.com>
2015-02-03 18:02:25 +0200

    XX-11578: Create patch for mongo-cxx-driver in order to be able to register log callbacks
    - added the patch file in .sipxecs.mk as well

--- mongo-cxx-driver/.sipxecs.mk
+++ mongo-cxx-driver/.sipxecs.mk
@@ -3,6 +3,8 @@ mongo-cxx-driver_REL = 1
 mongo-cxx-driver_SRPM = mongo-cxx-driver-$(mongo-cxx-driver_VER)-$(mongo-cxx-driver_REL)$(RPM_DIST).src.rpm
 mongo-cxx-driver_SPEC = $(SRC)/$(PROJ)/mongo-cxx-driver.spec
 mongo-cxx-driver_SOURCES = \
-        mongo-cxx-driver-legacy-0.0-26compat-2.6.7.tar.gz
+        mongo-cxx-driver-legacy-0.0-26compat-2.6.7.tar.gz \
+	$(SRC)/$(PROJ)/mongo-cxx-driver-2.6.7-logger.patch
+	                                                                                                                                     
 
 mongo-cxx-driver.dist:;
