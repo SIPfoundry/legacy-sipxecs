1adf8d7d1e6bec241df5fee47b33a9d837746a71
Andrei Vilcu <vilcu.andrei@gmail.com>
2015-02-11 16:59:04 +0200

    UC-108 Add IM ID to initial user account setup page

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/user/NewUser.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/user/NewUser.java
@@ -81,6 +81,9 @@ public abstract class NewUser extends PageWithCallback implements PageBeginRende
         }
         // Save the user
         User user = getUser();
+        if (user.getImId() == null) {
+            user.setImId(user.getUserName());
+        }
         EditUser.saveGroups(getSettingDao(), user.getGroups());
 
         // Execute the automatic assignments for the user.
